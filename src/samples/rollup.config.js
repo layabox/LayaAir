462b438e40d966b05ee96c4b3ef7385b60c8f035
GuoLei <gl3336563@163.com>
2019-06-12 17:10:20 +0800

    GL 基本清理一下配置

--- src/samples/rollup.config.js
+++ src/samples/rollup.config.js
@@ -1,23 +1,9 @@
-//import { terser } from 'rollup-plugin-terser';
 const typescript = require('rollup-plugin-typescript2');
 import glsl from 'rollup-plugin-glsl';
 const path = require('path')
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
-
-const resolveFile = function(filePath) {
-    return path.join(__dirname, '..', filePath)
-}
-
-
-function testPlug(){
-    return {
-        transform( code, id ) {
-            console.log(id,'\n',code);
-        }
-    }
-}
 
 export default { 
     input: './index.ts',
@@ -27,22 +13,17 @@ export default {
 		format: 'iife', // immediately-invoked function expression — suitable for <script> tags
         sourcemap: false,
         name:'laya',
-        //indent: false
 	},
 	plugins: [
         typescript({
             //abortOnError:false
             check: false
         }),
-        //testPlug(),
         glsl({
 			// By default, everything gets included
 			include: /.*(.glsl|.vs|.fs)$/,
             sourceMap: false,
             compress:false
 		}),        
-		//resolve(), // tells Rollup how to find date-fns in node_modules
-		//commonjs(), // converts date-fns to ES modules
-		//production && terser() // minify, but only in production
 	]
 };
