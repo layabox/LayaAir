74710ad9e5625efcadb738c67534ac1bd4daef0e
GuoLei <gl3336563@163.com>
2019-06-17 19:37:25 +0800

    GL  增加性能测试案例

--- src/samples/3d/LayaAir3DTest_Performance/DynamicBatchTest.ts
+++ src/samples/3d/LayaAir3DTest_Performance/DynamicBatchTest.ts
@@ -1,52 +1,61 @@
-import { Laya3D } from "Laya3D";
 import { Laya } from "Laya";
-import { CameraMoveScript } from "../common/CameraMoveScript"
-	import { Camera } from "laya/d3/core/Camera"
-	import { MeshSprite3D } from "laya/d3/core/MeshSprite3D"
-	import { Sprite3D } from "laya/d3/core/Sprite3D"
-	import { BaseMaterial } from "laya/d3/core/material/BaseMaterial"
-	import { BlinnPhongMaterial } from "laya/d3/core/material/BlinnPhongMaterial"
-	import { Scene3D } from "laya/d3/core/scene/Scene3D"
-	import { Vector3 } from "laya/d3/math/Vector3"
-	import { Mesh } from "laya/d3/resource/models/Mesh"
-	import { PrimitiveMesh } from "laya/d3/resource/models/PrimitiveMesh"
-	import { Shader3D } from "laya/d3/shader/Shader3D"
-	import { Stage } from "laya/display/Stage"
-	import { Handler } from "laya/utils/Handler"
-	import { Stat } from "laya/utils/Stat"
-	
-	export class DynamicBatchTest {
-		constructor(){
-			Shader3D.debugMode = true;
-			Laya3D.init(0, 0);
-			Laya.stage.scaleMode = Stage.SCALE_FULL;
-			Laya.stage.screenMode = Stage.SCREEN_NONE;
-			Stat.show();
-			
-			var scene:Scene3D = (<Scene3D>Laya.stage.addChild(new Scene3D()) );
-			scene.ambientColor = new Vector3(1, 1, 1);
-			
-			var camera:Camera = (<Camera>scene.addChild(new Camera(0, 0.1, 1000)) );
-			camera.transform.translate(new Vector3(0, 50, 100));
-			camera.transform.rotate(new Vector3(-30, 0, 0), true, false);
-			camera.addComponent(CameraMoveScript);
-			
-			var boxMesh:Mesh = PrimitiveMesh.createBox(0.5, 0.5, 0.5);
-			var mat:BlinnPhongMaterial = new BlinnPhongMaterial();
-			
-			Sprite3D.load("Test/Plane/plane.lh", Handler.create(null, function(sprite:Sprite3D):void {
-				//Sprite3D.load("../../../../res/threeDimen/staticModel/lizard/lizard.lh", Handler.create(null, function(sprite:Sprite3D):void {
-				for (var i:number = 0; i < 30000; i++) {
-					//var x:Sprite3D = sprite.clone();
-					var x:MeshSprite3D = new MeshSprite3D(boxMesh);
-					x.meshRenderer.sharedMaterial = mat;
-					//(((x.getChildAt(0) as MeshSprite3D).meshRenderer as MeshRenderer).sharedMaterial as BlinnPhongMaterial).renderQueue = BaseMaterial.RENDERQUEUE_TRANSPARENT;
-					x.transform.localPosition = new Vector3(i * 0.001 - 500 * 0.001, 0, 0);
-					scene.addChild(x);
+import { Camera } from "laya/d3/core/Camera";
+import { BlinnPhongMaterial } from "laya/d3/core/material/BlinnPhongMaterial";
+import { MeshSprite3D } from "laya/d3/core/MeshSprite3D";
+import { Scene3D } from "laya/d3/core/scene/Scene3D";
+import { Matrix4x4 } from "laya/d3/math/Matrix4x4";
+import { Vector3 } from "laya/d3/math/Vector3";
+import { Mesh } from "laya/d3/resource/models/Mesh";
+import { PrimitiveMesh } from "laya/d3/resource/models/PrimitiveMesh";
+import { Shader3D } from "laya/d3/shader/Shader3D";
+import { Stage } from "laya/display/Stage";
+import { Stat } from "laya/utils/Stat";
+import { Laya3D } from "Laya3D";
+import { CameraMoveScript } from "../common/CameraMoveScript";
+import { Texture2D } from "laya/resource/Texture2D";
+import { Handler } from "laya/utils/Handler";
+
+export class DynamicBatchTest {
+	constructor() {
+		Shader3D.debugMode = true;
+		Laya3D.init(0, 0);
+		Laya.stage.scaleMode = Stage.SCALE_FULL;
+		Laya.stage.screenMode = Stage.SCREEN_NONE;
+		Stat.show();
+
+		var scene: Scene3D = (<Scene3D>Laya.stage.addChild(new Scene3D()));
+		scene.ambientColor = new Vector3(1, 1, 1);
+
+		var camera: Camera = (<Camera>scene.addChild(new Camera(0, 0.1, 1000)));
+		camera.transform.translate(new Vector3(0, 6.2, 10.5));
+		camera.transform.rotate(new Vector3(-40, 0, 0), true, false);
+		camera.addComponent(CameraMoveScript);
+
+		Texture2D.load("res/threeDimen/layabox.png", Handler.create(null, function (tex: Texture2D): void {
+			var radius: Vector3 = new Vector3(0, 0, 1);
+			var radMatrix: Matrix4x4 = new Matrix4x4();
+			var circleCount: number = 50;
+
+			var boxMesh: Mesh = PrimitiveMesh.createBox(0.02, 0.02, 0.02);
+			var boxMat: BlinnPhongMaterial = new BlinnPhongMaterial();
+			boxMat.albedoTexture = tex;
+			for (var i: number = 0; i < circleCount; i++) {
+				radius.z = 1.0 + i * 0.15;
+				radius.y = i * 0.03;
+				var oneCircleCount: number = 100 + i * 15;
+				for (var j: number = 0; j < oneCircleCount; j++) {
+					var boxSprite: MeshSprite3D = new MeshSprite3D(boxMesh);
+					boxSprite.meshRenderer.sharedMaterial = boxMat;
+					var localPos: Vector3 = boxSprite.transform.localPosition;
+					var rad: number = ((Math.PI * 2) / oneCircleCount) * j;
+					Matrix4x4.createRotationY(rad, radMatrix);
+					Vector3.transformCoordinate(radius, radMatrix, localPos);
+					boxSprite.transform.localPosition = localPos;
+					scene.addChild(boxSprite);
 				}
-			}));
-		
-		}
-	
+			}
+		}));
 	}
+
+}
 
