59d0faa3203f950937828d17512fd843cc162d07
guozhaokui <guozk@layabox.com>
2019-06-26 15:45:49 +0800

    phy2d

---
+++ src/layaAir/laya/physics/joint/JointBase.ts
@@ -0,0 +1,35 @@
+import { Component } from "../../../../../core/src/laya/components/Component"
+	import { Physics } from "../Physics"
+	
+	/**
+	 * 关节基类
+	 */
+	export class JointBase extends Component {
+		/**原生关节对象*/
+		protected _joint:any;
+		
+		/**[只读]原生关节对象*/
+		 get joint():any {
+			if (!this._joint) this._createJoint();
+			return this._joint;
+		}
+		
+		/*override*/ protected _onEnable():void {
+			this._createJoint();
+		}
+		
+		/*override*/ protected _onAwake():void {
+			this._createJoint();
+		}
+		
+		protected _createJoint():void {
+		}
+		
+		/*override*/ protected _onDisable():void {
+			if (this._joint) {
+				Physics.I._removeJoint(this._joint);
+				this._joint = null;
+			}
+		}
+	}
+
