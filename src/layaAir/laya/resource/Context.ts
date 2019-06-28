ec18afb85301b02cc2d6bbc7a2b9700242fd33b4
guozhaokui <guozk@layabox.com>
2019-06-28 16:11:09 +0800

    drawtexture + uv

--- src/layaAir/laya/resource/Context.ts
+++ src/layaAir/laya/resource/Context.ts
@@ -1481,7 +1481,7 @@ export class Context {
 	 * @param	ty
 	 * @param	alpha
 	 */
-	drawTextureWithTransform(tex: Texture, x: number, y: number, width: number, height: number, transform: Matrix, tx: number, ty: number, alpha: number, blendMode: string, colorfilter: ColorFilter = null): void {
+	drawTextureWithTransform(tex: Texture, x: number, y: number, width: number, height: number, transform: Matrix, tx: number, ty: number, alpha: number, blendMode: string, colorfilter: ColorFilter = null,uv?:number[]): void {
 		var oldcomp: string = null;
 		var curMat: Matrix = this._curMat;
 		if (blendMode) {
@@ -1494,7 +1494,7 @@ export class Context {
 		}
 
 		if (!transform) {
-			this._drawTextureM(tex, x + tx, y + ty, width, height, curMat, alpha, null);
+			this._drawTextureM(tex, x + tx, y + ty, width, height, curMat, alpha, uv);
 			if (blendMode) {
 				this.globalCompositeOperation = oldcomp;
 			}
@@ -1519,7 +1519,7 @@ export class Context {
 			tmpMat.ty += curMat.ty;
 			transform = tmpMat;
 		}
-		this._drawTextureM(tex, x, y, width, height, transform, alpha, null);
+		this._drawTextureM(tex, x, y, width, height, transform, alpha, uv);
 		if (blendMode) {
 			this.globalCompositeOperation = oldcomp;
 		}
