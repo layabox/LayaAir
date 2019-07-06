f4260e65658978d0d9130c2702c0fa8d619484a2
ZhengQixv <1029805476@qq.com>
2019-07-06 15:46:56 +0800

    zqx--gulp 编译重新配置

--- gulpfile.js
+++ gulpfile.js
@@ -24,3 +24,27 @@ gulp.task('default', function () {
 		//把bundle.js复制到bin/js目录
 		.pipe(gulp.dest("bin"));
 });
+
+
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject('src/layaAir/tsconfig.json');
+
+gulp.task('LayaAir', () => {
+      return new Promise(function (resolve, reject) {
+            gulp.src(['src/layaAir/**/*.ts', '!src/layaAir/laya/d3Extend/Cube/**/*.ts', '!src/layaAir/laya/d3Extend/D3UI/**/*.ts',
+				  '!src/layaAir/laya/d3Extend/lineRender/**/*.ts', '!src/layaAir/laya/d3Extend/physics/**/*.ts', 
+				  '!src/layaAir/laya/d3Extend/vox/**/*.ts', '!src/layaAir/laya/d3Extend/worldMaker/**/*.ts']).pipe(tsProject()).pipe(gulp.dest('bin/tsc/layaAir'));
+            resolve();
+      });
+});
+
+gulp.task('Copy Shaders', () => {
+      return new Promise(function (resolve, reject) {
+            gulp.src(['/src/layaAir/laya/d3/shader/files/**/*.vs', '/src/layaAir/laya/d3/shader/files/**/*.ps', '/src/layaAir/d3/shader/files/**/*.glsl'])
+                  .pipe(gulp.dest('bin/tsc/layaAir/laya/d3/shader/files'));
+            resolve();
+      });
+});
+
+gulp.task('Build', gulp.series('Copy Shaders', 'LayaAir'));
+
