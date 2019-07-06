03115eab6918ed1c027a6840a5644ad7392a3e4e
ZhengQixv <1029805476@qq.com>
2019-07-06 14:52:51 +0800

    zqx--编译ts-->js(vscode中的快捷键)

---
+++ src/compileTool/compile.bat
@@ -0,0 +1,2 @@
+cd ..\layaAir
+gulp Build
--- src/layaAir/gulpfile.js
+++ src/layaAir/gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
+var tsProject = ts.createProject('../layaAir/tsconfig.json');
 
 gulp.task('LayaAir', () => {
       return new Promise(function (resolve, reject) {
-            gulp.src(['./**/*.ts', '!test.ts', '!laya/d3Extend/Cube/**/*.ts', '!laya/d3Extend/D3UI/**/*.ts',
+            gulp.src(['./**/*.ts', '!laya/d3Extend/Cube/**/*.ts', '!laya/d3Extend/D3UI/**/*.ts',
                   '!laya/d3Extend/lineRender/**/*.ts', '!laya/d3Extend/physics/**/*.ts', '!laya/d3Extend/vox/**/*.ts', '!laya/d3Extend/worldMaker/**/*.ts']).pipe(tsProject()).pipe(gulp.dest('../../bin/tsc/layaAir'));
             resolve();
       });
@@ -12,7 +12,7 @@ gulp.task('LayaAir', () => {
 
 gulp.task('Copy Shaders', () => {
       return new Promise(function (resolve, reject) {
-            gulp.src(['laya/d3/shader/files/**/*.vs', 'laya/d3/shader/files/**/*.ps', 'laya/d3/shader/files/**/*.glsl'])
+            gulp.src(['/laya/d3/shader/files/**/*.vs', '/laya/d3/shader/files/**/*.ps', '/d3/shader/files/**/*.glsl'])
                   .pipe(gulp.dest('../../bin/tsc/layaAir/laya/d3/shader/files'));
             resolve();
       });
