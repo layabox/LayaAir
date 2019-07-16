d171c61573209adb07785713f058cf1e64a40705
GuoLei <gl3336563@163.com>
2019-07-16 10:09:04 +0800

    GL  修复运行错误

--- src/layaAir/Laya3D.ts
+++ src/layaAir/Laya3D.ts
@@ -83,6 +83,7 @@ import { VertexShurikenParticleMesh } from "./laya/d3/graphics/Vertex/VertexShur
 import { VertexPositionTexture0 } from "./laya/d3/graphics/Vertex/VertexPositionTexture0";
 import { VertexTrail } from "./laya/d3/core/trail/VertexTrail";
 import { PixelLineVertex } from "./laya/d3/core/pixelLine/PixelLineVertex";
+import { VertexPositionTerrain } from "./laya/d3/graphics/Vertex/VertexPositionTerrain";
 
 /**
  * <code>Laya3D</code> 类用于初始化3D设置。
@@ -195,6 +196,7 @@ export class Laya3D {
 		VertexShurikenParticleMesh.__init__();
 		VertexPositionTexture0.__init__();
 		VertexTrail.__init__();
+		VertexPositionTerrain.__init__();
 		PixelLineVertex.__init__();
 		SubMeshInstanceBatch.__init__();
 		SubMeshDynamicBatch.__init__();
