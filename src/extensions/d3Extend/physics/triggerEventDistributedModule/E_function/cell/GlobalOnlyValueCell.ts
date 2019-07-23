93630e8f3567f12f2ff545722af7e9003bc8e01e
GuoLei <gl3336563@163.com>
2019-07-23 10:33:11 +0800

    GL  增加d3库

---
+++ src/extensions/d3Extend/physics/triggerEventDistributedModule/E_function/cell/GlobalOnlyValueCell.ts
@@ -0,0 +1,12 @@
+﻿export class GlobalOnlyValueCell {
+
+    private static currentID:number=0;
+
+     static getOnlyID():number
+    {
+        GlobalOnlyValueCell.currentID +=1;
+        return GlobalOnlyValueCell.currentID;
+    }
+}
+
+
