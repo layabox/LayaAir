7bf29158922ffbb0417a1a329b2d39d098842a69
lvfulong <775331175@qq.com>
2019-07-23 10:54:46 +0800

    revert

--- src/layaAir/laya/resource/HTMLCanvas.ts
+++ src/layaAir/laya/resource/HTMLCanvas.ts
@@ -124,8 +124,8 @@ import { Browser } from "../utils/Browser";
 				this._setCPUMemory(w * h * 4);
 				this._ctx && this._ctx.size && this._ctx.size(w, h);
 				if(this._source){// && this._source instanceof HTMLCanvasElement){
-					this._source._height = h;
-					this._source._width = w;
+					this._source.height = h;
+					this._source.width = w;
                 }
 				if (this._texture)
 				{
