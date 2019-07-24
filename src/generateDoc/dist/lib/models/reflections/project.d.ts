f9c53e9b64f7f17e90b7239a673c861b8d3cc0f7
ZhengQixv <1029805476@qq.com>
2019-07-24 18:31:59 +0800

    zqx--添加生成api工具

---
+++ src/generateDoc/dist/lib/models/reflections/project.d.ts
@@ -0,0 +1,20 @@
+import { SourceFile, SourceDirectory } from '../sources/index';
+import { Reflection, ReflectionKind } from './abstract';
+import { ContainerReflection } from './container';
+export declare class ProjectReflection extends ContainerReflection {
+    reflections: {
+        [id: number]: Reflection;
+    };
+    symbolMapping: {
+        [symbolId: number]: number;
+    };
+    directory: SourceDirectory;
+    files: SourceFile[];
+    name: string;
+    readme?: string;
+    packageInfo: any;
+    constructor(name: string);
+    isProject(): boolean;
+    getReflectionsByKind(kind: ReflectionKind): Reflection[];
+    findReflectionByName(arg: string | string[]): Reflection | undefined;
+}
---
+++ src/generateDoc/run.bat
@@ -0,0 +1,8 @@
+@echo off
+set curr_dir=%cd%
+chdir /D "%~dp0"
+
+node .\typedoc --mode modules --excludePrivate --excludeProtected --module commonjs --target ES6 --tsconfig ../layaAir/tsconfig.json --includes ../layaAir --name zqx --theme default --out doc\ --ignoreCompilerErrors
+
+chdir /D "%curr_dir%"
+@pause
