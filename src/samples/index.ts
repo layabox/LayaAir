30517fc5110b490ddbbf75ab978e42b0f798596d
liuchunling <chunling.liu@elegine.ai>
2019-07-30 16:20:24 +0800

    增加3D字体旋转

--- src/samples/index.ts
+++ src/samples/index.ts
@@ -1,2 +1,5 @@
 import { Main } from "./Main";
-new Main();
+import { PBRDemo } from "./3d/LayaAir3D_Demo/PBRDemo";
+import { PBRRender } from "./3d/LayaAir3D_Shader/PBRRender";
+import { LightmapScene } from "./3d/LayaAir3D_Scene3D/LightmapScene";
+new PBRRender();
