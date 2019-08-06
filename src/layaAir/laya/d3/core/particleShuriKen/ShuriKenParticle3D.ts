bf7ff2bf8fb4fd7b1061247dc33c3f9ab6d24633
GuoLei <gl3336563@163.com>
2019-08-06 11:17:02 +0800

    GL  增加3D粒子发射速度

--- src/layaAir/laya/d3/core/particleShuriKen/ShuriKenParticle3D.ts
+++ src/layaAir/laya/d3/core/particleShuriKen/ShuriKenParticle3D.ts
@@ -321,7 +321,7 @@ export class ShuriKenParticle3D extends RenderableSprite3D {
 		particleSystem.gravityModifier = data.gravityModifier;
 
 		particleSystem.simulationSpace = data.simulationSpace;
-		particleSystem.simulationSpeed = data.simulationSpeed;
+		(data.simulationSpeed !== undefined) && (particleSystem.simulationSpeed = data.simulationSpeed);
 
 		particleSystem.scaleMode = data.scaleMode;
 
