e2c0723f19654a96967cd8f9e422508f5d248f58
GuoLei <gl3336563@163.com>
2019-08-23 18:09:54 +0800

    GL gengxin demo

--- src/samples/3d/LayaAir3D_Advance/MultiLight.ts
+++ src/samples/3d/LayaAir3D_Advance/MultiLight.ts
@@ -1,26 +1,41 @@
-import { Vector3 } from "laya/d3/math/Vector3";
-import { Scene3D } from "laya/d3/core/scene/Scene3D";
-import { Camera } from "laya/d3/core/Camera";
-import { BaseCamera } from "laya/d3/core/BaseCamera";
-import { Shader3D } from "laya/d3/shader/Shader3D";
-import { Laya3D } from "Laya3D";
 import { Laya } from "Laya";
-import { Stage } from "laya/display/Stage";
-import { Stat } from "laya/utils/Stat";
-import { CameraMoveScript } from "../common/CameraMoveScript";
-import { DirectionLight } from "laya/d3/core/light/DirectionLight";
+import { Script3D } from "laya/d3/component/Script3D";
+import { Camera } from "laya/d3/core/Camera";
+import { LightSprite } from "laya/d3/core/light/LightSprite";
 import { PointLight } from "laya/d3/core/light/PointLight";
 import { SpotLight } from "laya/d3/core/light/SpotLight";
-import { Mesh } from "laya/d3/resource/models/Mesh";
-import { PrimitiveMesh } from "laya/d3/resource/models/PrimitiveMesh";
-import { MeshSprite3D } from "laya/d3/core/MeshSprite3D";
-import { BlinnPhongMaterial } from "laya/d3/core/material/BlinnPhongMaterial";
-import { Texture2D } from "laya/resource/Texture2D";
+import { Scene3D } from "laya/d3/core/scene/Scene3D";
+import { Transform3D } from "laya/d3/core/Transform3D";
+import { Vector3 } from "laya/d3/math/Vector3";
+import { Shader3D } from "laya/d3/shader/Shader3D";
+import { Stage } from "laya/display/Stage";
 import { Handler } from "laya/utils/Handler";
+import { Stat } from "laya/utils/Stat";
+import { Laya3D } from "Laya3D";
+import { CameraMoveScript } from "../common/CameraMoveScript";
+
+class LightMoveScript extends Script3D {
+	forward: Vector3 = new Vector3();
+	lights: LightSprite[] = [];
+	offsets: Vector3[] = [];
+	moveRanges: Vector3[] = [];
+
+	onUpdate(): void {
+		var seed: number = Laya.timer.currTimer * 0.002;
+		for (var i: number = 0, n: number = this.lights.length; i < n; i++) {
+			var transform: Transform3D = this.lights[i].transform;
+			var pos: Vector3 = transform.localPosition;
+			var off: Vector3 = this.offsets[i];
+			var ran: Vector3 = this.moveRanges[i];
+			pos.x = off.x + Math.sin(seed) * ran.x;
+			pos.y = off.y + Math.sin(seed) * ran.y;
+			pos.z = off.z + Math.sin(seed) * ran.z;
+			transform.localPosition = pos;
+		}
+	}
+}
 
 export class MultiLight {
-
-	private rotation: Vector3 = new Vector3(0, 0.01, 0);
 
 	constructor() {
 		Shader3D.debugMode = true;
@@ -29,100 +44,74 @@ export class MultiLight {
 		Laya.stage.screenMode = Stage.SCREEN_NONE;
 		Stat.show();
 
-		var scene: Scene3D = (<Scene3D>Laya.stage.addChild(new Scene3D()));
+		Scene3D.load("res/threeDimen/scene/MultiLightScene/InventoryScene_Forest.ls", Handler.create(this, function (scene: Scene3D): void {
+			Laya.stage.addChild(scene);
 
-		var camera: Camera = (<Camera>(scene.addChild(new Camera(0, 0.1, 100))));
-		camera.transform.translate(new Vector3(0, 0.0, 1.5));
-		//camera.transform.rotate(new Vector3(-15, 0, 0), true, false);
-		camera.clearFlag = BaseCamera.CLEARFLAG_SKY;
-		camera.addComponent(CameraMoveScript);
-		camera.fieldOfView=170;
+			var camera: Camera = <Camera>scene.getChildByName("Main Camera");
+			camera.addComponent(CameraMoveScript);
+			var moveScript: LightMoveScript = camera.addComponent(LightMoveScript);
 
-		// //增加后期处理
-		// var postProcess: PostProcess = new PostProcess();
-		// //增加后期处理泛光效果
-		// var bloom: BloomEffect = new BloomEffect();
-		// postProcess.addEffect(bloom);
-		// camera.postProcess = postProcess;
-		// camera.enableHDR = true;
+			// var directionLight: DirectionLight = (<DirectionLight>scene.addChild(new DirectionLight()));
+			// var mat: Matrix4x4 = directionLight.transform.worldMatrix;
+			// mat.setForward(new Vector3(0.0, -0.8, -1.0));
+			// directionLight.color.setValue(0.5, 0.5, 0.4);
 
-		// //设置泛光参数
-		// bloom.intensity = 5;
-		// bloom.threshold = 0.9;
-		// bloom.softKnee = 0.5;
-		// bloom.clamp = 65472;
-		// bloom.diffusion = 5;
-		// bloom.anamorphicRatio = 0.0;
-		// bloom.color = new Color(1, 1, 1, 1);
-		// bloom.fastMode = true;
-
-		var directionLight: DirectionLight = (<DirectionLight>scene.addChild(new DirectionLight()));
-		directionLight.color.setValue(1, 0.4, 1);
-
-		var directionLight: DirectionLight = (<DirectionLight>scene.addChild(new DirectionLight()));
-		directionLight.color.setValue(1, 0, 0);
-		directionLight.transform.rotationEuler = new Vector3(30, 20, 30);
-
-		var directionLight: DirectionLight = (<DirectionLight>scene.addChild(new DirectionLight()));
-		directionLight.color.setValue(0, 0.9, 0.3);
-		directionLight.transform.rotationEuler = new Vector3(70, -20, 10);
+			var moverLights: LightSprite[] = moveScript.lights;
+			var offsets: Vector3[] = moveScript.offsets;
+			var moveRanges: Vector3[] = moveScript.moveRanges;
+			moverLights.length = 50;
+			for (var i: number = 0; i < 50; i++) {
+				var pointLight: PointLight = (<PointLight>scene.addChild(new PointLight()));
+				pointLight.range = 2.0 + Math.random() * 8.0;
+				pointLight.color.setValue(Math.random(), Math.random(), Math.random());
+				pointLight.intensity = 2.0 + Math.random() * 8;
+				moverLights[i] = pointLight;
+				offsets[i] = new Vector3((Math.random() - 0.5) * 20, pointLight.range * 0.75, 20.0 * Math.random() - 10);
+				moveRanges[i] = new Vector3((Math.random() - 0.5) * 40, 0, (Math.random() - 0.5) * 40);
+			}
 
 
-		var directionLight: DirectionLight = (<DirectionLight>scene.addChild(new DirectionLight()));
-		directionLight.color.setValue(0.0, 0.4, 0.9);
-		directionLight.transform.rotationEuler = new Vector3(6, 60, 13);
-
-		var pointLight: PointLight = (<PointLight>scene.addChild(new PointLight()));
-		pointLight.transform.localPosition=new Vector3(0.2,0.0,0.0);
-		pointLight.color.setValue(0.0, 0.0, 1.0);
-		pointLight.range=0.6;
-
-		var pointLight: PointLight = (<PointLight>scene.addChild(new PointLight()));
-		pointLight.transform.localPosition=new Vector3(0.0,0.5,0.0);
-		pointLight.color.setValue(0.0, 1.0, 0.0);
-		pointLight.range=1.9;
-
-		var pointLight: PointLight = (<PointLight>scene.addChild(new PointLight()));
-		pointLight.transform.localPosition=new Vector3(-0.3,0.5,0.0);
-		pointLight.color.setValue(1.0, 0.0, 0.0);
-		pointLight.range=0.9;
-
-		var pointLight: PointLight = (<PointLight>scene.addChild(new PointLight()));
-		pointLight.transform.localPosition=new Vector3(0.3,0.9,0.0);
-		pointLight.color.setValue(2.0, 0.2, 1.0);
-		pointLight.range=1.8;
-
-		var spotLight: SpotLight = (<SpotLight>scene.addChild(new SpotLight()));
-		spotLight.transform.localPosition=new Vector3(0.0,0.0,1.51);
-		spotLight.color.setValue(0.0, 0.0, 1.0);
-		spotLight.range=2;
-		spotLight.spotAngle=45;
+			var spotLight: SpotLight = (<SpotLight>scene.addChild(new SpotLight()));
+			spotLight.transform.localPosition = new Vector3(0.0, 9.0, -35.0);
+			spotLight.transform.localRotationEuler = new Vector3(-15.0, 180.0, 0.0);
+			spotLight.color.setValue(Math.random(), Math.random(), Math.random());
+			spotLight.range = 50;
+			spotLight.intensity = 15;
+			spotLight.spotAngle = 70;
 
 
-		// var x: PointLight = (<PointLight>scene.addChild(new PointLight()));
-		// x.transform.localPosition=new Vector3(0.0,0.0,1.5);
-		// x.color.setValue(0.0, 0.0, 1.0);
-		// x.range=2;
+			// for (var i: number = 0; i < 1; i++) {
+			// 	var pointLight: PointLight = (<PointLight>scene.addChild(new PointLight()));
+			// 	pointLight.range = 1.0 + 1.0 * 2.0;
+			// 	pointLight.transform.localPosition = new Vector3((1.0 - 0.5) * 10, pointLight.range * 0.75, -13.0);
+			// 	pointLight.color.setValue(1.0, 0, 0);
+			// 	pointLight.intensity = 1.0 * 10 + 2.0;
+			// }
 
-		//创建一个SphereMesh
-		var sphereMesh: Mesh = PrimitiveMesh.createSphere();
+			// var pointLight: PointLight = (<PointLight>scene.addChild(new PointLight()));
+			// pointLight.transform.localPosition = new Vector3(-5.0, 0.2, -6.0);
+			// pointLight.color.setValue(0.0, 1.0, -6.0);
+			// pointLight.range = 6.0;
+			// pointLight.intensity = 12.0;
 
-		var earth1: MeshSprite3D = (<MeshSprite3D>scene.addChild(new MeshSprite3D(sphereMesh)));
-		earth1.transform.position = new Vector3(-0.6, 0, 0);
+			// var pointLight: PointLight = (<PointLight>scene.addChild(new PointLight()));
+			// pointLight.transform.localPosition = new Vector3(-5.0, 0.2, -6.0);
+			// pointLight.color.setValue(0.0, 1.0, -4.0);
+			// pointLight.range = 6.9;
 
-		var earth2: MeshSprite3D = (<MeshSprite3D>scene.addChild(new MeshSprite3D(sphereMesh)));
-		earth2.transform.position = new Vector3(0.6, 0, 0);
-		var material: BlinnPhongMaterial = new BlinnPhongMaterial();
-		//漫反射贴图
-		Texture2D.load("res/threeDimen/texture/earth.png", Handler.create(this, function (texture: Texture2D): void {
-			material.albedoTexture = texture;
+			// var pointLight: PointLight = (<PointLight>scene.addChild(new PointLight()));
+			// pointLight.transform.localPosition = new Vector3(-5.3, 0.2, -6.0);
+			// pointLight.color.setValue(1.0, 1.0, -2.0);
+			// pointLight.range = 6.9;o
+
+			// var pointLight: PointLight = (<PointLight>scene.addChild(new PointLight()));
+			// pointLight.transform.localPosition = new Vector3(-5.3, 0.2, 0.0);
+			// pointLight.color.setValue(2.0, 0.2, 1.0);
+			// pointLight.range = 6.8;
+
+
 		}));
-		earth2.meshRenderer.material = material;
 
-		Laya.timer.frameLoop(1, this, function (): void {
-			earth1.transform.rotate(this.rotation, false);
-			earth2.transform.rotate(this.rotation, false);
-		});
 	}
 }
 
