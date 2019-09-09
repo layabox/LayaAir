e37810479b1b928216e5509ace3fa7bdda342a19
GuoLei1990 <gl3336563@163.com>
2019-09-09 10:08:18 +0800

    temp update  gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,53 +12,53 @@ gulp.task('tsc', () => {
     });
 });
 
-gulp.task('CopyNoneTSFile', ()=>{
-    return gulp.src([
-        'src/**/*.vs',
-        'src/**/*.fs',
-        'src/**/*.glsl'], { base: "src" })
-        .pipe(gulp.dest('bin/tsc'));
-});
+// gulp.task('CopyNoneTSFile', ()=>{
+//     return gulp.src([
+//         'src/**/*.vs',
+//         'src/**/*.fs',
+//         'src/**/*.glsl'], { base: "src" })
+//         .pipe(gulp.dest('bin/tsc'));
+// });
 
 
-gulp.task('buildCore', async ()=>{
-    await bundleLib([
-        './src/layaAir/Config.ts',
-        './src/layaAir/laya/Const.ts',
-        './src/layaAir/ILaya.ts',
-        './src/layaAir/Laya.ts',
-        './src/layaAir/laya/components/**/*.*',
-        './src/layaAir/laya/display/**/*.*',
-        './src/layaAir/laya/events/**/*.*',
-        './src/layaAir/laya/filters/**/*.*', 
-        './src/layaAir/laya/layagl/**/*.*',
-        './src/layaAir/laya/maths/**/*.*',
-        './src/layaAir/laya/media/**/*.*',
-        './src/layaAir/laya/net/**/*.*',
-        './src/layaAir/laya/renders/**/*.*',
-        './src/layaAir/laya/resource/**/*.*',
-        './src/layaAir/laya/system/**/*.*',
-        './src/layaAir/laya/utils/**/*.*',
-        './src/layaAir/laya/webgl/**/*.*',
-        './src/layaAir/laya/effect/**/*.*'
-        ],
-        "./src/layaAir/tsconfig.json",
-        './build/js/libs/laya.core.js',
-        'window.Laya=window.Laya||{};',
-        'exports.static=_static;'
-        )
-    });
+// gulp.task('buildCore', async ()=>{
+//     await bundleLib([
+//         './src/layaAir/Config.ts',
+//         './src/layaAir/laya/Const.ts',
+//         './src/layaAir/ILaya.ts',
+//         './src/layaAir/Laya.ts',
+//         './src/layaAir/laya/components/**/*.*',
+//         './src/layaAir/laya/display/**/*.*',
+//         './src/layaAir/laya/events/**/*.*',
+//         './src/layaAir/laya/filters/**/*.*', 
+//         './src/layaAir/laya/layagl/**/*.*',
+//         './src/layaAir/laya/maths/**/*.*',
+//         './src/layaAir/laya/media/**/*.*',
+//         './src/layaAir/laya/net/**/*.*',
+//         './src/layaAir/laya/renders/**/*.*',
+//         './src/layaAir/laya/resource/**/*.*',
+//         './src/layaAir/laya/system/**/*.*',
+//         './src/layaAir/laya/utils/**/*.*',
+//         './src/layaAir/laya/webgl/**/*.*',
+//         './src/layaAir/laya/effect/**/*.*'
+//         ],
+//         "./src/layaAir/tsconfig.json",
+//         './build/js/libs/laya.core.js',
+//         'window.Laya=window.Laya||{};',
+//         'exports.static=_static;'
+//         )
+//     });
 
-gulp.task('buildUI',async ()=>{
-    await bundleLib([
-        './src/layaAir/laya/ui/**/*.*',
-        './src/layaAir/UIConfig.ts',
-        ],
-        "./src/layaAir/tsconfig.json",
-        './build/js/libs/laya.ui.js',
-        '',
-        ''
-    );    
-});
+// gulp.task('buildUI',async ()=>{
+//     await bundleLib([
+//         './src/layaAir/laya/ui/**/*.*',
+//         './src/layaAir/UIConfig.ts',
+//         ],
+//         "./src/layaAir/tsconfig.json",
+//         './build/js/libs/laya.ui.js',
+//         '',
+//         ''
+//     );    
+// });
 
-//gulp.task('BuildLayaAir', gulp.series('tsc', 'CopyNoneTSFile'));
+gulp.task('BuildLayaAir', gulp.series('tsc', 'CopyNoneTSFile'));
