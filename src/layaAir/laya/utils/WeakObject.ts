54c278da1fa6ddde1ced1e03df7dc9bc0a368f63
guozhaokui <guozhaokuiwang@gmail.com>
2019-10-22 19:39:28 +0800

    don't use weakmap

--- src/layaAir/laya/utils/WeakObject.ts
+++ src/layaAir/laya/utils/WeakObject.ts
@@ -1,22 +1,20 @@
-import { Browser } from "./Browser";
 import { Utils } from "./Utils";
 import { ILaya } from "../../ILaya";
 
-var supportWeakMap = !!WeakMap;
 /**
 	 * 封装弱引用WeakMap
 	 * 如果支持WeakMap，则使用WeakMap，如果不支持，则用Object代替
 	 * 注意：如果采用Object，为了防止内存泄漏，则采用定时清理缓存策略
+	 * 
+	 * 这里的设计是错误的，为了兼容，先不删掉这个类，直接采用Object
 	 */
 export class WeakObject {
     /**是否支持WeakMap*/
-    static supportWeakMap = supportWeakMap;
+    static supportWeakMap = false;
     /**如果不支持WeakMap，则多少时间清理一次缓存，默认10分钟清理一次*/
-    static delInterval: number = 10 * 60 * 1000;
+    static delInterval = 10 * 60 * 1000;
     /**全局WeakObject单例*/
     static I: WeakObject;
-    /**@private */
-    private static _keys: any = {};
     /**@private */
     private static _maps: any[] = [];
     /**@internal */
@@ -31,17 +29,16 @@ export class WeakObject {
     }
 
     /**清理缓存，回收内存*/
-    //TODO:coverage
     static clearCache(): void {
-        for (var i: number = 0, n: number = WeakObject._maps.length; i < n; i++) {
-            var obj: WeakObject = WeakObject._maps[i];
+        for (var i = 0, n = WeakObject._maps.length; i < n; i++) {
+            var obj = WeakObject._maps[i];
             obj._obj = {};
         }
     }
 
     constructor() {
-        this._obj = WeakObject.supportWeakMap ? new Browser.window.WeakMap() : {};
-        if (!WeakObject.supportWeakMap) WeakObject._maps.push(this);
+        this._obj = {};
+        WeakObject._maps.push(this);
     }
 
 	/**
@@ -52,12 +49,6 @@ export class WeakObject {
     set(key: any, value: any): void {
         if (key == null) return;
         if (WeakObject.supportWeakMap) {
-            var objKey: any = key;
-            if (typeof (key) == 'string' || typeof (key) == 'number') {
-                objKey = WeakObject._keys[key as any];
-                if (!objKey) objKey = WeakObject._keys[key as any] = { k: key };
-            }
-            this._obj.set(objKey, value);
         } else {
             if (typeof (key) == 'string' || typeof (key) == 'number') {
                 this._obj[key as any] = value;
@@ -75,9 +66,6 @@ export class WeakObject {
     get(key: any): any {
         if (key == null) return null;
         if (WeakObject.supportWeakMap) {
-            var objKey: any = (typeof (key) == 'string' || typeof (key) == 'number') ? WeakObject._keys[key as any] : key;
-            if (!objKey) return null;
-            return this._obj.get(objKey);
         } else {
             if (typeof (key) == 'string' || typeof (key) == 'number') return this._obj[key as any];
             return this._obj[key.$_GID];
@@ -87,13 +75,9 @@ export class WeakObject {
 	/**
 	 * 删除缓存
 	 */
-    //TODO:coverage
     del(key: any): void {
         if (key == null) return;
         if (WeakObject.supportWeakMap) {
-            var objKey: any = (typeof (key) == 'string' || typeof (key) == 'number') ? WeakObject._keys[key as any] : key;
-            if (!objKey) return;
-            this._obj.delete(objKey);
         } else {
             if (typeof (key) == 'string' || typeof (key) == 'number') delete this._obj[key as any];
             else delete this._obj[this._obj.$_GID];
@@ -103,12 +87,9 @@ export class WeakObject {
 	/**
 	 * 是否有缓存
 	 */
-    //TODO:coverage
     has(key: any): boolean {
         if (key == null) return false;
         if (WeakObject.supportWeakMap) {
-            var objKey: any = (typeof (key) == 'string' || typeof (key) == 'number') ? WeakObject._keys[key as any] : key;
-            return this._obj.has(objKey);
         } else {
             if (typeof (key) == 'string' || typeof (key) == 'number') return this._obj[key as any] != null;
             return this._obj[this._obj.$_GID] != null;
