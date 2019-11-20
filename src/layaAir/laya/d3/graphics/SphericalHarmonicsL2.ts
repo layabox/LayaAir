e161508ea4eebedb70335c8b0b7a223c232993f6
GuoLei1990 <gl3336563@163.com>
2019-11-20 20:26:46 +0800

    add new  function to sh.

--- src/layaAir/laya/d3/graphics/SphericalHarmonicsL2.ts
+++ src/layaAir/laya/d3/graphics/SphericalHarmonicsL2.ts
@@ -25,4 +25,30 @@ export class SphericalHarmonicsL2 {
     setCoefficient(i: number, j: number, coefficient: number): void {
         this._coefficients[i * 3 + j] = coefficient;
     }
+
+    /**
+     * 设置颜色通道的系数。
+     * @param i 通道索引，范围0到2。
+     * @param coefficient0 系数0
+     * @param coefficient1 系数1
+     * @param coefficient2 系数2
+     * @param coefficient3 系数3
+     * @param coefficient4 系数4
+     * @param coefficient5 系数5
+     * @param coefficient6 系数6
+     * @param coefficient7 系数7
+     * @param coefficient8 系数8
+     */
+    setCoefficients(i: number, coefficient0: number, coefficient1: number, coefficient2: number, coefficient3: number, coefficient4: number, coefficient5: number, coefficient6: number, coefficient7: number, coefficient8: number): void {
+        var offset: number = i * 3;
+        this._coefficients[offset] = coefficient0;
+        this._coefficients[++offset] = coefficient1;
+        this._coefficients[++offset] = coefficient2;
+        this._coefficients[++offset] = coefficient3;
+        this._coefficients[++offset] = coefficient4;
+        this._coefficients[++offset] = coefficient5;
+        this._coefficients[++offset] = coefficient6;
+        this._coefficients[++offset] = coefficient7;
+        this._coefficients[++offset] = coefficient8;
+    }
 }
