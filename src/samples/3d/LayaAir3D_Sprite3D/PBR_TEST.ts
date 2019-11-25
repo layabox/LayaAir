4178eb4b2f074e8797191dcbec018b28e4aafa3e
lirongf <619213582@qq.com>
2019-11-25 15:27:42 +0800

    change example

--- src/samples/3d/LayaAir3D_Sprite3D/PBR_TEST.ts
+++ src/samples/3d/LayaAir3D_Sprite3D/PBR_TEST.ts
@@ -152,8 +152,8 @@ export class PBR_TEST {
 			var sphere:MeshSprite3D = new MeshSprite3D(PrimitiveMesh.createSphere(1));
 			scene.addChild(sphere);
 			var PBR02:PBRStandardMaterial = new PBRStandardMaterial();
-			PBR02.metallic = 1.0;
-			PBR02.smoothness = 1.0;
+			PBR02.metallic = 0.0;
+			PBR02.smoothness = 0.0;
 			sphere.meshRenderer.sharedMaterial = PBR02;
 			// var SHAr: Vector4  = new Vector4(-0.04560062,0.03957067,0.1411899,0.5705163);
 			// var SHAg:Vector4 = new Vector4(-0.04186664,0.05843516,0.1915448,0.2173594);
@@ -163,13 +163,13 @@ export class PBR_TEST {
 			// var SHBb:Vector4 = new Vector4(2.462286E-09,6.675703E-11,-0.05573735,-7.070138E-11);
 			// var SHC: Vector4  = new Vector4(-0.1337408,0.003887157,0.06891572,1.0);
 			// PBR02.SetGIDiffuse(SHAr,SHAg,SHAb,SHBr,SHBg,SHBb,SHC);
-			// scene.ambientProbe.setCoefficients(0,0.5961902,0.03957067,0.1411899,-0.04560062,4.062612E-10, 4.996603E-09, 0.02567386,1.100793E-09,-0.1337408);
-			// scene.ambientProbe.setCoefficients(1,0.2638047,0.05843516,0.1915448,-0.04186664,1.033786E-09,2.861993E-09,0.04644532,-1.315754E-10,0.003887157);
-			// scene.ambientProbe.setCoefficients(2,0.1557601, -0.005364253, -0.02622023, 0.1074025,2.462286E-09, 6.675703E-11, -0.01857912, -7.070138E-11,0.06891572);
+			scene.ambientProbe.setCoefficients(0,0.5961902,0.03957067,0.1411899,-0.04560062,4.062612E-10,4.996603E-09,0.02567386,1.100793E-09,-0.1337408);
+			 scene.ambientProbe.setCoefficients(1,0.2638047,0.05843516,0.1915448,-0.04186664,1.033786E-09 ,2.861993E-09 ,0.04644532, -1.315754E-10,0.003887157);
+			 scene.ambientProbe.setCoefficients(2,0.1557601, -0.005364253, -0.02622023, 0.1074025,2.462286E-09, 6.675703E-11, -0.01857912, -7.070138E-11,0.06891572);
 				 			
-			 	scene.ambientProbe.setCoefficients(0,0.2105249,-0.007550218,-0.01213387,0.02468685,-0.01043628,-0.01621458,0.01429429,-0.03880488,0.01278264);
-			 	scene.ambientProbe.setCoefficients(1,0.1975697,-0.003520536,-0.03160046,0.03231646,-0.006845312,-0.01398246,0.0105745,-0.04098841,0.01005551);
-			 	scene.ambientProbe.setCoefficients(2,0.1783197,0.008783088,-0.02437624,0.0443121,0.0002688426,-0.01042652,0.01097564,-0.04568882,0.003249854);
+			 	//scene.ambientProbe.setCoefficients(0,0.2105249,-0.007550218,-0.01213387,0.02468685,-0.01043628,-0.01621458,0.01429429,-0.03880488,0.01278264);
+			 	//scene.ambientProbe.setCoefficients(1,0.1975697,-0.003520536,-0.03160046,0.03231646,-0.006845312,-0.01398246,0.0105745,-0.04098841,0.01005551);
+			 	//scene.ambientProbe.setCoefficients(2,0.1783197,0.008783088,-0.02437624,0.0443121,0.0002688426,-0.01042652,0.01097564,-0.04568882,0.003249854);
 				 PBR02.diffuseDefined();
 				 scene.reflectionProbe = this.reflectCubeMap;
 				 PBR02.enableReflection = true;
