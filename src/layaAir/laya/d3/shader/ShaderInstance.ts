18dbb3c01d351711e18a88229ad344f2a42b777a
GuoLei1990 <gl3336563@163.com>
2019-12-03 16:06:10 +0800

    remove debug code.

--- src/layaAir/laya/d3/shader/ShaderInstance.ts
+++ src/layaAir/laya/d3/shader/ShaderInstance.ts
@@ -93,7 +93,6 @@ export class ShaderInstance extends Resource {
 	private _create(): void {
 		var gl: WebGLRenderingContext = LayaGL.instance;
 		this._program = gl.createProgram();
-		console.log(this._ps);
 		this._vshader = this._createShader(gl, this._vs, gl.VERTEX_SHADER);
 		this._pshader = this._createShader(gl, this._ps, gl.FRAGMENT_SHADER);
 		gl.attachShader(this._program, this._vshader);
