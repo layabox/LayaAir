518a1436aedcb89ce58a3961813365b580de23b2
KylinLove <kylin_zdd@163.com>
2020-01-09 17:09:07 +0800

    2.5.0beta rebuild

--- build/js/libs/laya.core.js
+++ build/js/libs/laya.core.js
@@ -18334,7 +18334,7 @@ window.Laya= (function (exports) {
         send(url, data = null, method = "get", responseType = "text", headers = null) {
             this._responseType = responseType;
             this._data = null;
-            if (Browser.onVVMiniGame || Browser.onQGMiniGame || Browser.onQQMiniGame) {
+            if (Browser.onVVMiniGame || Browser.onQGMiniGame || Browser.onQQMiniGame || Browser.onAlipayMiniGame) {
                 url = encodeURI(url);
             }
             this._url = url;
