f7fa3e02d7aacec5317964e14f4c46426f2b50f9
lvfulong <775331175@qq.com>
2020-01-10 16:05:46 +0800

    CLOSE

--- src/layaAir/laya/renders/Render.ts
+++ src/layaAir/laya/renders/Render.ts
@@ -133,13 +133,13 @@ export class Render {
     if (Render.isConchApp) {
         Render.supportWebGLPlusCulling = false;
         Render.supportWebGLPlusAnimation = false;
-        Render.supportWebGLPlusRendering = true;
+        Render.supportWebGLPlusRendering = false;
     }
     else if((window as any).qq != null && (window as any).qq.webglPlus != null)
     {
         Render.supportWebGLPlusCulling = false;
         Render.supportWebGLPlusAnimation = false;
-        Render.supportWebGLPlusRendering = true;
+        Render.supportWebGLPlusRendering = false;
     }
 }
 
