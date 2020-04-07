7542891e7802941908e43b28696b50c420860ee7
KylinLove <kylin_zdd@163.com>
2020-04-07 12:04:01 +0800

    version: 2.6.0

--- src/layaAir/Laya.ts
+++ src/layaAir/Laya.ts
@@ -88,7 +88,7 @@ export class Laya {
 	/** 加载管理器的引用。*/
 	static loader: LoaderManager = null;
 	/** 当前引擎版本。*/
-	static version: string = "2.6.0beta";
+	static version: string = "2.6.0";
 	/**@private Render 类的引用。*/
 	static render: Render;
 	/**@internal */
