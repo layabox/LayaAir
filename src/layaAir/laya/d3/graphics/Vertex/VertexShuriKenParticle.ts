97be0bad1fc1687f73ee43659522928c3ab6ece0
lirongf <619213582@qq.com>
2020-05-26 21:01:41 +0800

    Solve the problem Safari Browser play shurikenPhysics bug

--- src/layaAir/laya/d3/graphics/Vertex/VertexShuriKenParticle.ts
+++ src/layaAir/laya/d3/graphics/Vertex/VertexShuriKenParticle.ts
@@ -5,12 +5,13 @@
  * @author ...
  */
 export class VertexShuriKenParticle {
-	static PARTICLE_CORNERTEXTURECOORDINATE0: number = 0;
+	//TODO Attribute
+	static PARTICLE_CORNERTEXTURECOORDINATE0: number = 5;
 	static PARTICLE_POSITION0: number = 1;
 	static PARTICLE_COLOR0: number = 2;
 	static PARTICLE_TEXTURECOORDINATE0: number = 3;
 	static PARTICLE_SHAPEPOSITIONSTARTLIFETIME: number = 4;
-	static PARTICLE_DIRECTIONTIME: number = 5;
+	static PARTICLE_DIRECTIONTIME: number = 0;
 	static PARTICLE_STARTCOLOR0: number = 6;
 	static PARTICLE_ENDCOLOR0: number = 7;
 	static PARTICLE_STARTSIZE: number = 8;
