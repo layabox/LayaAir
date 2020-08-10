820f3f5559899551b5f0f15550819a349f3e61f3
lirongf <619213582@qq.com>
2020-08-10 14:15:25 +0800

    修复sample index

--- src/samples/index.ts
+++ src/samples/index.ts
@@ -1,4 +1,2 @@
 import { Main } from "./Main";
-import { SimpleSkinAnimationInstance } from "./3d/LayaAir3D_Animation3D/SimpleSkinAnimationInstance";
-import { VideoPlayIn3DWorld } from "./3d/LayaAir3D_Advance/VideoPlayIn3DWorld";
-new SimpleSkinAnimationInstance(); 
+new Main(); 
