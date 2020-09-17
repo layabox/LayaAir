77f0b64a6b12b1d592f7488c2b9a5ec9aed3e12f
lirongf <619213582@qq.com>
2020-09-17 17:14:45 +0800

    index 恢复

--- src/samples/index.ts
+++ src/samples/index.ts
@@ -1,5 +1,2 @@
-import { BlurEffect } from "./3d/LayaAir3D_PostProcess/BlurShader/BlurEffect";
-import { PostProcess_Blur } from "./3d/LayaAir3D_PostProcess/PostProcess_Blur";
-import { LayaBuilder } from "./3d/LayaAirIDE/LayaBuilder";
 import { Main } from "./Main";
-new PostProcess_Blur(); 
+new Main(); 
