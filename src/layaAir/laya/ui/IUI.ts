c3bbb92b16e85831607f0be01f548350b7428392
guozhaokui <guozhaokuiwang@gmail.com>
2020-12-17 14:41:10 +0800

    部分编译错误

--- src/layaAir/laya/ui/IUI.ts
+++ src/layaAir/laya/ui/IUI.ts
@@ -1,9 +1,8 @@
-import { DialogManager } from "./DialogManager";
 import { Dialog } from "./Dialog";
 /**
  * @internal
  */
 export class IUI{
     //static DialogManager:typeof DialogManager=null;
-    static Dialog:typeof Dialog=null;
+    static Dialog:typeof Dialog;
 }
