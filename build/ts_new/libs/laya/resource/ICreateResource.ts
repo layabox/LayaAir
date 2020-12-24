1c2857de0aa70b7c4985956f020f8311dd6446f0
lirongf <619213582@qq.com>
2020-12-24 19:22:31 +0800

    删除build

--- build/ts_new/libs/laya/resource/ICreateResource.ts
+++
@@ -1,6 +0,0 @@
-/**
- * <code>ICreateResource</code> 对象创建接口。
- */
-export interface ICreateResource {
-    _setCreateURL(url: string): void;
-}
--- build/ts_new/libs/laya/resource/ISingletonElement.ts
+++
@@ -1,7 +0,0 @@
-/**
- * <code>IList</code> 可加入队列接口。
- */
-export interface ISingletonElement {
-    _getIndexInList(): number;
-    _setIndexInList(index: number): void;
-}
