7359051ca83954ab6afe2b211eb0a2593e3bfbf1
Lurenyiabc <934555872@qq.com>
2021-01-05 11:29:38 +0800

    提交input变更导致的类型变更

--- src/layaAir/laya/display/Stage.ts
+++ src/layaAir/laya/display/Stage.ts
@@ -199,7 +199,7 @@ export class Stage extends Sprite {
 			this._isFocused = false;
 			this.event(Event.BLUR);
 			this.event(Event.FOCUS_CHANGE);
-			if (this._isInputting()) Input["inputElement"].target.focus = false;
+			if (this._isInputting()) (Input["inputElement"] as any).target.focus = false;
 		});
 
 		// 各种浏览器兼容
@@ -222,7 +222,7 @@ export class Stage extends Sprite {
 		window.document.addEventListener(visibilityChange, ()=> {
 			if (Browser.document[state] == "hidden") {
 				this._isVisibility = false;
-				if (this._isInputting()) Input["inputElement"].target.focus = false;
+				if (this._isInputting()) (Input["inputElement"] as any).target.focus = false;
 			} else {
 				this._isVisibility = true;
 			}
@@ -233,7 +233,7 @@ export class Stage extends Sprite {
 			// 处理屏幕旋转。旋转后收起输入法。
 			var orientation: any = Browser.window.orientation;
 			if (orientation != null && orientation != this._previousOrientation && this._isInputting()) {
-				Input["inputElement"].target.focus = false;
+				(Input["inputElement"] as any).target.focus = false;
 			}
 			this._previousOrientation = orientation;
 
