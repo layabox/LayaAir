17e4251b6eb4244a482a355b225c0d71b23bae8d
lirongf <619213582@qq.com>
2021-01-11 12:03:40 +0800

    修改默认值

--- src/layaAir/Config.ts
+++ src/layaAir/Config.ts
@@ -45,7 +45,7 @@ export class Config {
     /**
      * 是否打印Webgl指令，同时定位webgl报错
      */
-    static printWebglOrder:boolean = true;
+    static printWebglOrder:boolean = false;
     
     /** 是否允许GPUInstance动态合并,仅对3D有效。*/
 	static allowGPUInstanceDynamicBatch: boolean = true;
