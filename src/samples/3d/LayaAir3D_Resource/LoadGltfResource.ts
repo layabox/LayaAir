08dbe1ea005464a98023f534b75e6538e800eb89
QTEvD <agp62601@gmail.com>
2021-01-20 14:19:52 +0800

    更新gltf加载示例

--- src/samples/3d/LayaAir3D_Resource/LoadGltfResource.ts
+++ src/samples/3d/LayaAir3D_Resource/LoadGltfResource.ts
@@ -9,12 +9,11 @@ import { Camera } from "laya/d3/core/Camera";
 import { Vector3 } from "laya/d3/math/Vector3";
 import { DirectionLight } from "laya/d3/core/light/DirectionLight";
 import { Matrix4x4 } from "laya/d3/math/Matrix4x4";
-import { GLTFLoader } from "laya/gltf/GLTFLoader";
 import { Sprite3D } from "laya/d3/core/Sprite3D";
 import { CameraMoveScript } from "../common/CameraMoveScript";
 import { Loader } from "laya/net/Loader";
 import { TextureCube } from "laya/d3/resource/TextureCube";
-import { Quaternion } from "laya/d3/math/Quaternion";
+import { glTFLoader } from "laya/gltf/glTFLoader";
 
 export class LoadGltfRosource {
 
@@ -35,7 +34,6 @@ export class LoadGltfRosource {
         this.camera.addComponent(CameraMoveScript);
 
         this.camera.transform.position = new Vector3(0, 1, 7);
-        // this.camera.transform.rotation = new Quaternion();
 
         //light
         var directionLight: DirectionLight = (<DirectionLight>this.scene.addChild(new DirectionLight()));
@@ -51,17 +49,18 @@ export class LoadGltfRosource {
             this.scene.reflection = Loader.getRes("res/threeDimen/LayaScene_depthNormalScene/Conventional/Assets/Scenes/depthNormalSceneGIReflection.ltcb.ls") as TextureCube;
             this.scene.reflectionDecodingFormat = 1;
             this.scene.reflectionIntensity = 1;
-        }))
+        }));
+
+        // 初始化 glTFLoader
+        glTFLoader.init();
 
         var gltfResource = [
-            { url: "res/threeDimen/gltf/RiggedFigure/RiggedFigure.gltf", type: Loader.JSON },
-            { url: "res/threeDimen/gltf/Duck/Duck.gltf", type: Loader.JSON },
-            { url: "res/threeDimen/gltf/AnimatedCube/AnimatedCube.gltf", type: Loader.JSON },
+            "res/threeDimen/gltf/RiggedFigure/RiggedFigure.gltf",
+            "res/threeDimen/gltf/Duck/Duck.gltf",
+            "res/threeDimen/gltf/AnimatedCube/AnimatedCube.gltf"
         ];
-        
-        // 创建 gltf loader
-        var gltfLoader: GLTFLoader = new GLTFLoader();
-        gltfLoader.loadGLTF(gltfResource, Handler.create(this, this.onGLTFComplate));
+
+        Laya.loader.create(gltfResource, Handler.create(this, this.onGLTFComplate));
     }
 
     onGLTFComplate(success: boolean): void {
@@ -70,15 +69,15 @@ export class LoadGltfRosource {
             console.log("gltf load failed");
             return;
         }
-        var RiggedFigure: Sprite3D = GLTFLoader.getRes("res/threeDimen/gltf/RiggedFigure/RiggedFigure.gltf");
+        var RiggedFigure: Sprite3D = Loader.getRes("res/threeDimen/gltf/RiggedFigure/RiggedFigure.gltf");
         this.scene.addChild(RiggedFigure);
         RiggedFigure.transform.position = new Vector3(-2, 0, 0);
         console.log("RiggedFigure: This model is licensed under a Creative Commons Attribution 4.0 International License.");
 
-        var duck: Sprite3D = GLTFLoader.getRes("res/threeDimen/gltf/Duck/Duck.gltf");
+        var duck: Sprite3D = Loader.getRes("res/threeDimen/gltf/Duck/Duck.gltf");
         this.scene.addChild(duck);
 
-        var cube: Sprite3D = GLTFLoader.getRes("res/threeDimen/gltf/AnimatedCube/AnimatedCube.gltf");
+        var cube: Sprite3D = Loader.getRes("res/threeDimen/gltf/AnimatedCube/AnimatedCube.gltf");
         this.scene.addChild(cube);
         cube.transform.position = new Vector3(2.5, 0.6, 0);
         cube.transform.setWorldLossyScale(new Vector3(0.6, 0.6, 0.6));
