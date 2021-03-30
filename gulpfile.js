eab395b08e2aacc78fe9625d65904b0d694c57b5
Lurenyiabc <934555872@qq.com>
2021-03-30 20:36:44 +0800

    execFile改为exec

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 const gulp = require('gulp');
-const { execFile } = require('child_process');
+const { exec } = require('child_process');
 const path = require("path");
 gulp.task('tsc', (cb) => {
-    let cmd = ["-b","src/samples/tsconfig.json"];
-    let cmddir = path.join(__dirname,"./node_modules/.bin/tsc.cmd");
-    execFile(cmddir,cmd,(err,stdout,stderr)=>{
+    let args = ["-b","src/samples/tsconfig.json"];
+    let cmddir = path.join(__dirname,"./node_modules/.bin/tsc");
+    let cmd = `${cmddir} ${args[0]} ${args[1]}`
+    exec(cmd,(err,stdout,stderr)=>{
         if(err){
             console.log(err,'\n',stdout,'\n',stderr);
         }
