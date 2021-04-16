0d2d7cff151684a934d8f509e50e3cd09a0bb864
QTEvD <agp62601@gmail.com>
2021-04-16 17:22:35 +0800

    change scene

--- src/samples/3d/LayaAir3D_PostProcess/PostProcess_DoF.ts
+++ src/samples/3d/LayaAir3D_PostProcess/PostProcess_DoF.ts
@@ -16,6 +16,8 @@ import { DepthTextureMode } from "laya/d3/depthMap/DepthPass";
 import { Mesh } from "laya/d3/resource/models/Mesh";
 import { PBRStandardMaterial } from "laya/d3/core/material/PBRStandardMaterial";
 import { GaussianDoF } from "./PostProcess_DoF/GaussianDoF";
+import { Handler } from "laya/utils/Handler";
+import { Loader } from "laya/net/Loader";
 
 export class PostProcessDoF {
 
@@ -31,70 +33,29 @@ export class PostProcessDoF {
 
         Shader3D.debugMode = true;
 
-        let scene: Scene3D = this.scene = new Scene3D();
-        Laya.stage.addChild(scene);
-        let directionLight: DirectionLight = new DirectionLight();
-        scene.addChild(directionLight);
-        directionLight.transform.rotationEuler = new Vector3(-50, -150, 0);
-        let camera: Camera = this.camera = new Camera(0, 0.3, 100);
-        camera.enableHDR = true;
-        camera.transform.position = new Vector3(0, 1, -11);
-        camera.transform.rotationEuler = new Vector3(0, -180, 0);
-        camera.addComponent(CameraMoveScript);
-        scene.addChild(camera);
-
-        this.onComplate();
+        Laya.loader.create("res/threeDimen/LayaScene_zhuandibanben/Conventional/zhuandibanben.ls", Handler.create(this, this.onComplate));
 
     }
 
     onComplate(): void {
 
-        let scene: Scene3D = this.scene;
-        let camera: Camera = this.camera;
-
+        let scene: Scene3D = this.scene = Loader.getRes("res/threeDimen/LayaScene_zhuandibanben/Conventional/zhuandibanben.ls");
+        Laya.stage.addChild(scene);
+        let camera: Camera = this.camera = <Camera>scene.getChildByName("MainCamera");
+        camera.addComponent(CameraMoveScript);
+        let mainCamera = scene.getChildByName("BlurCamera");
+        mainCamera.removeSelf();
         camera.depthTextureMode |= DepthTextureMode.Depth;
-
-        this.buildTestScene(this.scene, this.camera);
 
         let postProcess: PostProcess = new PostProcess();
         camera.postProcess = postProcess;
 
         let gaussianDoF: GaussianDoF = new GaussianDoF();
+        console.log(gaussianDoF);
+
         postProcess.addEffect(gaussianDoF);
-        gaussianDoF.farStart = 10;
-        gaussianDoF.farEnd = 20;
+        gaussianDoF.farStart = 1;
+        gaussianDoF.farEnd = 5;
         gaussianDoF.maxRadius = 1.0;
     }
-
-    buildTestScene(scene: Scene3D, camera: Camera) {
-
-        let test: Sprite3D = new Sprite3D();
-        scene.addChild(test);
-
-        let boxMesh: Mesh = PrimitiveMesh.createBox();
-        let sphereMesh: Mesh = PrimitiveMesh.createSphere();
-        let capsuleMesh: Mesh = PrimitiveMesh.createCapsule();
-        let ConeMesh: Mesh = PrimitiveMesh.createCone();
-
-        let meshes: Mesh[] = [boxMesh, sphereMesh, capsuleMesh, ConeMesh];
-
-        let material: PBRStandardMaterial = new PBRStandardMaterial();
-
-        let range: number = 30;
-        let step: number = 4;
-
-        for (let x = -range; x <= range; x += step) {
-            for (let y = -range; y <= range; y += step) {
-                for (let z = -range; z <= range; z += step) {
-                    let position: Vector3 = new Vector3(x, y, z);
-                    let mesh: Mesh = meshes[Math.floor(Math.random() * 4)];
-                    let sprite: MeshSprite3D = new MeshSprite3D(mesh);
-                    test.addChild(sprite);
-                    sprite.transform.position = position;
-                    sprite.meshRenderer.sharedMaterial = material;
-                }
-            }
-        }
-    }
-
 }
