e9d8299e50652f1089a7b1b499944c1280930d98
wangsong <wangsong@layabox.com>
2021-07-23 12:40:21 +0800

    修复相对布局对象自身宽高为0的bug； 修复九宫格设置错乱显示的bug

--- src/layaAir/laya/resource/Context.ts
+++ src/layaAir/laya/resource/Context.ts
@@ -2510,29 +2510,40 @@ export class Context {
 		var d_bottom: number = bottom / h;
 
 		//处理进度条不好看的问题
-		if (left + right > width) {
-			var clipWidth: number = width;
-			needClip = true;
-			width = left + right;
-			this.save();
-			this.clipRect(0 + tx, 0 + ty, clipWidth, height);
-		}
+		// if (left + right > width) {
+		// 	var clipWidth: number = width;
+		// 	needClip = true;
+		// 	width = left + right;
+		// 	this.save();
+		// 	this.clipRect(0 + tx, 0 + ty, clipWidth, height);
+		// }
 
 		var imgid: number = (tex.bitmap as Texture2D).id;
 		var mat: Matrix = this._curMat;
 		var tuv = this._tempUV;
+
+		//解决九宫格设置left+right或top+bottom的累加值超过宽或高导致九宫格显示错乱的bug
+		var scale_x = 1;
+		var scale_y = 1;
+		if(left + right > width) scale_x = width / (left + right);
+		if(top + bottom > height) scale_y = height / (top +bottom);
+		left *= scale_x;
+		right *= scale_x;
+		top *= scale_y;
+		bottom *= scale_y;
+
 		// 整图的uv
 		// 一定是方的，所以uv只要左上右下就行
-		var uvl: number = uv[0];
-		var uvt: number = uv[1];
-		var uvr: number = uv[4];
-		var uvb: number = uv[5];
+		var uvl = uv[0];
+		var uvt = uv[1];
+		var uvr = uv[4];
+		var uvb = uv[5];
 
 		// 小图的uv
-		var uvl_: number = uvl;
-		var uvt_: number = uvt;
-		var uvr_: number = uvr;
-		var uvb_: number = uvb;
+		var uvl_ = uvl;
+		var uvt_ = uvt;
+		var uvr_ = uvr;
+		var uvb_ = uvb;
 
 		//绘制四个角
 		// 构造uv
