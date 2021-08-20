12626167f212ae25896a72ea513042c7631e99e8
lirongf <619213582@qq.com>
2021-08-20 10:57:06 +0800

    ｓｓｓ

--- src/samples/index.ts
+++ src/samples/index.ts
@@ -1,2 +1,6 @@
+import { CommandBuffer_DrawCustomInstance } from "./3d/LayaAir3D_Advance/CommandBuffer_DrawCustomInstance";
+import { CommandBuffer_Outline } from "./3d/LayaAir3D_Advance/CommandBuffer_Outline";
+import { AnimatorDemo } from "./3d/LayaAir3D_Animation3D/AnimatorDemo";
+import { SceneLoad1 } from "./3d/LayaAir3D_Scene3D/SceneLoad1";
 import { Main } from "./Main";
-new Main();
+new SceneLoad1();
