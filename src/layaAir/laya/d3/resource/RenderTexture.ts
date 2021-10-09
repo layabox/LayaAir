c7bd037df65b43fb36e77bcc8555df9f31c145b8
lirongf <619213582@qq.com>
2021-10-09 18:05:01 +0800

    修改Rendertexture的一些问题

--- src/layaAir/laya/d3/resource/RenderTexture.ts
+++ src/layaAir/laya/d3/resource/RenderTexture.ts
@@ -281,9 +281,11 @@ export class RenderTexture extends BaseTexture {
 				default:
 					throw "RenderTexture: depth format RenderTexture must use depthFormat with DEPTH_16 and DEPTHSTENCIL_16_8.";
 			}
-			if (isWebGL2 && format == RenderTextureFormat.ShadowMap)
+			if (isWebGL2 && format == RenderTextureFormat.ShadowMap){
 				gl2.texParameteri(glTextureType, gl2.TEXTURE_COMPARE_MODE, gl2.COMPARE_REF_TO_TEXTURE);
 				gl2.texParameteri(glTextureType, gl2.TEXTURE_COMPARE_FUNC, gl2.LESS);
+			}
+				
 		}
 	}
 
