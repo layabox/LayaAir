3b812b532217eae298225e680d8f593ee9f29146
wangsong <wangsong@layabox.com>
2022-02-28 10:46:49 +0800

    scrollbar增加下拉拖到停顿方法以及下拉定位值

--- src/layaAir/laya/ui/ScrollBar.ts
+++ src/layaAir/laya/ui/ScrollBar.ts
@@ -56,7 +56,8 @@ export class ScrollBar extends UIComponent {
     downButton: Button;
     /**滑条 */
     slider: Slider;
-
+    /**顶部移动上限（配合stopMoveLimit方法做滑动到底部抛出一个dragTopLimit事件，让开发者做一些动态数据更新的操作）*/
+    topMoveLimit:number = 0;
     /**@private */
     protected _showButtons: boolean = UIConfig.showButtons;
     /**@private */
@@ -499,7 +500,7 @@ export class ScrollBar extends UIComponent {
 
     triggerDownDragLimit: Function;
     triggerUpDragLimit: Function;
-
+    stopMoveLimit:Function;//暂停滚动重载方法-addxiaosong
     private checkTriggers(isTweenMove: boolean = false): boolean {
         if (this.value >= 0 && this.value - this._lastOffset <= 0) {
             if ((this.triggerDownDragLimit) && this.triggerDownDragLimit(isTweenMove)) {
@@ -528,6 +529,8 @@ export class ScrollBar extends UIComponent {
     }
     /**@private */
     protected loop(): void {
+        if(this.stopMoveLimit && this.stopMoveLimit())
+			return;
         var mouseY: number = ILaya.stage.mouseY;
         var mouseX: number = ILaya.stage.mouseX;
         this._lastOffset = this.isVertical ? (mouseY - this._lastPoint.y) : (mouseX - this._lastPoint.x);
@@ -608,7 +611,11 @@ export class ScrollBar extends UIComponent {
 
         if (this._isElastic) {
             if (this._value < this.min) {
-                Tween.to(this, { value: this.min }, this.elasticBackTime, Ease.sineOut, Handler.create(this, this.elasticOver));
+                this.event("dragTopLimit");
+                var moveValue:number = this.min;
+                if(this.stopMoveLimit && this.stopMoveLimit())
+                    moveValue = this.topMoveLimit;//add xiaosong
+                Tween.to(this, { value: moveValue }, this.elasticBackTime, Ease.sineOut, Handler.create(this, this.elasticOver));
             } else if (this._value > this.max) {
                 Tween.to(this, { value: this.max }, this.elasticBackTime, Ease.sineOut, Handler.create(this, this.elasticOver));
             }
