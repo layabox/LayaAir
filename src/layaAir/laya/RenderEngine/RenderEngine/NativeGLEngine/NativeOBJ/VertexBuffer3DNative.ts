50ffb45a35c86193422f93c56c1a894fc12818ce
lirongf <619213582@qq.com>
2022-04-07 14:54:14 +0800

    补充提交

---
+++ src/layaAir/laya/RenderEngine/RenderEngine/NativeGLEngine/NativeOBJ/VertexBuffer3DNative.ts
@@ -0,0 +1,43 @@
+import { VertexBuffer3D } from "../../../../d3/graphics/VertexBuffer3D";
+import { BufferUsage } from "../../../RenderEnum/BufferTargetType";
+import { VertexDeclaration } from "../../../VertexDeclaration";
+
+export class VertexBuffer3DNative extends VertexBuffer3D{
+    /**
+	 * 获取顶点声明。
+	 */
+	get vertexDeclaration(): VertexDeclaration | null {
+        return this._vertexDeclaration;
+	}
+
+	set vertexDeclaration(value: VertexDeclaration | null) {
+        this.serilizeVertexDeclaration(value);
+		this._vertexDeclaration = value;
+	}
+
+    serilizeVertexDeclaration(value:VertexDeclaration):Int32Array{
+        let array =new Int32Array( value._vertexElements.length*6);
+        let offset = 0;
+        var valueData: any = value._shaderValues;
+        for (var k in valueData) {
+            var loc: number = parseInt(k);
+            var attribute: Int32Array = valueData[k];
+            array[offset++] = loc;
+            array.set(attribute,offset);
+            offset+=5;
+        }
+        return array;
+    }
+
+
+    /**
+	 * 创建一个 <code>VertexBuffer3D</code> 实例。
+	 * @param	byteLength 字节长度。
+	 * @param	bufferUsage VertexBuffer3D用途类型。
+	 * @param	canRead 是否可读。
+	 */
+	constructor(byteLength: number, bufferUsage: BufferUsage, canRead: boolean = false) {
+        super(byteLength,bufferUsage,canRead);
+        //Native init
+    }
+}
