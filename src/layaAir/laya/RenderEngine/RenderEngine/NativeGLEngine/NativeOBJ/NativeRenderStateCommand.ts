a026e99f84da10a909c72b5d8a4615c9260b1a6d
lvfulong <775331175@qq.com>
2022-04-26 16:54:26 +0800

    NativeRenderStateCommand

---
+++ src/layaAir/laya/RenderEngine/RenderEngine/NativeGLEngine/NativeOBJ/NativeRenderStateCommand.ts
@@ -0,0 +1,53 @@
+import { LayaGL } from "../../../../layagl/LayaGL";
+import { BlendType } from "../../../RenderEnum/BlendType";
+import { RenderStateType } from "../../../RenderEnum/RenderStateType";
+
+/**
+ * 渲染状态设置命令流
+ */
+export class NativeRenderStateCommand{
+    private _nativeObj: any;
+    constructor(){
+        this._nativeObj = new (window as any).conchRenderStateCommand();
+    }
+
+    addCMD(renderstate:RenderStateType,value:any){
+        switch (renderstate) {
+            case RenderStateType.DepthTest:
+            case RenderStateType.DepthMask:
+            case RenderStateType.DepthFunc:
+            case RenderStateType.StencilTest:
+            case RenderStateType.StencilMask:
+            case RenderStateType.BlendEquation:
+            case RenderStateType.CullFace:
+            case RenderStateType.FrontFace:
+                this._nativeObj.addCMDInt1(value);
+                break;
+            case RenderStateType.StencilFunc:         
+            case RenderStateType.BlendFunc:            
+            case RenderStateType.BlendEquationSeparate:
+                this._nativeObj.addCMDInt2(value[0], value[1]);
+                break;
+            case RenderStateType.StencilOp:
+                this._nativeObj.addCMDInt3(value[0],value[1], value[2]);//TODO
+                break;
+            case RenderStateType.BlendType:
+                this._nativeObj.addCMDInt1(value != BlendType.BLEND_DISABLE ? 1 : 0);
+                break;
+            case RenderStateType.BlendFuncSeperate:
+                this._nativeObj.addCMDInt4(value[0], value[1], value[2], value[3]);
+                break;
+            default:
+                throw "unknow type of renderStateType";
+                break;
+        }
+    }
+
+    applyCMD(){
+        this._nativeObj.applyRenderStateCMD();
+    }
+
+    clear(){
+        this._nativeObj.clear();
+    }
+}
