36357c4df149b27fb1bfb308cc4916f4696add20
lvfulong <775331175@qq.com>
2022-05-03 16:28:15 +0800

    NativeCameraCullInfo

--- src/layaAir/laya/RenderEngine/RenderEngine/NativeGLEngine/NativeOBJ/NativeCameraCullInfo.ts
+++ src/layaAir/laya/RenderEngine/RenderEngine/NativeGLEngine/NativeOBJ/NativeCameraCullInfo.ts
@@ -7,11 +7,46 @@ import { NativeBoundFrustum } from "./NativeBoundFrustum";
  */
  export class NativeCameraCullInfo implements ICameraCullInfo{
 	/**位置 */
-	position: Vector3;
-	/**是否遮挡剔除 */
-	useOcclusionCulling: Boolean;
-	/**锥体包围盒 */
-	boundFrustum: NativeBoundFrustum;
-	/**遮挡标记 */
-	cullingMask: number;
+	private _position: Vector3;
+
+	private _boundFrustum: NativeBoundFrustum;
+
+	private _nativeObj: any;
+	
+	constructor() {
+    	this._nativeObj = new (window as any).conchCameraCullInfo();
+  	}
+	set position(position: Vector3) {
+		this._position = position;
+		this._nativeObj.setPosition(position.x, position.y, position.z);
+	} 
+
+	get position(): Vector3 {
+		return this._position;
+	}
+
+	set useOcclusionCulling(useOcclusionCulling: Boolean) {
+		this._nativeObj.useOcclusionCulling = useOcclusionCulling;
+	}
+
+	get useOcclusionCulling(): Boolean {
+		return this._nativeObj.useOcclusionCulling;
+	}
+
+	set boundFrustum(boundFrustum: NativeBoundFrustum) {
+		this._boundFrustum = boundFrustum;
+		this._nativeObj.boundFrustum = boundFrustum._nativeObj;
+	}
+
+	get boundFrustum(): NativeBoundFrustum {
+		return this._boundFrustum;
+	}
+
+	set cullingMask(cullingMask: number) {
+		this._nativeObj.cullingMask = cullingMask;
+	}
+
+	get cullingMask(): number {
+		return this._nativeObj.cullingMask;
+	}
 }
