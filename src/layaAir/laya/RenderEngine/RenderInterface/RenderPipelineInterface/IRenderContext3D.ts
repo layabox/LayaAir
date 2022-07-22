43a06963ba78083f20c2dc99e6855c0ecd9612d7
QTEvD <agp62601@gmail.com>
2022-07-22 15:03:31 +0800

    pipeline mode

--- src/layaAir/laya/RenderEngine/RenderInterface/RenderPipelineInterface/IRenderContext3D.ts
+++ src/layaAir/laya/RenderEngine/RenderInterface/RenderPipelineInterface/IRenderContext3D.ts
@@ -4,30 +4,31 @@ import { ShaderData } from "../../RenderShader/ShaderData";
 import { IRenderTarget } from "../IRenderTarget";
 import { IRenderElement } from "./IRenderElement";
 
-  export interface IRenderContext3D{
-    //dest Texture
-    destTarget:IRenderTarget;
-    //viewPort
-    viewPort:Viewport;
-    //scissor
-    scissor:Vector4;
-    //is invert Y
-    invertY:boolean;
-    //pipeLineMode
-    pipelineMode:string;
-    //Camera Shader Data
-    cameraShaderData:ShaderData;
-    //Scene cache
-    sceneID:number;
-    //scene Shader Data
-    sceneShaderData:ShaderData;
-    //Camera Update Mark
-    cameraUpdateMark:number;
-    //Global ShaderData
-    globalShaderData:ShaderData;
-    /**设置IRenderContext */
-    applyContext(cameraUpdateMark:number):void;
-    /**draw one element by context */
-    drawRenderElement(renderelemt:IRenderElement):void;
-  }
-  
+export type PipelineMode = "Forward" | "ShadowCaster" | "DepthNormal" | string;
+
+export interface IRenderContext3D {
+	//dest Texture
+	destTarget: IRenderTarget;
+	//viewPort
+	viewPort: Viewport;
+	//scissor
+	scissor: Vector4;
+	//is invert Y
+	invertY: boolean;
+	//pipeLineMode
+	pipelineMode: PipelineMode;
+	//Camera Shader Data
+	cameraShaderData: ShaderData;
+	//Scene cache
+	sceneID: number;
+	//scene Shader Data
+	sceneShaderData: ShaderData;
+	//Camera Update Mark
+	cameraUpdateMark: number;
+	//Global ShaderData
+	globalShaderData: ShaderData;
+	/**设置IRenderContext */
+	applyContext(cameraUpdateMark: number): void;
+	/**draw one element by context */
+	drawRenderElement(renderelemt: IRenderElement): void;
+}
