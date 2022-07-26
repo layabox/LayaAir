6b6415465ed63bb063f5ae83bdadc7792d78b288
lvfulong <775331175@qq.com>
2022-07-26 17:29:49 +0800

    NativeUniformBufferObject

--- src/layaAir/laya/RenderEngine/RenderEngine/NativeGLEngine/NativeOBJ/NativeUniformBufferObject.ts
+++ src/layaAir/laya/RenderEngine/RenderEngine/NativeGLEngine/NativeOBJ/NativeUniformBufferObject.ts
@@ -1,5 +1,13 @@
+import { LayaGL } from "../../../../layagl/LayaGL";
+import { BufferUsage } from "../../../RenderEnum/BufferTargetType";
 import { UniformBufferObject } from "../../../UniformBufferObject";
 
 export class NativeUniformBufferObject extends UniformBufferObject{
-    //LV TODO
+    _conchUniformBufferObject:any = null;
+
+    constructor(glPointer: number, name: string, bufferUsage: BufferUsage, byteLength: number, isSingle: boolean) {
+        super(glPointer, name, bufferUsage, byteLength, isSingle);
+        this._conchUniformBufferObject = new (window as any).conchUniformBufferObject((LayaGL.renderEngine as any)._nativeObj, glPointer);
+        this._conchUniformBufferObject.setGLBuffer(this._glBuffer);
+    }
 }
