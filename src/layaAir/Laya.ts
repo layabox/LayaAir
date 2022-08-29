0164ce33ba4a5f02cb5f1bd53c31b68b34737d0d
liyan-debug <1121362394@qq.com>
2022-08-29 10:36:14 +0800

    修改版本号

--- src/layaAir/Laya.ts
+++ src/layaAir/Laya.ts
@@ -88,7 +88,7 @@ export class Laya {
 	static loader: LoaderManager = null;
 	/** 当前引擎版本。*/
 
-	static version: string = "2.13.1beta";
+	static version: string = "2.13.1";
 
 	/**@private Render 类的引用。*/
 	static render: Render;
