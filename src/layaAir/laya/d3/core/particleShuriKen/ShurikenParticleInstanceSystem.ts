b06281cdaaa0d2d47d57f626d9125eb5f292d49e
LayaCharley <399050@qq.com>
2022-09-02 19:10:52 +0800

    修复BUG：修复粒子内存的显示错误

--- src/layaAir/laya/d3/core/particleShuriKen/ShurikenParticleInstanceSystem.ts
+++ src/layaAir/laya/d3/core/particleShuriKen/ShurikenParticleInstanceSystem.ts
@@ -539,17 +539,21 @@ export class ShurikenParticleInstanceSystem extends ShurikenParticleSystem {
     destroy(): void {
         // todo
         super.destroy();
-
+        let memorySize = 0;
         if (this._indexBuffer) {
+            memorySize += this._indexBuffer._byteLength;
             this._indexBuffer.destroy();
         }
         if (this._vertexBuffer) {
+            memorySize +=  this._vertexBuffer._byteLength;
             this._vertexBuffer.destroy();
         }
         if (this._instanceParticleVertexBuffer) {
+            this._instanceParticleVertexBuffer._byteLength;
             this._instanceParticleVertexBuffer.destroy();
         }
-
+        
+        Resource._addMemory(-memorySize,-memorySize);
         this._instanceVertex = null;
         this._meshIndexCount = null;
         this._meshFloatCountPreVertex = null;
