c0398c98359d40e7cab145a26bad5d7fe64d47ae
lirongf <619213582@qq.com>
2022-10-19 19:42:52 +0800

    还原IDE闪屏的bug

--- src/layaAir/laya/display/Sprite.ts
+++ src/layaAir/laya/display/Sprite.ts
@@ -1291,7 +1291,8 @@ export class Sprite extends Node {
             ctx.flush();
             ctx._targets.end();
             ctx._targets.restore();
-            //ctx._targets = null;
+            ctx._targets.destroy();//释放
+            ctx._targets = null;//IDE闪
         }
         if (!rt) {
             var rtex: Texture = new Texture(((<Texture2D>(ctx._targets as any))), Texture.INV_UV);
