d39541a32c8c4c8bf8a3c9c544f5617fac73f84a
guzhu@fairygui.com <guzhu@fairygui.com>
2022-10-21 11:56:43 +0800

    反序列化时加强保护

--- src/layaAir/laya/loaders/HierarchyParser.ts
+++ src/layaAir/laya/loaders/HierarchyParser.ts
@@ -159,11 +159,8 @@ export class HierarchyParser {
                                 let parentNode = findNodeInPrefab(node, nodeData2._$parent);
                                 if (parentNode) {
                                     let pos = nodeData2._$index;
-                                    if (pos != null) {
-                                        if (pos > parentNode.numChildren)
-                                            pos = parentNode.numChildren;
+                                    if (pos != null && pos < parentNode.numChildren)
                                         parentNode.addChildAt(n, pos);
-                                    }
                                     else
                                         parentNode.addChild(n);
                                 }
