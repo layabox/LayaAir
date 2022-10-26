746104ec015995d7c512d4f0aad6df963f85e912
lirongf <619213582@qq.com>
2022-10-26 19:23:43 +0800

    增加glsl2.0转置矩阵

--- src/layaAir/laya/d3/shader/utils/Math.glsl
+++ src/layaAir/laya/d3/shader/utils/Math.glsl
@@ -44,6 +44,13 @@ mat4 inverse(mat4 m)
 	       a20 * b03 - a21 * b01 + a22 * b00)
 	/ det;
 }
+
+mat4 transpose(mat4 m){
+    return mat4(m[0][0],m[1][0],m[2][0],m[3][0],
+                m[0][1],m[1][1],m[2][1],m[3][1],
+                m[0][2],m[1][2],m[2][2],m[3][2],
+                m[0][3],m[1][3],m[2][3],m[3][3]);
+}
     #endif // GRAPHICS_API_GLES3
 
     #define PI 3.14159265359
