e140ebfd5bed6826a8e3c167324159d6490dbd84
QTED <agp62601@gmail.com>
2022-11-30 20:52:52 +0800

    shader

--- src/layaAir/laya/d3/shader/utils/Color.glsl
+++ src/layaAir/laya/d3/shader/utils/Color.glsl
@@ -6,9 +6,6 @@
 vec3 linearToGamma(in vec3 value)
 {
     return vec3(mix(pow(value.rgb, vec3(0.41666)) * 1.055 - vec3(0.055), value.rgb * 12.92, vec3(lessThanEqual(value.rgb, vec3(0.0031308)))));
-
-    // return pow(value, vec3(1.0 / 2.2));
-    // return pow(value, vec3(0.455));
 }
 
 vec4 linearToGamma(in vec4 value)
