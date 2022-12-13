d6db9f6671548fb68cbdf397af8d66093b09802b
lvfulong <775331175@qq.com>
2022-12-13 18:00:52 +0800

    fix webgl1.0报错

--- src/layaAir/laya/d3/shader/utils/Math.glsl
+++ src/layaAir/laya/d3/shader/utils/Math.glsl
@@ -51,6 +51,11 @@ mat4 transpose(mat4 m){
                 m[0][2],m[1][2],m[2][2],m[3][2],
                 m[0][3],m[1][3],m[2][3],m[3][3]);
 }
+mat3 transpose(mat3 m){
+    return mat3(m[0][0],m[1][0],m[2][0],
+                m[0][1],m[1][1],m[2][1],
+                m[0][2],m[1][2],m[2][2]);
+}
     #endif // GRAPHICS_API_GLES3
 
     #define PI 3.14159265359
