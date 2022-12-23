5fd7780a6f655722219d855a50671bccf001242f
lirongf <619213582@qq.com>
2022-12-23 15:00:18 +0800

    style: to generate changeLog specification submission/为了生成changeLog规范提交

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,14 @@ module.exports = {
     rules: {
         'type-enum': [2,
             'always', [
-                'feature', 'fixbug', 'refactor', 'optimize', 'style', 'docs', 'chore'
+                'feat',//新功能
+                'fix',//bug
+                'refactor',//重构
+                'optimize',//优化
+                'style',//格式
+                'docs',//文档
+                'chore',//辅助工具变动
+                'test'//测试案例改动
             ]]
     }
 }
