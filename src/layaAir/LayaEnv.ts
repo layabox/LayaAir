3081e1e98effc6cc7e106f127f8600711ef23c94
guzhu@fairygui.com <guzhu@fairygui.com>
2023-01-06 12:09:32 +0800

    feat: add new url extension override

--- src/layaAir/LayaEnv.ts
+++ src/layaAir/LayaEnv.ts
@@ -1,5 +1,6 @@
 export class LayaEnv {
     static version: string = "3.0.0rc";
     static isPlaying: boolean = true;
+    static isPreview: boolean = false;
     static isConch: boolean = (<any>window).conch != null;
 }
