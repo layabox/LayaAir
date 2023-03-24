3335ebfedebe35f148c5d417efe9f0e3c88183f9
INEEDSSD <hellomyworld1997@outlook.com>
2023-03-24 11:32:17 +0800

    fix：Fix UI3D sorting and bounding box calculation errors. 修复：修复UI3D的排序以及包围盒计算错误问题.

--- src/layaAir/laya/d3/core/UI3D/UI3DManager.ts
+++ src/layaAir/laya/d3/core/UI3D/UI3DManager.ts
@@ -32,7 +32,11 @@ export class UI3DManager {
         let rayOri = ray.origin;
         this._UI3Dlist.clean();
         //sort
-        this._UI3Dlist.elements.sort((a: UI3D, b: UI3D) => Number(a._getCameraDistane(rayOri) > b._getCameraDistane(rayOri)));
+        this._UI3Dlist.elements.sort(
+            (a: UI3D, b: UI3D) =>{
+                return a._getCameraDistance(rayOri) - b._getCameraDistance(rayOri);
+            }
+        );
         let elements = this._UI3Dlist.elements;
         for (var i = 0, n = this._UI3Dlist.length; i < n; i++) {
             let hit = elements[i]._checkUIPos(ray);
