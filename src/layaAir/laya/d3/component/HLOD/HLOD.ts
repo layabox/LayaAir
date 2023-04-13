1b65e066cfd3a9c8555c393b045d1f2b3a2b104e
lirongf <619213582@qq.com>
2023-04-13 11:33:41 +0800

    feat: add HLOD Render

--- src/layaAir/laya/d3/component/HLOD/HLOD.ts
+++ src/layaAir/laya/d3/component/HLOD/HLOD.ts
@@ -1,156 +1,17 @@
 import { Component } from "../../../components/Component";
-import { Vector3 } from "../../../maths/Vector3";
-import { Camera } from "../../core/Camera";
-import { Sprite3D } from "../../core/Sprite3D";
-import { Scene3D } from "../../core/scene/Scene3D";
-import { Bounds } from "../../math/Bounds";
-import { HLODRender } from "./HLODRender";
-import { HLODConfig, HLODResourceGroup } from "./HLODUtil";
-
-const tempVec = new Vector3();
 
 export class HLOD extends Component {
-    /**@internal */
-    _resourceList: HLODResourceGroup[];
-    /**@internal */
-    _curLODSource: HLODResourceGroup;
-    /**@internal 包围盒*/
-    _bounds: Bounds;
-    /**@internal */
-    _curRender: HLODRender[];
-    /**@internal TODO*/
-    //_crossRender: HLODRender[];
-    /**@internal TODO*/
-    //_renderMode:
-    /**@internal TODO*/
-    //cacheRender:HLODRender[];
-    /**@internal */
-    _HLODConfig: HLODConfig;
-    /**@internal */
-    private _lodRateArray: number[];
-    /**@internal */
-    private _size: number;
 
-    
-    constructor() {
+    /**@internal */
+    _singleton: boolean;
+
+
+    constructor(){
         super();
-        this._singleton = true;
-    }
-
-    /**
-     * IDE
-     */
-    get bounds() {
-        return this._bounds;
-    }
-
-    set bounds(value: Bounds) {
-        this._bounds = value;
-        this.recalculateBounds();
-    }
-
-    /**
-     * IDE
-     */
-    get lodResource() {
-        return this._resourceList;
-    }
-    set lodResource(value: HLODResourceGroup[]) {
-        this._resourceList = value;
-    }
-
-
-    /**
-     * lod裁剪过滤
-     */
-    set lodCullRateArray(value: number[]) {
-        value.sort((a, b) => b - a);
-        this._lodRateArray = value;
-    }
-
-    get lodCullRateArray() {
-        return this._lodRateArray;
-    }
-
-    private _applyLODResource(resource: HLODResourceGroup) {
-        this._curLODSource = resource;
-        let element = resource.resources;
-        for (let i = 0, n = element.length; i < n; i++) {
-            let hlodRender = this.owner.addComponent(HLODRender);
-            this._curRender.push(hlodRender);
-            hlodRender.curHLODRS = element[i];
-        }
-    }
-
-    private _releaseGroupRender() {
-        this._curRender.forEach(element => {
-            element.destroy();
-        });
-        this._curRender = [];
-    }
-
-
-    /**
-     * @internal
-     * 重新计算包围盒
-     */
-    recalculateBounds() {
-        //如果移动了就修改Bounds
-        let extend = this._bounds.getExtent();
-        this._size = 2 * Math.max(extend.x, extend.y, extend.z);
-    }
-
-    /**
-    * @internal
-    * 渲染之前的更新
-    */
-    onPreRender() {
-        // this.recalculateBounds();
-        // //查看相机的距离
-        let checkCamera = (this.owner.scene as Scene3D).cullInfoCamera as Camera;
-        let maxYDistance = checkCamera.maxlocalYDistance;
-        let cameraFrustum = checkCamera.boundFrustum;
-        Vector3.subtract((this.owner as Sprite3D).transform.position, checkCamera.transform.position, tempVec);
-        //大于farplane,或者不在视锥内.不做lod操作
-        let length = tempVec.length();
-        if (length > checkCamera.farPlane || cameraFrustum.containsPoint((this.owner as Sprite3D).transform.position) == 0) {
-            return;
-        }
-        let rateYDistance = length / checkCamera.farPlane * maxYDistance;
-        let rate = (this._size / rateYDistance);
-        for (let i = 0; i < this._lodRateArray.length; i++) {
-            if (rate < this._lodRateArray[i])
-                continue;
-            this.applyResource(this._resourceList[i]);
-            break;
-        }
-    }
-
-    onUpdate(): void {
-        this._curLODSource.updateMark = Camera._updateMark;
-        //GC TODO
-
-    }
-
-    applyResource(resource: HLODResourceGroup) {
-        if (resource == this._curLODSource)
-            return;
-        if (resource.loaded) {
-            if (this._curLODSource) {
-                //Cache TODO
-                //是否有Cross fade Render 
-                this._releaseGroupRender();
-                this._applyLODResource(resource);
-
-            }
-        } else {
-            resource.load(this.applyResource, this);
-        }
     }
 
     onEnable() {
         super.onEnable();
-        //根据
     }
 
     onDisable() {
@@ -159,11 +20,8 @@ export class HLOD extends Component {
 
     onDestroy() {
         super.onDestroy();
-        //TODO删除
     }
 
     _cloneTo(dest: HLOD) {
-        //Clone to
-        throw "cant clone HLOD"
     }
 }
--- src/layaAir/laya/d3/component/HLOD/HLODUtil.ts
+++ src/layaAir/laya/d3/component/HLOD/HLODUtil.ts
@@ -1,18 +1,9 @@
-import { Laya } from "../../../../Laya";
 import { Vector2 } from "../../../maths/Vector2";
-import { Handler } from "../../../utils/Handler";
+import { Texture2D } from "../../../resource/Texture2D";
 import { Material } from "../../core/material/Material";
 import { Lightmap } from "../../core/scene/Lightmap";
 import { Bounds } from "../../math/Bounds";
 import { HLODBatchMesh } from "./HLODBatchMesh";
-/**
- * HLOD的设置
- */
-export class HLODConfig{
-    releaseCallTime:number;//CG调用时间
-    releaseTime:number;//资源闲置时间
-}
-
 
 /**
  * HLODBatch resource
@@ -28,39 +19,10 @@ export class HLODBatchSubMesh{
 export class HLODElement {
     /**batch mesh */
     HLODMesh: HLODBatchMesh;
-    
     /**material */
-    private _material: Material;
-    public get material(): Material {
-        return this._material;
-    }
-    public set material(value: Material) {
-        if(this._material!=value){
-            this._material&&this._material._removeReference();
-            this._material = value;
-            this._material._addReference();
-        }
-        
-    }
-
+    material: Material;
     /**lightmap */
-    private _lightmap: Lightmap;
-    public get lightmap(): Lightmap {
-        return this._lightmap;
-    }
-    public set lightmap(value: Lightmap) {
-        if(this._lightmap!=value){
-            if(this._lightmap){
-                this._lightmap.lightmapColor._removeReference();
-                this._lightmap.lightmapDirection._removeReference();
-            }
-            this._lightmap = value;
-            this._lightmap.lightmapColor._addReference();
-            this._lightmap.lightmapDirection._addReference();
-
-        }
-        this._lightmap = value;
-    }
+    lightmap: Lightmap;
 
     /**
      * 释放资源
@@ -70,8 +32,8 @@ export class HLODElement {
         this.HLODMesh.destroy();
         this.material.destroy();
         if(this.lightmap){
-            this._lightmap.lightmapColor.destroy();
-            this._lightmap.lightmapDirection.destroy();
+            this.lightmap.lightmapColor.destroy();
+            this.lightmap.lightmapDirection.destroy();
         }
     }
 }
@@ -81,34 +43,18 @@ export class HLODElement {
  * Load/release
  */
 export class HLODResourceGroup {
-    url: string;   
+    url: string;
     updateMark: number;
     resources: HLODElement[];
     loaded: boolean;
 
-    /**
-     * loaded
-     * @param callFun 
-     * @param hlod 
-     */
-    load(callFun:Function,hlod:any) {
-        if(!this.loaded){
-            Laya.loader.load(this.url,Handler.create(
-                this,(res:any)=>{
-                    callFun.apply(hlod,[this]);
-                    this.loaded = true;
-                },[this]
-            ));
-        }
+    load(call:Function) {
+
     }
 
-    /**
-     * 释放HLODGourp资源
-     */
     release() {
         this.resources.forEach(element => {
             element.release();
         });
-        this.loaded = false;
     }
 }
