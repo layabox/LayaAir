cecfb48111f13fa883852aff9cc80bc1b7bd4e82
lirongf <619213582@qq.com>
2023-04-14 15:10:25 +0800

    feat: Improve HLOD

--- src/layaAir/laya/d3/component/HLOD/HLODUtil.ts
+++ src/layaAir/laya/d3/component/HLOD/HLODUtil.ts
@@ -1,9 +1,18 @@
+import { Laya } from "../../../../Laya";
 import { Vector2 } from "../../../maths/Vector2";
-import { Texture2D } from "../../../resource/Texture2D";
+import { Handler } from "../../../utils/Handler";
 import { Material } from "../../core/material/Material";
 import { Lightmap } from "../../core/scene/Lightmap";
 import { Bounds } from "../../math/Bounds";
 import { HLODBatchMesh } from "./HLODBatchMesh";
+/**
+ * HLOD的设置
+ */
+export class HLODConfig{
+    releaseCallTime:number;//CG调用时间
+    releaseTime:number;//资源闲置时间
+}
+
 
 /**
  * HLODBatch resource
@@ -19,10 +28,39 @@ export class HLODBatchSubMesh{
 export class HLODElement {
     /**batch mesh */
     HLODMesh: HLODBatchMesh;
+    
     /**material */
-    material: Material;
+    private _material: Material;
+    public get material(): Material {
+        return this._material;
+    }
+    public set material(value: Material) {
+        if(this._material!=value){
+            this._material&&this._material._removeReference();
+            this._material = value;
+            this._material._addReference();
+        }
+        
+    }
+
     /**lightmap */
-    lightmap: Lightmap;
+    private _lightmap: Lightmap;
+    public get lightmap(): Lightmap {
+        return this._lightmap;
+    }
+    public set lightmap(value: Lightmap) {
+        if(this._lightmap!=value){
+            if(this._lightmap){
+                this._lightmap.lightmapColor._removeReference();
+                this._lightmap.lightmapDirection._removeReference();
+            }
+            this._lightmap = value;
+            this._lightmap.lightmapColor._addReference();
+            this._lightmap.lightmapDirection._addReference();
+
+        }
+        this._lightmap = value;
+    }
 
     /**
      * 释放资源
@@ -32,8 +70,8 @@ export class HLODElement {
         this.HLODMesh.destroy();
         this.material.destroy();
         if(this.lightmap){
-            this.lightmap.lightmapColor.destroy();
-            this.lightmap.lightmapDirection.destroy();
+            this._lightmap.lightmapColor.destroy();
+            this._lightmap.lightmapDirection.destroy();
         }
     }
 }
@@ -48,13 +86,29 @@ export class HLODResourceGroup {
     resources: HLODElement[];
     loaded: boolean;
 
-    load(call:Function) {
-
+    /**
+     * loaded
+     * @param callFun 
+     * @param hlod 
+     */
+    load(callFun:Function,hlod:any) {
+        if(!this.loaded){
+            Laya.loader.load(this.url,Handler.create(
+                this,(res:any)=>{
+                    callFun.apply(hlod,[this]);
+                    this.loaded = true;
+                },[this]
+            ));
+        }
     }
 
+    /**
+     * 释放HLODGourp资源
+     */
     release() {
         this.resources.forEach(element => {
             element.release();
         });
+        this.loaded = false;
     }
 }
