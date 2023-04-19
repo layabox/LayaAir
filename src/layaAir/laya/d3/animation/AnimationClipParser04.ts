a8385f0383af7fc544bbe3169e6c623d494de064
zringhost <38626955@qq.com>
2023-04-19 19:45:50 +0800

    更新动画文件写入逻辑

--- src/layaAir/laya/d3/animation/AnimationClipParser04.ts
+++ src/layaAir/laya/d3/animation/AnimationClipParser04.ts
@@ -105,15 +105,16 @@ export class AnimationClipParser04 {
 		for (i = 0; i < nodeCount; i++) {
 			node = new KeyframeNode();
 			if ("LAYAANIMATION:WEIGHT_05" == AnimationClipParser04._version) {
-				//propertyChangePath,IDE里面还原数据时候使用，引擎中是不需要的
-				let propertyChangePath = AnimationClipParser04._strings[reader.getUint16()];
-				if ("undefined" != propertyChangePath) {
-					(node as any).propertyChangePath = propertyChangePath;
+				let index = reader.getUint16();
+				if (0 != index) {
+					//propertyChangePath,IDE里面还原数据时候使用，引擎中是不需要的
+					(node as any).propertyChangePath = AnimationClipParser04._strings[index];
 				}
-				let callbackFunData = AnimationClipParser04._strings[reader.getUint16()];
-				if ("undefined" != callbackFunData) {
-					node.callbackFunData = callbackFunData;
+				index = reader.getUint16();
+				if (0 != index) {
+					node.callbackFunData = AnimationClipParser04._strings[index];
 				}
+
 				let paramLen = reader.getUint8();
 				let callParms: any[] = null;
 				for (j = 0; j < paramLen; j++) {
