5019b6a7d3100dbd067fbb8e95a3eb6c4eb44577
lirongf <619213582@qq.com>
2023-04-20 19:33:21 +0800

    style: standard style

--- src/layaAir/laya/d3/resource/models/SubMesh.ts
+++ src/layaAir/laya/d3/resource/models/SubMesh.ts
@@ -31,9 +31,6 @@ export class SubMesh extends GeometryElement {
 
 	/** @internal */
 	_indexInMesh: number;
-
-	/** @internal */
-	_vertexStart: number;
 	/** @internal */
 	_indexStart: number;
 	/** @internal */
