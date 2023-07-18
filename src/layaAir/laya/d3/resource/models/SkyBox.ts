7954c4f53a6a52177812aa2b9892a877715b1342
QTEvD <agp62601@gmail.com>
2023-07-18 20:49:52 +0800

    fix: skybox render bug

--- src/layaAir/laya/d3/resource/models/SkyBox.ts
+++ src/layaAir/laya/d3/resource/models/SkyBox.ts
@@ -26,28 +26,28 @@ export class SkyBox extends GeometryElement {
 	 * 创建一个 <code>SkyBox</code> 实例。
 	 */
 	constructor() {
-		super(MeshTopology.Triangles,DrawType.DrawElement);
+		super(MeshTopology.Triangles, DrawType.DrawElement);
 		var halfHeight: number = 1.0;
 		var halfWidth: number = 1.0;
 		var halfDepth: number = 1.0;
 		var vertices: Float32Array = new Float32Array([-halfDepth, halfHeight, -halfWidth, halfDepth, halfHeight, -halfWidth, halfDepth, halfHeight, halfWidth, -halfDepth, halfHeight, halfWidth,//上
 		-halfDepth, -halfHeight, -halfWidth, halfDepth, -halfHeight, -halfWidth, halfDepth, -halfHeight, halfWidth, -halfDepth, -halfHeight, halfWidth]);//下
 		var indices: Uint8Array = new Uint8Array([
-			0, 1, 2, 2, 3, 0, //上
-			4, 7, 6, 6, 5, 4, //下
-			0, 3, 7, 7, 4, 0, //左
-			1, 5, 6, 6, 2, 1,//右
-			3, 2, 6, 6, 7, 3, //前
-			0, 4, 5, 5, 1, 0]);//后
+			0, 2, 1, 2, 0, 3, //上
+			4, 6, 7, 6, 4, 5, //下
+			0, 7, 3, 7, 0, 4, //左
+			1, 6, 5, 6, 1, 2,//右
+			3, 6, 2, 6, 3, 7, //前
+			0, 5, 4, 5, 0, 1]);//后
 		var verDec: VertexDeclaration = VertexMesh.getVertexDeclaration("POSITION");
 		let vertexBuffer = LayaGL.renderOBJCreate.createVertexBuffer3D(verDec.vertexStride * 8, BufferUsage.Static, false);
 		vertexBuffer.vertexDeclaration = verDec;
 		let indexBuffer = LayaGL.renderOBJCreate.createIndexBuffer3D(IndexFormat.UInt8, 36, BufferUsage.Static, false);
-		vertexBuffer.setData(vertices.buffer);
+		vertexBuffer.setData(vertices);
 		indexBuffer.setData(indices);
 		this.bufferState = new BufferState();
-		this.bufferState.applyState([vertexBuffer],indexBuffer);
-		this._geometryElementOBj.setDrawElemenParams(36,0);
+		this.bufferState.applyState([vertexBuffer], indexBuffer);
+		this._geometryElementOBj.setDrawElemenParams(36, 0);
 		this.indexFormat = IndexFormat.UInt8;
 	}
 
@@ -55,8 +55,8 @@ export class SkyBox extends GeometryElement {
 	 * @internal
 	 * UpdateGeometry Data
 	 */
-	 _updateRenderParams(state: RenderContext3D): void {
-		
+	_updateRenderParams(state: RenderContext3D): void {
+
 	}
 }
 
