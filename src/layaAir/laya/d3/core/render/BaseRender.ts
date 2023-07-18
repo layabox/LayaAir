f7daaa1780fc4be2196f33b42577b58bb1b8c598
INEEDSSD <hellomyworld1997@outlook.com>
2023-07-18 23:04:19 +0800

    fix: Fix the update timing problem of reflection probes after optimization. 修复: 修复优化后反射探针更新时序问题.

--- src/layaAir/laya/d3/core/render/BaseRender.ts
+++ src/layaAir/laya/d3/core/render/BaseRender.ts
@@ -142,6 +142,8 @@ export class BaseRender extends Component implements IBoundsCell {
     _updateMark: number = -1;
     /** @internal 是否需要反射探针*/
     _probReflection: ReflectionProbe;
+    /**@internal 属于更新反射探针的标志 */
+    _probeReflectionUpdateMark: number = -1;
     /** @internal 材质是否支持反射探针*/
     _surportReflectionProbe: boolean = false;
     /** @internal 设置是反射探针模式 off  simple */
@@ -451,6 +453,7 @@ export class BaseRender extends Component implements IBoundsCell {
     set probReflection(voluemProbe: ReflectionProbe) {
         if (this._probReflection == voluemProbe)
             return;
+        this._probeReflectionUpdateMark = -1;
         this._probReflection = voluemProbe;
         if (this._reflectionMode == ReflectionProbeMode.off) {
             this._shaderValues.removeDefine(Sprite3DRenderDeclaration.SHADERDEFINE_SPECCUBE_BOX_PROJECTION);
@@ -639,8 +642,9 @@ export class BaseRender extends Component implements IBoundsCell {
 
     _applyReflection() {
         if (!this._probReflection) return;
-        if (this._probReflection._updateMark = ILaya3D.Scene3D._updateMark) {
-            this._probReflection.applyReflectionShaderData(this._shaderValues)
+        if (this._probReflection._updateMark != this._probeReflectionUpdateMark) {
+            this._probeReflectionUpdateMark = this._probReflection._updateMark;
+            this._probReflection.applyReflectionShaderData(this._shaderValues);
         }
     }
 
