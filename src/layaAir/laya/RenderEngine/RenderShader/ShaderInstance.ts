7c9aaea001f634bee77759d7d244231eb5998bb8
lirongf <619213582@qq.com>
2023-07-25 15:02:05 +0800

    fix: fix ShaderData no RenderState Value bug

--- src/layaAir/laya/RenderEngine/RenderShader/ShaderInstance.ts
+++ src/layaAir/laya/RenderEngine/RenderShader/ShaderInstance.ts
@@ -438,28 +438,33 @@ ${uniformglsl}`;
 		var datas: any = shaderDatas.getData();
 
 		var depthWrite: any = datas[Shader3D.DEPTH_WRITE];
-
+		depthWrite = depthWrite??false;
 		RenderStateContext.setDepthMask(depthWrite);
 		if (depthTest === RenderState.DEPTHTEST_OFF)
 			RenderStateContext.setDepthTest(false);
 		else {
 			RenderStateContext.setDepthTest(true);
 			var depthTest: any = datas[Shader3D.DEPTH_TEST];
+			depthTest = depthTest??RenderState.Default.depthTest;
 			RenderStateContext.setDepthFunc(depthTest);
 		}
 
 		var stencilWrite: any = datas[Shader3D.STENCIL_WRITE];
+		stencilWrite = stencilWrite??false;
 		//Stencil
 		var stencilTest: any = datas[Shader3D.STENCIL_TEST];
+		stencilTest = stencilTest??RenderState.Default.stencilTest;
 		RenderStateContext.setStencilMask(stencilWrite);
 		if (stencilWrite) {
 			var stencilOp: any = datas[Shader3D.STENCIL_Op];
+			stencilOp = stencilTest??RenderState.Default.stencilOp;
 			RenderStateContext.setstencilOp(stencilOp.x, stencilOp.y, stencilOp.z);
 		}
 		if (stencilTest == RenderState.STENCILTEST_OFF) {
 			RenderStateContext.setStencilTest(false);
 		} else {
 			var stencilRef: any = datas[Shader3D.STENCIL_Ref];
+			stencilRef = stencilRef??RenderState.Default.stencilRef;
 			RenderStateContext.setStencilTest(true);
 			RenderStateContext.setStencilFunc(stencilTest, stencilRef);
 		}
@@ -473,6 +478,9 @@ ${uniformglsl}`;
 				var blendEquation: any = datas[Shader3D.BLEND_EQUATION] //Shader3D.RENDER_STATE_BLEND_EQUATION);
 				var srcBlend: any = datas[Shader3D.BLEND_SRC] //Shader3D.RENDER_STATE_BLEND_SRC);
 				var dstBlend: any = datas[Shader3D.BLEND_DST] //Shader3D.RENDER_STATE_BLEND_DST);
+				blendEquation = blendEquation??RenderState.Default.blendEquation;
+				srcBlend = srcBlend??RenderState.Default.srcBlend;
+				dstBlend = dstBlend??RenderState.Default.dstBlend;
 				RenderStateContext.setBlend(true);
 				RenderStateContext.setBlendEquation(blendEquation);
 				RenderStateContext.setBlendFunc(srcBlend, dstBlend);
@@ -484,6 +492,12 @@ ${uniformglsl}`;
 				var dstRGB: any = datas[Shader3D.BLEND_DST_RGB];
 				var srcAlpha: any = datas[Shader3D.BLEND_SRC_ALPHA];
 				var dstAlpha: any = datas[Shader3D.BLEND_DST_ALPHA];
+				blendEquationRGB = blendEquationRGB??RenderState.Default.blendEquationRGB;
+				blendEquationAlpha = blendEquationAlpha??RenderState.Default.blendEquationAlpha;
+				srcRGB = srcRGB??RenderState.Default.srcBlendRGB;
+				dstRGB = dstRGB??RenderState.Default.dstBlendRGB;
+				srcAlpha = srcAlpha??RenderState.Default.srcBlendAlpha;
+				dstAlpha = dstAlpha??RenderState.Default.dstBlendAlpha;
 				RenderStateContext.setBlend(true);
 				RenderStateContext.setBlendEquationSeparate(blendEquationRGB, blendEquationAlpha);
 				RenderStateContext.setBlendFuncSeperate(srcRGB, dstRGB, srcAlpha, dstAlpha);
