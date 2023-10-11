0432b608d9d0bd3f84dbb07469d6cf428bd67583
jsjgameer <411963337@qq.com>
2023-10-11 15:05:01 +0800

    add bullet add MeshColliderShape

--- src/layaAir/laya/Physics3D/Bullet/Shape/btMeshColliderShape.ts
+++ src/layaAir/laya/Physics3D/Bullet/Shape/btMeshColliderShape.ts
@@ -1,7 +1,5 @@
-import { VertexBuffer3D } from "../../../d3/graphics/VertexBuffer3D";
 import { Mesh } from "../../../d3/resource/models/Mesh";
 import { Vector3 } from "../../../maths/Vector3";
-import { VertexElement } from "../../../renders/VertexElement";
 import { IMeshColliderShape } from "../../interface/Shape/IMeshColliderShape";
 import { btPhysicsCreateUtil } from "../btPhysicsCreateUtil";
 import { btPhysicsManager } from "../btPhysicsManager";
@@ -22,12 +20,11 @@ export class btMeshColliderShape extends btColliderShape implements IMeshCollide
 	/**@internal */
 	static _btTempVector32: number;
 
-	/**@internal */
-	static _tempVector30: Vector3;
-	/**@internal */
-	static _tempVector31: Vector3;
-	/**@internal */
-	static _tempVector32: Vector3;
+
+	private _limitvertex = 10;
+
+	private _convex: boolean;
+
 
 	/**
 	 * 网格
@@ -39,7 +36,11 @@ export class btMeshColliderShape extends btColliderShape implements IMeshCollide
 		if (this._mesh == value)
 			return;
 		this._mesh = value;
-		this._createShape();
+		if (this._convex) {
+			this._createConvexMeshGeometry();
+		} else {
+			this._createTrianggleMeshGeometry();
+		}
 	}
 
 
@@ -48,77 +49,104 @@ export class btMeshColliderShape extends btColliderShape implements IMeshCollide
 		btMeshColliderShape._btTempVector30 = bt.btVector3_create(0, 0, 0);
 		btMeshColliderShape._btTempVector31 = bt.btVector3_create(0, 0, 0);
 		btMeshColliderShape._btTempVector32 = bt.btVector3_create(0, 0, 0);
-		btMeshColliderShape._tempVector30 = new Vector3();
-		btMeshColliderShape._tempVector31 = new Vector3();
-		btMeshColliderShape._tempVector32 = new Vector3();
 	}
 
 	constructor() {
 		super();
 	}
+
 	setPhysicsMeshFromMesh(value: Mesh): void {
-		throw new Error("Method not implemented.");
-	}
-	setConvexMesh(value: Mesh): void {
-		throw new Error("Method not implemented.");
-	}
-	setLimitVertex(limit: number): void {
-		throw new Error("Method not implemented.");
+		this._mesh = value;
+		this._convex = false;
+		this._createTrianggleMeshGeometry();
 	}
 
-	createPhysicsMeshFromMesh(value: Mesh): number {
+	setConvexMesh(value: Mesh): void {
+		this._mesh = value;
+		this._convex = true;
+		//TODO
+		this._createConvexMeshGeometry();
+	}
+
+	setLimitVertex(limit: number): void {
+
+		this._limitvertex = limit;
+	}
+
+	/**@internal */
+	get convex(): boolean {
+		return this._convex;
+	}
+
+	private _createPhysicsMeshFromMesh(value: Mesh): number {
+		if (value._triangleMesh) {
+			return value._triangleMesh;
+		}
 		let bt = btPhysicsCreateUtil._bt;
 
-		var triangleMesh: number = bt.btTriangleMesh_create();//TODO:独立抽象btTriangleMesh,增加内存复用
-		// var nativePositio0: number = btMeshColliderShape._btTempVector30;
-		// var nativePositio1: number = btMeshColliderShape._btTempVector31;
-		// var nativePositio2: number = btMeshColliderShape._btTempVector32;
-		// var position0: Vector3 = btMeshColliderShape._tempVector30;
-		// var position1: Vector3 = btMeshColliderShape._tempVector31;
-		// var position2: Vector3 = btMeshColliderShape._tempVector32;
+		var triangleMesh: number = value._triangleMesh = bt.btTriangleMesh_create();//TODO:独立抽象btTriangleMesh,增加内存复用
+		var nativePositio0: number = btMeshColliderShape._btTempVector30;
+		var nativePositio1: number = btMeshColliderShape._btTempVector31;
+		var nativePositio2: number = btMeshColliderShape._btTempVector32;
 
-		// var vertexBuffer: VertexBuffer3D = value._vertexBuffer;
-		// var positionElement: VertexElement = value._getPositionElement(vertexBuffer);
-		// var verticesData: Float32Array = vertexBuffer.getFloat32Data();
-		// var floatCount: number = vertexBuffer.vertexDeclaration.vertexStride / 4;
-		// var posOffset: number = positionElement._offset / 4;
+		let posArray = new Array<Vector3>();
+		value.getPositions(posArray);
 
-		// var indices: Uint16Array = value._indexBuffer.getData();//TODO:API修改问题
-		// for (var i: number = 0, n: number = indices.length; i < n; i += 3) {
-		// 	var p0Index: number = indices[i] * floatCount + posOffset;
-		// 	var p1Index: number = indices[i + 1] * floatCount + posOffset;
-		// 	var p2Index: number = indices[i + 2] * floatCount + posOffset;
-		// 	position0.setValue(verticesData[p0Index], verticesData[p0Index + 1], verticesData[p0Index + 2]);
-		// 	position1.setValue(verticesData[p1Index], verticesData[p1Index + 1], verticesData[p1Index + 2]);
-		// 	position2.setValue(verticesData[p2Index], verticesData[p2Index + 1], verticesData[p2Index + 2]);
-
-		// 	btPhysicsManager._convertToBulletVec3(position0, nativePositio0);
-		// 	btPhysicsManager._convertToBulletVec3(position1, nativePositio1);
-		// 	btPhysicsManager._convertToBulletVec3(position2, nativePositio2);
-		// 	bt.btTriangleMesh_addTriangle(triangleMesh, nativePositio0, nativePositio1, nativePositio2, true);
-		// }
+		var indices: Uint16Array = value._indexBuffer.getData();//TODO:API修改问题
+		for (var i: number = 0, n: number = indices.length; i < n; i += 3) {
+			var position0: Vector3 = posArray[indices[i]];
+			var position1: Vector3 = posArray[indices[i + 1]];
+			var position2: Vector3 = posArray[indices[i + 2]];
+			btPhysicsManager._convertToBulletVec3(position0, nativePositio0);
+			btPhysicsManager._convertToBulletVec3(position1, nativePositio1);
+			btPhysicsManager._convertToBulletVec3(position2, nativePositio2);
+			bt.btTriangleMesh_addTriangle(triangleMesh, nativePositio0, nativePositio1, nativePositio2, true);
+		}
 		return triangleMesh;
 	}
 
-	protected _createShape(): void {
+	private _createConvexMeshFromMesh(value: Mesh): number {
+		if (!value._convexMesh) {
+			let bt = btPhysicsCreateUtil._bt;
+			let physicMesh = this._createPhysicsMeshFromMesh(this._mesh);
+			value._convexMesh = bt.btShapeHull_create(physicMesh);
+		}
+		return value._convexMesh;
+	}
+
+	protected _createTrianggleMeshGeometry(): void {
 		let bt = btPhysicsCreateUtil._bt;
 		if (this._btShape) {
 			bt.btCollisionShape_destroy(this._btShape);
 		}
-		this._physicMesh = this.createPhysicsMeshFromMesh(this._mesh);
-		this._physicMesh && (this._btShape = bt.btBvhTriangleMeshShape_create(this._physicMesh));
+		this._physicMesh = this._createPhysicsMeshFromMesh(this._mesh);
+		if (this._physicMesh) {
+			this._btShape = bt.btBvhTriangleMeshShape_create(this._physicMesh);
+			if (this._btCollider) this._btCollider.setColliderShape(this);
+		}
 	}
 
+	protected _createConvexMeshGeometry(): void {
+		let bt = btPhysicsCreateUtil._bt;
+		if (this._btShape) {
+			bt.btCollisionShape_destroy(this._btShape);
+		}
+		let convexMesh = this._createConvexMeshFromMesh(this._mesh);
+		this._btShape = bt.btConvexHullShape_create(convexMesh);
+		if (this._btCollider) this._btCollider.setColliderShape(this);
+
+	}
+
+
+
 	setWorldScale(value: Vector3): void {
-		if (false) {//TODO:待查,这里有问题  if (this._compoundParent)
-			// this.updateLocalTransformations();//TODO:
-		} else {
+		if (this._btShape && this._btCollider) {
 			let bt = btPhysicsCreateUtil._bt;
 			bt.btVector3_setValue(btMeshColliderShape._btTempVector30, value.x, value.y, value.z);
 			bt.btCollisionShape_setLocalScaling(this._btShape, btMeshColliderShape._btTempVector30);
-			if (this._btCollider._btColliderShape && this._btCollider._enableProcessCollisions) {
-				bt.btGImpactShapeInterface_updateBound(this._btShape);//更新缩放后需要更新包围体,有性能损耗
-			}
+			// if (this._btCollider._btColliderShape && this._btCollider._enableProcessCollisions) {
+			// 	bt.btGImpactShapeInterface_updateBound(this._btShape);//更新缩放后需要更新包围体,有性能损耗
+			// }
 		}
 	}
 
