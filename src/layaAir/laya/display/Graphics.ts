31810c2979fdb0b9f8fa72420a2f7c1a8d747d1d
lirongf <619213582@qq.com>
2023-10-24 19:46:53 +0800

    fix: graphic

--- src/layaAir/laya/display/Graphics.ts
+++ src/layaAir/laya/display/Graphics.ts
@@ -39,6 +39,7 @@ import { LayaGL } from "../layagl/LayaGL";
 import { CommandUniformMap } from "../RenderEngine/CommandUniformMap";
 import { ShaderDataType } from "../RenderEngine/RenderShader/ShaderData";
 import { Value2D } from "../webgl/shader/d2/value/Value2D";
+import { DrawEllipseCmd } from "./cmd/DrawEllipseCmd";
 
 /**
  * <code>Graphics</code> 类用于创建绘图显示对象。Graphics可以同时绘制多个位图或者矢量图，还可以结合save，restore，transform，scale，rotate，translate，alpha等指令对绘图效果进行变化。
