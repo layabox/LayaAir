ef88342955ccc84742363ae6f0dd31fcc61f98aa
QTEvD <agp62601@gmail.com>
2024-01-17 15:35:23 +0800

    fix: InstanceRenderElement recover

--- src/layaAir/laya/d3/core/render/InstanceRenderElement.ts
+++ src/layaAir/laya/d3/core/render/InstanceRenderElement.ts
@@ -211,7 +211,6 @@ export class InstanceRenderElement extends RenderElement {
     }
     recover(): void {
         InstanceRenderElement._pool.push(this);
-        this.render._updateRenderType = this.oriRendertype;
         this._isInPool = true;
     }
 }
