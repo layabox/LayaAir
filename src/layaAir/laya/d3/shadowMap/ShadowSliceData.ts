e0a27dce9a3ecaac3604547550ae107f52e2e7d0
QTEvD <agp62601@gmail.com>
2024-01-30 19:36:10 +0800

    ...

--- src/layaAir/laya/d3/shadowMap/ShadowSliceData.ts
+++ src/layaAir/laya/d3/shadowMap/ShadowSliceData.ts
@@ -5,6 +5,11 @@ import { Vector3 } from "../../maths/Vector3";
 import { LayaGL } from "../../layagl/LayaGL";
 import { BoundFrustum } from "../math/BoundFrustum";
 import { ShaderData } from "../../RenderDriver/RenderModuleData/Design/ShaderData";
+import { Config3D } from "../../../Config3D";
+import { UniformBufferObject } from "../../RenderEngine/UniformBufferObject";
+import { BufferUsage } from "../../RenderEngine/RenderEnum/BufferTargetType";
+import { BaseCamera } from "../core/BaseCamera";
+import { UnifromBufferData } from "../../RenderEngine/UniformBufferData";
 
 
 
@@ -29,7 +34,9 @@ export class CameraCullInfo {
  * 阴影分割数据。
  */
 export class ShadowSliceData {
-    cameraShaderValue: ShaderData = LayaGL.unitRenderModuleDataFactory.createShaderData(null);
+    cameraShaderValue: ShaderData;
+    cameraUBO: UniformBufferObject;
+    cameraUBData: UnifromBufferData;
     position: Vector3 = new Vector3();
     offsetX: number;
     offsetY: number;
@@ -41,6 +48,26 @@ export class ShadowSliceData {
     cullPlaneCount: number;
     splitBoundSphere: BoundSphere = new BoundSphere(new Vector3(), 0.0);
     sphereCenterZ: number;
+
+    constructor() {
+        this.cameraShaderValue = LayaGL.unitRenderModuleDataFactory.createShaderData(null);
+
+        if (Config3D._uniformBlock) {
+            let cameraUBO = UniformBufferObject.getBuffer(UniformBufferObject.UBONAME_CAMERA, 0);
+            let cameraUBData = BaseCamera.createCameraUniformBlock();
+
+            if (!cameraUBO) {
+                cameraUBO = UniformBufferObject.create(UniformBufferObject.UBONAME_CAMERA, BufferUsage.Dynamic, cameraUBData.getbyteLength(), false);
+            }
+
+            this.cameraShaderValue._addCheckUBO(UniformBufferObject.UBONAME_CAMERA, cameraUBO, cameraUBData);
+            this.cameraShaderValue.setUniformBuffer(BaseCamera.CAMERAUNIFORMBLOCK, cameraUBO);
+
+            this.cameraUBO = cameraUBO;
+            this.cameraUBData = cameraUBData;
+        }
+
+    }
 }
 
 /**
