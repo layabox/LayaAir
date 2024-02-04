0b601d9d2e5d8a61e1875578750d16b99f46474e
zhangyuhao <zhangyh@layabox.com>
2024-02-04 10:46:09 +0800

    Add feature # 蓝图调试相关

--- src/layaAir/laya/bp/runtime/node/BlueprintCustomFunNode.ts
+++ src/layaAir/laya/bp/runtime/node/BlueprintCustomFunNode.ts
@@ -5,7 +5,6 @@ import { IRuntimeDataManger } from "../../core/interface/IRuntimeDataManger";
 import { TBPNode } from "../../datas/types/BlueprintTypes";
 import { BlueprintFactory } from "../BlueprintFactory";
 import { BlueprintPinRuntime } from "../BlueprintPinRuntime";
-import { BpDebuggerRunType } from "../debugger/BlueprintDebuggerManager";
 import { IRunAble } from "../interface/IRunAble";
 import { BlueprintFunNode } from "./BlueprintFunNode";
 
@@ -38,14 +37,7 @@ export class BlueprintCustomFunNode extends BlueprintFunNode {
             let result: any;
             let _funcContext = caller[BlueprintFactory.contextSymbol];
             result = caller[BlueprintFactory.bpSymbol].runCustomFun(_funcContext, this.functionID, parmsArray, () => {
-                const _runTimeData = _funcContext.getDataMangerByID(this.functionID);
-                if (_runTimeData.debuggerPause == BpDebuggerRunType.stepOut) {
-                    _runTimeData.debuggerPause = BpDebuggerRunType.none;
-                    _funcContext.debuggerManager.pause(_funcContext as any, this);
-                }
-                if (result === false && cb) {
-                    cb();
-                }
+                this._excuteFun(_funcContext, cb);
             }, runId, this.inExcutes.indexOf(fromPin));
             if (result === false) {
                 primise = new Promise((resolve, reject) => {
@@ -56,6 +48,12 @@ export class BlueprintCustomFunNode extends BlueprintFunNode {
         }
         return null;
         //return context.excuteFun(this.nativeFun, this.outPutParmPins, caller, parmsArray);
+    }
+
+    protected _excuteFun(context: IRunAble, cb: any) {
+        if (cb) {
+            cb();
+        }
     }
 
     addPin(pin: BlueprintPinRuntime) {
