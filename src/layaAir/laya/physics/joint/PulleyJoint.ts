ba168174dd06b5a07e59a5c30f126a7a97a89f56
LayaCharley <399050@qq.com>
2024-02-21 11:01:58 +0800

    optimize:Adjust the default value of the ratio for the PulleyJoint to 1.(将滑轮关节的传动比率默认值调整为1)

--- src/layaAir/laya/physics/joint/PulleyJoint.ts
+++ src/layaAir/laya/physics/joint/PulleyJoint.ts
@@ -24,7 +24,7 @@ export class PulleyJoint extends JointBase {
     /**[首次设置有效]滑轮上与节点otherAnchor相连接的节点，是相对于otherBody的左上角位置偏移*/
     otherGroundPoint: any[] = [0, -100];
     /**[首次设置有效]两刚体移动距离比率*/
-    ratio: number = 1.5;
+    ratio: number = 1;
     /**[首次设置有效]两个刚体是否可以发生碰撞，默认为false*/
     collideConnected: boolean = false;
     /**
