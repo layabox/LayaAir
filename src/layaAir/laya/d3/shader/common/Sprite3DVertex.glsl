0e21249d3526cfbf08984105b64c85bbbb1290c1
jsjgameer <411963337@qq.com>
2024-03-08 12:34:26 +0800

    fix:fixed SimpleSkinnedMeshRenderer add CustomData Interface

--- src/layaAir/laya/d3/shader/common/Sprite3DVertex.glsl
+++ src/layaAir/laya/d3/shader/common/Sprite3DVertex.glsl
@@ -1,67 +1,67 @@
-#if!defined(Sprite3DVertex_lib)
-#define Sprite3DVertex_lib
+#if !defined(Sprite3DVertex_lib)
+    #define Sprite3DVertex_lib
 
-#include "Sprite3DCommon.glsl";
+    #include "Sprite3DCommon.glsl";
 
-#ifdef BONE
+    #ifdef BONE
 // todo const int c_MaxBoneCount = 24
 uniform mat4 u_Bones[24];
 
-#ifdef SIMPLEBONE
+	#ifdef SIMPLEBONE
 uniform vec4 u_SimpleAnimatorParams;
 uniform sampler2D u_SimpleAnimatorTexture;
 uniform float u_SimpleAnimatorTextureSize;
-#include "BakedBoneMatrixSampler.glsl";
+	    #include "BakedBoneMatrixSampler.glsl";
 
-#endif// SIMPLEBONE
+	#endif // SIMPLEBONE
 
-#endif// BONE
+    #endif // BONE
 
 /**
-* world matrix
-*/
+ * world matrix
+ */
 mat4 getWorldMatrix()
 {
     #ifdef GPU_INSTANCE
-    mat4 worldMat=a_WorldMat;
+    mat4 worldMat = a_WorldMat;
     #else
-    mat4 worldMat=u_WorldMat;
-    #endif// GPU_INSTANCE
-    
+    mat4 worldMat = u_WorldMat;
+    #endif // GPU_INSTANCE
+
     #ifdef BONE
-    
-    #ifdef SIMPLEBONE
-    
-    #ifdef GPU_INSTANCE
-    float currentPixelPos=a_SimpleTextureParams.x+a_SimpleTextureParams.y;
-    #else// GPU_INSTANCE
-    float currentPixelPos=u_SimpleAnimatorParams.x+u_SimpleAnimatorParams.y;
-    #endif// GPU_INSTANCE
-    
-    float offset=1./u_SimpleAnimatorTextureSize;
-    mat4 skinTrans=loadBakedMatMatrix(currentPixelPos,a_BoneIndices.x,offset)*a_BoneWeights.x;
-    skinTrans+=loadBakedMatMatrix(currentPixelPos,a_BoneIndices.y,offset)*a_BoneWeights.y;
-    skinTrans+=loadBakedMatMatrix(currentPixelPos,a_BoneIndices.z,offset)*a_BoneWeights.z;
-    skinTrans+=loadBakedMatMatrix(currentPixelPos,a_BoneIndices.w,offset)*a_BoneWeights.w;
-    worldMat=worldMat*skinTrans;
-    
-    #else// SIMPLEBONE
-    
-    ivec4 boneIndex=ivec4(a_BoneIndices);
-    mat4 skinTrans=u_Bones[boneIndex.x]*a_BoneWeights.x;
-    skinTrans+=u_Bones[boneIndex.y]*a_BoneWeights.y;
-    skinTrans+=u_Bones[boneIndex.z]*a_BoneWeights.z;
-    skinTrans+=u_Bones[boneIndex.w]*a_BoneWeights.w;
-    worldMat=worldMat*skinTrans;
-    
-    #endif// SIMPLEBONE
-    
-    #endif// BONE
-    
+
+	#ifdef SIMPLEBONE
+
+	    #ifdef GPU_INSTANCE
+    float currentPixelPos = a_SimpleTextureParams.x + a_SimpleTextureParams.y;
+	    #else // GPU_INSTANCE
+    float currentPixelPos = u_SimpleAnimatorParams.x + u_SimpleAnimatorParams.y;
+	    #endif // GPU_INSTANCE
+
+    float offset = 1.0 / u_SimpleAnimatorTextureSize;
+    mat4 skinTrans = loadBakedMatMatrix(currentPixelPos, a_BoneIndices.x, offset) * a_BoneWeights.x;
+    skinTrans += loadBakedMatMatrix(currentPixelPos, a_BoneIndices.y, offset) * a_BoneWeights.y;
+    skinTrans += loadBakedMatMatrix(currentPixelPos, a_BoneIndices.z, offset) * a_BoneWeights.z;
+    skinTrans += loadBakedMatMatrix(currentPixelPos, a_BoneIndices.w, offset) * a_BoneWeights.w;
+    worldMat = worldMat * skinTrans;
+
+	#else // SIMPLEBONE
+
+    ivec4 boneIndex = ivec4(a_BoneIndices);
+    mat4 skinTrans = u_Bones[boneIndex.x] * a_BoneWeights.x;
+    skinTrans += u_Bones[boneIndex.y] * a_BoneWeights.y;
+    skinTrans += u_Bones[boneIndex.z] * a_BoneWeights.z;
+    skinTrans += u_Bones[boneIndex.w] * a_BoneWeights.w;
+    worldMat = worldMat * skinTrans;
+
+	#endif // SIMPLEBONE
+
+    #endif // BONE
+
     return worldMat;
 }
 
-vec2 getCustomData(){
+vec2 getSimpleBoneCustomData(){
     vec2 custom;
     #ifdef SIMPLEBONE
     #ifdef GPU_INSTANCE
@@ -73,4 +73,4 @@ vec2 getCustomData(){
     return custom;
 }
 
-#endif// Sprite3DVertex_lib
+#endif // Sprite3DVertex_lib
