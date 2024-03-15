7b12af796913697fc5ea5b8271459976d49e3859
guozhaokui <guozhaokuiwang@gmail.com>
2024-03-15 19:06:14 +0800

    fix: cache as bitmap. 原来的偏移不对

--- src/layaAir/laya/test/2d/test.ts
+++ src/layaAir/laya/test/2d/test.ts
@@ -1,9 +1,11 @@
+import { Laya } from "../../../Laya";
 import test from "./simple1"
 import { usewebgl } from "./utils";
 
 let testfile = window.location.search.substring(1);
 async function testf(){
     usewebgl();
+    (window as any).Laya=Laya;
     await import('./'+testfile);
 
 }
---
+++ src/layaAir/laya/test/2d/test_TiledMap_AnimationTile.ts
@@ -0,0 +1,36 @@
+import "laya/d3/core/scene/Scene3D";
+import "laya/ModuleDef";
+import "laya/ui/ModuleDef";
+import "laya/ani/ModuleDef";
+
+import { Laya } from "../../../Laya";
+import { Shader3D } from "../../RenderEngine/RenderShader/Shader3D";
+import { Stage } from "../../display/Stage";
+import { Sprite } from "../../display/Sprite";
+import { Templet } from "../../ani/bone/Templet";
+import { Event } from "../../events/Event";
+import { URL } from "../../net/URL";
+import { TiledMap } from "../../map/TiledMap";
+import { Rectangle } from "../../maths/Rectangle";
+import { Handler } from "../../utils/Handler";
+
+//HierarchyLoader和MaterialLoader等是通过前面的import完成的
+
+async function test(){
+    //初始化引擎
+    await Laya.init(0,0);
+    Laya.stage.scaleMode = Stage.SCALE_FULL;
+    Laya.stage.screenMode = Stage.SCREEN_NONE;
+    Shader3D.debugMode = true;
+    URL.basePath += "sample-resource/";
+
+    let tiledMap = new TiledMap();
+    tiledMap.createMap("res/tiledMap/orthogonal-test-movelayer.json", new Rectangle(0, 0, Laya.stage.width, Laya.stage.height), 
+    Handler.create(null, ()=>{
+        tiledMap.mapSprite().removeSelf();
+        Laya.stage.addChild(tiledMap.mapSprite());
+    }));
+}
+
+
+test();
