25f576729bf7275e6ec799076008f648f7099345
QTEvD <agp62601@gmail.com>
2024-03-21 17:19:32 +0800

    fix: camera viewport bug

--- src/layaAir/laya/RenderDriver/WebGLDriver/3DRenderPass/WebGLRender3DProcess.ts
+++ src/layaAir/laya/RenderDriver/WebGLDriver/3DRenderPass/WebGLRender3DProcess.ts
@@ -7,6 +7,7 @@ import { RenderContext3D } from "../../../d3/core/render/RenderContext3D";
 import { Scene3D } from "../../../d3/core/scene/Scene3D";
 import { Scene3DShaderDeclaration } from "../../../d3/core/scene/Scene3DShaderDeclaration";
 import { DepthPass } from "../../../d3/depthMap/DepthPass";
+import { Viewport } from "../../../d3/math/Viewport";
 import { ShadowCasterPass } from "../../../d3/shadowMap/ShadowCasterPass";
 import { Vector4 } from "../../../maths/Vector4";
 import { DepthTextureMode, RenderTexture } from "../../../resource/RenderTexture";
@@ -18,6 +19,7 @@ import { WebCameraNodeData } from "../../RenderModuleData/WebModuleData/3D/WebMo
 import { WebGLForwardAddRP } from "./WebGLForwardAddRP";
 import { WebGLRenderContext3D } from "./WebGLRenderContext3D";
 
+const viewport = new Viewport(0, 0, 0, 0);
 
 export class WebGLRender3DProcess implements IRender3DProcess {
 
@@ -27,7 +29,6 @@ export class WebGLRender3DProcess implements IRender3DProcess {
         let renderpass = this.renderpass.renderpass;
 
         let renderRT = camera._getRenderTexture();
-        let viewport = camera.viewport;
 
         // clear
         let clearConst = 0;
@@ -66,6 +67,7 @@ export class WebGLRender3DProcess implements IRender3DProcess {
         renderpass.clearFlag = clearConst;
         renderpass.clearColor = clearValue;
 
+        viewport.set(0, 0, renderRT.width, renderRT.height);
         renderpass.setViewPort(viewport);
         let scissor = Vector4.tempVec4;
         scissor.setValue(viewport.x, viewport.y, viewport.width, viewport.height);
