ae6bcbc2d79e184a47ffb2986a59d1002a82a4bb
guzhu@fairygui.com <guzhu@fairygui.com>
2024-03-27 10:59:33 +0800

    fix: VideoTexture play issue in wechat browser

--- src/layaAir/laya/media/VideoTexture.ts
+++ src/layaAir/laya/media/VideoTexture.ts
@@ -96,10 +96,6 @@ export class VideoTexture extends BaseTexture {
         if ('requestVideoFrameCallback' in ele) {
             ele.requestVideoFrameCallback(updateVideo);
         }
-        //ios微信浏览器环境下默认不触发loadedmetadata，在主动调用play方法的时候才会触发loadedmetadata事件
-        if (ILaya.Browser.onWeiXin) {
-            this.loadedmetadata();
-        }
     }
 
     private isNeedUpdate() {
