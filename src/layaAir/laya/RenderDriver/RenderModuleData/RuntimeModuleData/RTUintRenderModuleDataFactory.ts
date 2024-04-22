a44beb7e68e1130cf8887ccf59e14de1f54ee451
lirongf <619213582@qq.com>
2024-04-22 20:43:53 +0800

    fixed：remove unuse code

--- src/layaAir/laya/RenderDriver/RenderModuleData/RuntimeModuleData/RTUintRenderModuleDataFactory.ts
+++ src/layaAir/laya/RenderDriver/RenderModuleData/RuntimeModuleData/RTUintRenderModuleDataFactory.ts
@@ -1,11 +1,7 @@
 import { ShaderPass } from "../../../RenderEngine/RenderShader/ShaderPass";
-import { Resource } from "../../../resource/Resource";
-import { IShaderPassData } from "../Design/IShaderPassData";
-import { ISubshaderData } from "../Design/ISubShaderData";
 import { IUnitRenderModuleDataFactory } from "../Design/IUnitRenderModuleDataFactory";
 import { RTDefineDatas } from "./RTDefineDatas";
 import { RTRenderState } from "./RTRenderState";
-import { RTShaderDefine } from "./RTShaderDefine";
 import { RTShaderPass } from "./RTShaderPass";
 import { RTSubShader } from "./RTSubShader";
 
@@ -19,9 +15,6 @@ export class RTUintRenderModuleDataFactory implements IUnitRenderModuleDataFacto
    createRenderState(): RTRenderState {
       return new RTRenderState();
    }
-   //createShaderDefine(index: number, value: number): RTShaderDefine {
-   //   return new RTShaderDefine(index,value);
-   //}
    createDefineDatas(): RTDefineDatas {
       return new RTDefineDatas();
    }
