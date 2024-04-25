5eb3fcdfd559805b1cd3fd0e741882f027d3670a
jsjgameer <411963337@qq.com>
2024-04-25 20:23:28 +0800

    fix: fixed the Sprite global to local and local to globa

--- src/samples/2d/Physics_Strandbeests.ts
+++ src/samples/2d/Physics_Strandbeests.ts
@@ -55,6 +55,7 @@ export class Physics_Strandbeests {
     }
 
     Construct() {
+        debugger;
         // Ground
         let ground = new Sprite();
         ground.name = "ground"
@@ -109,7 +110,7 @@ export class Physics_Strandbeests {
 
 
 
-        // 转动关节
+        // // 转动关节
         let motorJoint: RevoluteJoint = this.motorJoint = new RevoluteJoint();
         motorJoint.otherBody = chassisBody;
         motorJoint.collideConnected = false;
@@ -130,10 +131,10 @@ export class Physics_Strandbeests {
     }
 
     private getDistance(body: RigidBody, p: number[], body1: RigidBody, p1: number[]) {
-        let g1 = body.GetWorldPoint(p[0], p[1]);
+        let g1 = body.getWorldPoint(p[0], p[1]);
         let x = g1.x;
         let y = g1.y;
-        g1 = body1.GetWorldPoint(p1[0], p1[1]);
+        g1 = body1.getWorldPoint(p1[0], p1[1]);
         return Math.sqrt(Math.pow(g1.x - x, 2) + Math.pow(g1.y - y, 2))
     }
 
