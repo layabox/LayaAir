cb053c9cf33f3df51980b1910648f178ea7926da
Adonis2016 <cpi2012@qq.com>
2024-05-09 14:31:54 +0800

    更新WebGPU

--- src/layaAir/laya/RenderDriver/WebGPUDriver/RenderDevice/Naga/NagaWASM.ts
+++ src/layaAir/laya/RenderDriver/WebGPUDriver/RenderDevice/Naga/NagaWASM.ts
@@ -1,18 +1,18 @@
 //@ts-ignore
-import init, { glsl_compile } from "./naga";
+import initSync, { glsl_compile } from "./naga_wasm";
 
 export class NagaWASM {
     /**
      * 初始化wasm库
      */
     async init() {
-        await init('./naga/naga.wasm');
+        await initSync('./naga/naga_wasm_bg.wasm');
     }
 
     /**
      * 将GLSL4.5转译成WGSL
      */
     compileGLSL2WGSL(code: string, type: string) {
-        return glsl_compile(code, type, false);
+        return glsl_compile(code, type);
     }
 }
