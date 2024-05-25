948ab00118293dcbc0e057f5d3bdab8dfe338561
Adonis2016 <cpi2012@qq.com>
2024-05-25 10:42:09 +0800

    添加文件

---
+++ src/layaAir/laya/RenderDriver/WebGPUDriver/3DRenderPass/WebGPURenderCMD/WebGPUDrawElementCMDData.ts
@@ -0,0 +1,27 @@
+import { DrawElementCMDData, RenderCMDType } from "../../../DriverDesign/3DRenderPass/IRendderCMD";
+import { WebGPURenderContext3D } from "../WebGPURenderContext3D";
+import { WebGPURenderElement3D } from "../WebGPURenderElement3D";
+
+export class WebGPUDrawElementCMDData extends DrawElementCMDData {
+    type: RenderCMDType;
+    private _elemets: WebGPURenderElement3D[];
+
+    constructor() {
+        super();
+        this.type = RenderCMDType.DrawElement;
+    }
+
+    setRenderelements(value: WebGPURenderElement3D[]): void {
+        this._elemets = value;
+    }
+
+    apply(context: WebGPURenderContext3D): void {
+        if (this._elemets.length == 1) {
+            context.drawRenderElementOne(this._elemets[0]);
+        } else {
+            this._elemets.forEach(element => {
+                context.drawRenderElementOne(element);
+            });
+        }
+    }
+}
