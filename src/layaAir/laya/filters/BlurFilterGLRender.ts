db6aca362d2a90b02e76b16123a39cc94f6bd86b
guozhaokui <guozhaokuiwang@gmail.com>
2024-06-15 12:26:52 +0800

    fix: The blur filter causes transparency blur滤镜会导致变暗和变透明。strength很小的保护

--- src/layaAir/laya/filters/BlurFilterGLRender.ts
+++ src/layaAir/laya/filters/BlurFilterGLRender.ts
@@ -19,7 +19,9 @@ export class BlurFilterGLRender {
     private static blurinfo = new Vector2();
     render(rt: RenderTexture2D, ctx: Context, width: number, height: number, filter: BlurFilter): void {
         var shaderValue: Value2D = Value2D.create(RenderSpriteData.Texture2D);
-        this.setShaderInfo(shaderValue, filter, rt.width, rt.height);
+        if(filter.strength>=2){
+            this.setShaderInfo(shaderValue, filter, rt.width, rt.height);
+        }
         ctx.drawTarget(rt, 0, 0, width, height, Matrix.EMPTY.identity(), shaderValue);
     }
 
@@ -28,30 +30,29 @@ export class BlurFilterGLRender {
         var sv = <TextureSV>shader;
         BlurFilterGLRender.blurinfo.x = w; BlurFilterGLRender.blurinfo.y = h;
         sv.blurInfo = BlurFilterGLRender.blurinfo;
-        var sigma: number = filter.strength / 3.0;//3σ以外影响很小。即当σ=1的时候，半径为3;
+        let strength = Math.max(Math.abs(filter.strength),2);//<2的函数太尖不适合下面的方法，会导致变透明，具体以后再找
+        var sigma: number = strength / 3.0;//3σ以外影响很小。即当σ=1的时候，半径为3;
         var sigma2: number = sigma * sigma;
         let vz = 2.0 * sigma2;
         let vw = 1.0 / (2.0 * Math.PI * sigma2);
         //由于目前shader中写死了blur宽度是9，相当于希望3σ是4，可是实际使用的时候经常会strength超过4，
         //这时候blur范围内的积分<1会导致变暗变透明，所以需要计算实际积分值进行放大
         //使用公式计算误差较大，直接累加把
-        if (filter.strength > 4) {
-            let s = 0;
-            let key = Math.floor(filter.strength * 10);
-            if (_definiteIntegralMap[key] != undefined) {
-                s = _definiteIntegralMap[key];
-            }else{
-                for (let y = -4; y <= 4; ++y) {
-                    for (let x = -4; x <= 4; ++x) {
-                        s += vw * Math.exp(-(x * x + y * y) / vz);
-                    }
+        let s = 0;
+        let key = Math.floor(strength * 10);
+        if (_definiteIntegralMap[key] != undefined) {
+            s = _definiteIntegralMap[key];
+        }else{
+            for (let y = -4; y <= 4; ++y) {
+                for (let x = -4; x <= 4; ++x) {
+                    s += vw * Math.exp(-(x * x + y * y) / vz);
                 }
-                _definiteIntegralMap[key] = s;
             }
-            vw /= s;
-        }        
+            _definiteIntegralMap[key] = s;
+        }
+        vw /= s;
 
-        Vector4.tempVec4.x = filter.strength_sig2_2sig2_gauss1[0] = filter.strength;
+        Vector4.tempVec4.x = filter.strength_sig2_2sig2_gauss1[0] = strength;
         Vector4.tempVec4.y = filter.strength_sig2_2sig2_gauss1[1] = sigma2;			//做一些预计算传给shader，提高效率
         Vector4.tempVec4.z = filter.strength_sig2_2sig2_gauss1[2] = vz;
         Vector4.tempVec4.w = filter.strength_sig2_2sig2_gauss1[3] = vw;
