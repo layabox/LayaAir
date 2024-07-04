96b391f9756fdcdb769bb6bea9d986fed83d695b
zhuliang <38626955@qq.com>
2024-07-04 16:21:28 +0800

    fix:Remove the SpineComponent code and directly create a sprite+component logic in the IDE.(去掉SpineComponent代码，直接在IDE创建一个sprite+component逻辑)

--- src/layaAir/laya/spine/SpineComponent.ts
+++
@@ -1,9 +0,0 @@
-import { Sprite } from "../display/Sprite";
-import { Spine2DRenderNode } from "./Spine2DRenderNode";
-
-export class SpineComponent extends Sprite {
-    constructor() {
-        super();
-        this.addComponent(Spine2DRenderNode);
-    }
-}
