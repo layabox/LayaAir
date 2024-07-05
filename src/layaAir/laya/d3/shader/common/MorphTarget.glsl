52096bdd77f0be7d2480aa7eb82de787b4b54c45
Adonis2016 <cpi2012@qq.com>
2024-07-05 15:44:31 +0800

    fix: a bug for support WebGPU uniformBlock (修复uniform数组成员必须16字节对齐的问题）

--- src/layaAir/laya/d3/shader/common/MorphTarget.glsl
+++ src/layaAir/laya/d3/shader/common/MorphTarget.glsl
@@ -18,11 +18,13 @@ uniform vec4 u_MorphAttrOffset;
 	#define Morph_NormalOffset   u_MorphAttrOffset.y
 	#define Morph_TangentOffset  u_MorphAttrOffset.z
 
-// #define MORPH_MAX_COUNT 8
+#define MORPH_MAX_COUNT 32 //兼容WGSL
 
 /// dynamic params
-uniform float u_MorphActiveTargets[MORPH_MAX_COUNT];
-uniform float u_MorphTargetWeights[MORPH_MAX_COUNT];
+//uniform float u_MorphActiveTargets[MORPH_MAX_COUNT];
+//uniform float u_MorphTargetWeights[MORPH_MAX_COUNT];
+uniform vec4 u_MorphActiveTargets[MORPH_MAX_COUNT]; //兼容WGSL
+uniform vec4 u_MorphTargetWeights[MORPH_MAX_COUNT];
 uniform int u_MorphTargetActiveCount;
 
 	#define MORPH_ACTIVE_COUNT u_MorphTargetActiveCount
@@ -43,9 +45,9 @@ vec3 positionMorph(in vec3 position)
 
     for (int i = 0; i < MORPH_ACTIVE_COUNT; i++)
 	{
-	    float index = u_MorphActiveTargets[i];
+	    float index = u_MorphActiveTargets[i].x; //兼容WGSL
 
-	    position += sampleMorphTargets(vertexID, index).xyz * u_MorphTargetWeights[i];
+	    position += sampleMorphTargets(vertexID, index).xyz * u_MorphTargetWeights[i].x; //兼容WGSL
 	}
 
     return position;
@@ -56,9 +58,9 @@ vec3 normalMorph(in vec3 normal)
     int vertexID = gl_VertexID * int(Morph_AttributeNum) + int(Morph_NormalOffset);
     for (int i = 0; i < MORPH_ACTIVE_COUNT; i++)
 	{
-	    float index = u_MorphActiveTargets[i];
+	    float index = u_MorphActiveTargets[i].x; //兼容WGSL
 
-	    normal += sampleMorphTargets(vertexID, index).xyz * u_MorphTargetWeights[i];
+	    normal += sampleMorphTargets(vertexID, index).xyz * u_MorphTargetWeights[i].x; //兼容WGSL
 	}
 
     return normal;
@@ -69,11 +71,11 @@ vec4 tangentMorph(in vec4 tangent)
     int vertexID = gl_VertexID * int(Morph_AttributeNum) + int(Morph_TangentOffset);
     for (int i = 0; i < MORPH_ACTIVE_COUNT; i++)
 	{
-	    float index = u_MorphActiveTargets[i];
+	    float index = u_MorphActiveTargets[i].x; //兼容WGSL
 
 	    vec4 sampleTangent = sampleMorphTargets(vertexID, index);
 
-	    tangent.xyz += sampleTangent.xyz * u_MorphTargetWeights[i] * tangent.w * sampleTangent.w;
+	    tangent.xyz += sampleTangent.xyz * u_MorphTargetWeights[i].x * tangent.w * sampleTangent.w; //兼容WGSL
 	}
 
     return tangent;
