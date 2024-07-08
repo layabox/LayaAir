61675e6e58b17ef7851008a7cfc08f92900778db
songbo <hellomyworld1997@outlook.com>
2024-07-08 16:11:53 +0800

    fix: Fixed the problem of custom font in Alipay mini game. 修复: 修复支付宝小游戏自定义字体问题.

--- src/layaAir/laya/webgl/text/CharRender_Canvas.ts
+++ src/layaAir/laya/webgl/text/CharRender_Canvas.ts
@@ -189,11 +189,11 @@ export class CharRender_Canvas extends ICharRender {
 		//if (canvas.width != (w + 1) || canvas.height != (h + 1)) {
 		CharRender_Canvas.canvas.width = Math.min(w + 1, this.maxTexW);
 		CharRender_Canvas.canvas.height = Math.min(h + 1, this.maxTexH);
-		ctx.font = font;
 		//}
 		ctx.clearRect(0, 0, w + 1 + lineWidth, h + 1 + lineWidth);
 		ctx.setTransform(1, 0, 0, 1, 0, 0);	// 强制清理缩放
 		ctx.save();
+		ctx.font = font;
 		if (this.scaleFontSize) {
 			//这里的缩放会导致与上面的缩放同时起作用。所以上面保护
 			ctx.scale(this.lastScaleX, this.lastScaleY);
