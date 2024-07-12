d8293b2b09f42ee290d7a80c19f64492ed6694aa
LayaCharley <399050@qq.com>
2024-07-12 16:50:14 +0800

    style: Code comments for internationalizing the ISelect class(国际化ISelect类的代码注释)

--- src/layaAir/laya/ui/ISelect.ts
+++ src/layaAir/laya/ui/ISelect.ts
@@ -2,15 +2,18 @@ import { Handler } from "../utils/Handler"
 import { UIComponent } from "./UIComponent";
 
 /**
- * <code>ISelect</code> 接口，实现对象的 <code>selected</code> 属性和 <code>clickHandler</code> 选择回调函数处理器。
+ * @en The ISelect interface, which should be implemented by objects that have a selected property and a clickHandler for selection callback function processing.
+ * @zh ISelect 接口，实现对象的 selected属性和clickHandler 选择回调函数处理器。
  */
 export interface ISelect extends UIComponent {
     /**
-     * 一个布尔值，表示是否被选择。
+     * @en A boolean value indicating whether the object is selected.
+     * @zh 是否被选择。
      */
     selected: boolean;
     /**
-     * 对象的点击事件回掉函数处理器。
+     * @en The click event callback function processing handler of the object.
+     * @zh 对象的点击事件回调函数处理器。
      */
     clickHandler: Handler;
 }
