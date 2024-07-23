f5f400ddb99a4407631519f8ff8f7f28144acd42
LayaCharley <399050@qq.com>
2024-07-23 11:49:44 +0800

    style: Code comments for internationalizing all classes in the `bone` directory for skeletal animation.(国际化骨骼动画目录bone下所有类的代码注释)

--- src/layaAir/laya/ani/bone/DeformSlotDisplayData.ts
+++ src/layaAir/laya/ani/bone/DeformSlotDisplayData.ts
@@ -1,17 +1,51 @@
 import { BoneSlot } from "./BoneSlot";
 /**
  * @internal
+ * @en The `DeformSlotDisplayData` class is used internally to store and manage the display data for deformable slots in animations.
+ * @zh `DeformSlotDisplayData` 类用于在动画中存储和管理可变形插槽的显示数据。
  */
 export class DeformSlotDisplayData {
 
+	/**
+	 * @en The bone slot to which the deform data is applied.
+	 * @zh 应用变形数据的骨骼插槽。
+	 */
 	boneSlot: BoneSlot;
+	/**
+	 * @en The index of the slot in the skin.
+	 * @zh 插槽在皮肤中的索引。
+	 */
 	slotIndex: number = -1;
+	/**
+	 * @en The name of the attachment associated with the deform data.
+	 * @zh 与变形数据相关的附件名称。
+	 */
 	attachment: string;
+	/**
+	 * @en A list of time values corresponding to the keyframes of the deform animation.
+	 * @zh 对应于变形动画关键帧的时间值列表。
+	 */
 	timeList: number[] = [];
+	/**
+	 * @en A list of vertex data arrays for each keyframe, representing the deformed state.
+	 * @zh 每个关键帧的顶点数据数组列表，代表变形状态。
+	 */
 	vectices: any[][] = [];
+	/**
+	 * @en A list indicating whether there is a tween between keyframes.
+	 * @zh 指示关键帧之间是否存在缓动的列表。
+	 */
 	tweenKeyList: boolean[] = [];
 
+	/**
+	 * @en The deformed vertex data applied to the slot.
+	 * @zh 应用于插槽的变形顶点数据。
+	 */
 	deformData: any[];
+	/**
+	 * @en The current frame index for the deform animation.
+	 * @zh 变形动画的当前帧索引。
+	 */
 	frameIndex: number = 0;
 
 	//TODO:coverage
@@ -37,6 +71,16 @@ export class DeformSlotDisplayData {
 		return 0; // Can't happen.
 	}
 
+	/**
+	 * @en Applies the deform data to the bone slot based on the given time and alpha value.
+	 * @param time The current time of the animation.
+	 * @param boneSlot The bone slot to which the deform data will be applied.
+	 * @param alpha The alpha value for tweening between keyframes, default is 1.
+	 * @zh 根据给定的时间和 alpha 值将变形数据应用到骨骼插槽。
+	 * @param time 当前动画的时间。
+	 * @param boneSlot 应用变形数据的骨骼插槽。
+	 * @param alpha 用于在关键帧之间补间的 alpha 值，默认为 1。
+	 */
 	//TODO:coverage
 	apply(time: number, boneSlot: BoneSlot, alpha: number = 1): void {
 		time += 0.05;
--- src/layaAir/laya/ani/bone/SlotData.ts
+++ src/layaAir/laya/ani/bone/SlotData.ts
@@ -1,11 +1,25 @@
 import { SkinSlotDisplayData } from "./SkinSlotDisplayData";
+/**
+ * Represents the data of a slot, which includes the slot's name and an array of display data.
+ * @zh 表示插槽的数据，包括插槽名称和显示数据数组。
+ */
 export class SlotData {
+	/**
+	* @en The name of the slot.
+	* @zh 插槽的名称。
+	*/
 	name: string;
+	/**
+	 * @en An array of display data that this slot contains.
+	 * @zh 此插槽包含的显示数据数组。
+	 */
 	displayArr: any[] = [];
 
 	/**
-	 * 通过附件名称获取位置
-	 * @param name 
+	 * @en Get the index of the display data by attachment name.
+	 * @param name The name of the attachment to search for.
+	 * @zh 通过附件名称获取显示数据的位置。
+	 * @param name 附件名称。
 	 */
 	getDisplayByName(name: string): number {
 		var tDisplay: SkinSlotDisplayData;
