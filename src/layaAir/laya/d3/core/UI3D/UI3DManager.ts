82e0946a22efe844e33c67b27ae45c03346c5e7a
LayaCharley <399050@qq.com>
2024-08-14 09:23:58 +0800

    style: Code comments for internationalizing all classes in the `d3/core/UI3D` directory(国际化`d3/core/UI3D`目录下所有类的代码注释)

--- src/layaAir/laya/d3/core/UI3D/UI3DManager.ts
+++ src/layaAir/laya/d3/core/UI3D/UI3DManager.ts
@@ -2,22 +2,46 @@ import { FastSinglelist } from "../../../utils/SingletonList";
 import { Ray } from "../../math/Ray";
 import { UI3D } from "./UI3D";
 
+/**
+ * @en UI3DManager calss is used to create 3D UI manager, which manages all 3D UI objects.
+ * @zh UI3DManager 类用于创建3D-UI管理器，管理所有3D-UI对象。
+ */
 export class UI3DManager {
 
+    /**
+     * @en A list to store 3D UI elements.
+     * @zh 存储3D-UI元素的列表。
+     */
     _UI3Dlist: FastSinglelist<UI3D> = new FastSinglelist<UI3D>();
 
+    /** @ignore */
     constructor() {
     }
 
+    /**
+     * @en Adds a 3D UI element to the manager.
+     * @param value The 3D UI element to add.
+     * @zh 向管理器添加一个3D-UI元素。
+     * @param value 要添加的3D-UI元素。
+     */
     add(value: UI3D) {
         this._UI3Dlist.add(value);
     }
 
+    /**
+     * @en Removes a 3D UI element from the manager.
+     * @param value The 3D UI element to remove.
+     * @zh 从管理器中移除一个3D-UI元素。
+     * @param value 要移除的3D-UI元素。
+     */
     remove(value: UI3D) {
         this._UI3Dlist.remove(value);
     }
 
-    //需要在
+    /**
+     * @en Updates the 3D UI elements in the scene.
+     * @zh 更新场景中的3D-UI元素。
+     */
     update() {
         let elements = this._UI3Dlist.elements;
         for (var i = 0, n = this._UI3Dlist.length; i < n; i++) {
@@ -26,7 +50,12 @@ export class UI3DManager {
     }
 
     /**
-     * 判断是否碰撞
+     * @en Performs a ray cast to determine if there is a collision with any 3D UI elements.
+     * @param ray The ray for the ray casting.
+     * @returns The hit result or null if no collision occurs.
+     * @zh 执行射线投射以判断是否与任何3D-UI元素发生碰撞。
+     * @param ray 射线投射的射线。
+     * @returns 返回碰撞结果或如果没有碰撞发生则返回null。
      */
     rayCast(ray: Ray): any {
         let rayOri = ray.origin;
@@ -48,7 +77,8 @@ export class UI3DManager {
     }
 
     /**
-     * Destroy
+     * @en Destroys the 3D UI elements managed by the list.
+     * @zh 销毁列表中存储的3D-UI元素。
      */
     destory() {
         this._UI3Dlist.destroy();
