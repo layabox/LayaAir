c5d63d4dcbd6a8542138d49fa4d8b02c0d9c3f15
LayaCharley <399050@qq.com>
2024-08-21 20:33:49 +0800

    style: Code comments for internationalizing all classes in the `map` directory(国际化`map`目录下所有类的代码注释)

--- src/layaAir/laya/map/GridSprite.ts
+++ src/layaAir/laya/map/GridSprite.ts
@@ -2,34 +2,56 @@ import { TiledMap } from "./TiledMap";
 import { TileAniSprite } from "./TileAniSprite";
 import { Sprite } from "../display/Sprite";
 
-
-
 /**
- * 地图的每层都会分块渲染处理
- * 本类就是地图的块数据
- * @author ...
+ * @en Each layer of the map is rendered in chunks. This class represents the chunk data of the map.
+ * @zh 地图的每层都会分块渲染处理，本类就是地图的块数据。
  */
 export class GridSprite extends Sprite {
 
-    /**相对于地图X轴的坐标*/
+    /**
+     * @en The coordinate relative to the X-axis of the map.
+     * @zh 相对于地图X轴的坐标。
+     */
     relativeX: number = 0;
-    /**相对于地图Y轴的坐标*/
+    /**
+     * @en The coordinate relative to the Y-axis of the map.
+     * @zh 相对于地图Y轴的坐标。
+     */
     relativeY: number = 0;
-    /**是否用于对象层的独立物件*/
+    /**
+     * @en Indicates whether the object is an independent entity within the object layer.
+     * @zh 是否用于对象层的独立物件。
+     */
     isAloneObject: boolean = false;
-    /**当前GRID中是否有动画*/
+    /**
+     * @en Indicates whether there is an animation in the current GRID.
+     * @zh 当前GRID中是否有动画。
+     */
     isHaveAnimation: boolean = false;
-    /**当前GRID包含的动画*/
+    /**
+     * @en The array containing animations within the current GRID.
+     * @zh 当前GRID包含的动画数组。
+     */
     aniSpriteArray: any[];
-    /**当前GRID包含多少个TILE(包含动画)*/
+    /**
+     * @en The number of TILEs (including animations) included in the current GRID.
+     * @zh 当前GRID包含多少个TILE(包含动画)。
+     */
     drawImageNum: number = 0;
 
+    /**
+     * @en Reference to the current map object.
+     * @zh 当前地图对象的引用。
+     */
     private _map: TiledMap = null;//当前地图对象的引用
 
     /**
-     * 传入必要的参数，用于裁剪，跟确认此对象类型
-     * @param	map	把地图的引用传进来，参与一些裁剪计算
-     * @param	objectKey true:表示当前GridSprite是个活动对象，可以控制，false:地图层的组成块
+     * @en Initialize the data with necessary parameters for clipping and confirming the type of this object.
+     * @param map The reference to the map, used for some clipping calculations.
+     * @param objectKey If true, indicates that the current GridSprite is an active object that can be controlled; if false, it is a block of the map layer.
+     * @zh 传入必要的参数，用于裁剪，跟确认此对象类型。
+     * @param map 把地图的引用传进来，参与一些裁剪计算。
+     * @param objectKey true:表示当前GridSprite是个活动对象，可以控制，false:地图层的组成块。
      */
     initData(map: TiledMap, objectKey: boolean = false): void {
         this._map = map;
@@ -37,8 +59,10 @@ export class GridSprite extends Sprite {
     }
 
     /**
-     * 把一个动画对象绑定到当前GridSprite
-     * @param	sprite 动画的显示对象
+     * @en Bind an animation object to the current GridSprite.
+     * @param sprite The display object of the animation.
+     * @zh 把一个动画对象绑定到当前GridSprite。
+     * @param sprite 动画的显示对象。
      */
     addAniSprite(sprite: TileAniSprite): void {
         if (this.aniSpriteArray == null) {
@@ -48,7 +72,8 @@ export class GridSprite extends Sprite {
     }
 
     /**
-     * 显示当前GridSprite，并把上面的动画全部显示
+     * @en Display the current GridSprite and show all animations on it.
+     * @zh 显示当前GridSprite，并把上面的动画全部显示。
      */
     show(): void {
         if (!this.visible) {
@@ -65,7 +90,8 @@ export class GridSprite extends Sprite {
     }
 
     /**
-     * 隐藏当前GridSprite，并把上面绑定的动画全部移除
+     * @en Hide the current GridSprite and remove all animations bound to it.
+     * @zh 隐藏当前GridSprite，并把上面绑定的动画全部移除。
      */
     hide(): void {
         if (this.visible) {
@@ -82,7 +108,8 @@ export class GridSprite extends Sprite {
     }
 
     /**
-     * 刷新坐标，当我们自己控制一个GridSprite移动时，需要调用此函数，手动刷新
+     * @en Refresh the position coordinates. Call this function manually to refresh when you control the movement of a GridSprite.
+     * @zh 当我们自己控制一个GridSprite移动时，需要调用此函数，手动刷新坐标。
      */
     updatePos(): void {
         if (this.isAloneObject) {
@@ -104,7 +131,8 @@ export class GridSprite extends Sprite {
     }
 
     /**
-     * 重置当前对象的所有属性
+     * @en Reset all properties of the current object.
+     * @zh 重置当前对象的所有属性。
      */
     clearAll(): void {
         if (this._map) {
