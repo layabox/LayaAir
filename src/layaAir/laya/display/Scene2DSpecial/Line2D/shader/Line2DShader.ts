77fe8300f7ccc3522ac4ef523cfd6c57abeb3d5d
lirongf <619213582@qq.com>
2024-11-22 11:49:49 +0800

    fixed:Supplement the function submission of line2DRender(补充line2DRender的功能提交)

--- src/layaAir/laya/display/Scene2DSpecial/Line2D/shader/Line2DShader.ts
+++ src/layaAir/laya/display/Scene2DSpecial/Line2D/shader/Line2DShader.ts
@@ -1,10 +1,7 @@
-
-import { Laya } from "../../../../../Laya";
 import { LayaGL } from "../../../../layagl/LayaGL";
 import { IIndexBuffer } from "../../../../RenderDriver/DriverDesign/RenderDevice/IIndexBuffer";
 import { IVertexBuffer } from "../../../../RenderDriver/DriverDesign/RenderDevice/IVertexBuffer";
 import { ShaderDataType } from "../../../../RenderDriver/DriverDesign/RenderDevice/ShaderData";
-import { ShaderDefine } from "../../../../RenderDriver/RenderModuleData/Design/ShaderDefine";
 import { BufferUsage } from "../../../../RenderEngine/RenderEnum/BufferTargetType";
 import { Shader3D, ShaderFeatureType } from "../../../../RenderEngine/RenderShader/Shader3D";
 import { SubShader } from "../../../../RenderEngine/RenderShader/SubShader";
@@ -17,6 +14,7 @@ import LineVs from "./2DLine.vs";
 export class LineShader {
     static LINEWIDTH: number;
     static DASHED: number;
+    static TILINGOFFSET: number;
 
     static linePoisitionDesc: VertexDeclaration;
 
@@ -38,7 +36,6 @@ export class LineShader {
         LineShader._isInit = true;
         let attributeMap: { [name: string]: [number, ShaderDataType] } = {
             'a_position': [0, ShaderDataType.Vector3],
-            'a_uv': [1, ShaderDataType.Vector2],
             'a_linePos': [2, ShaderDataType.Vector4],
             "a_linelength": [3, ShaderDataType.Float],
         };
@@ -52,45 +49,41 @@ export class LineShader {
 
         LineShader.LINEWIDTH = Shader3D.propertyNameToID("u_lineWidth");
         LineShader.DASHED = Shader3D.propertyNameToID("u_dashed");
+        LineShader.TILINGOFFSET = Shader3D.propertyNameToID("u_TilingOffset");
 
 
         const commandUniform = LayaGL.renderDeviceFactory.createGlobalUniformMap("Line2DRender");
         commandUniform.addShaderUniform(LineShader.LINEWIDTH, "u_lineWidth", ShaderDataType.Float);
-        commandUniform.addShaderUniform(LineShader.LINEWIDTH, "u_dashed", ShaderDataType.Vector3);
+        commandUniform.addShaderUniform(LineShader.DASHED, "u_dashed", ShaderDataType.Vector3);
+        commandUniform.addShaderUniform(LineShader.TILINGOFFSET, "u_TilingOffset", ShaderDataType.Vector4);
 
-        if (!LineShader._vbs) {
-            let vertexs: Float32Array = new Float32Array([
-                -0.5, -0.5, 0, 0, 0,
-                0.5, -0.5, 0, 1, 0,
-                0.5, 0.5, 0, 1, 1,
-                - 0.5, 0.5, 0, 0, 1]);
-            let index: Uint16Array = new Uint16Array([0, 1, 2, 0, 2, 3]);
-            var declaration = new VertexDeclaration(20, [
-                new VertexElement(0, VertexElementFormat.Vector3, 0),
-                new VertexElement(12, VertexElementFormat.Vector2, 1),
-            ]);
-            let vertex = LineShader._vbs = LayaGL.renderDeviceFactory.createVertexBuffer(BufferUsage.Dynamic);
-            vertex.vertexDeclaration = declaration;
-            vertex.instanceBuffer = false;
-            vertex.setDataLength(vertexs.byteLength);
-            vertex.setData(vertexs.buffer, 0, 0, vertexs.byteLength);
 
-            let ibs = LineShader._ibs = LayaGL.renderDeviceFactory.createIndexBuffer(BufferUsage.Dynamic);
-            ibs._setIndexDataLength(index.buffer.byteLength);
-            ibs._setIndexData(index, 0);
-        }
+        let vertexs: Float32Array = new Float32Array([
+            -0.5, -0.5, 0,
+            0.5, -0.5, 0,
+            0.5, 0.5, 0,
+            - 0.5, 0.5, 0]);
+        let index: Uint16Array = new Uint16Array([0, 1, 2, 0, 2, 3]);
+        var declaration = new VertexDeclaration(12, [
+            new VertexElement(0, VertexElementFormat.Vector3, 0),
+        ]);
+        let vertex = LineShader._vbs = LayaGL.renderDeviceFactory.createVertexBuffer(BufferUsage.Dynamic);
+        vertex.vertexDeclaration = declaration;
+        vertex.instanceBuffer = false;
+        vertex.setDataLength(vertexs.byteLength);
+        vertex.setData(vertexs.buffer, 0, 0, vertexs.byteLength);
 
-        if (!LineShader.linePoisitionDesc) {
-            LineShader.linePoisitionDesc = new VertexDeclaration(16, [
-                new VertexElement(0, VertexElementFormat.Vector4, 2),
-            ]);
-        }
+        let ibs = LineShader._ibs = LayaGL.renderDeviceFactory.createIndexBuffer(BufferUsage.Dynamic);
+        ibs._setIndexDataLength(index.buffer.byteLength);
+        ibs._setIndexData(index, 0);
 
-        if (!LineShader.lineLengthDesc) {
-            LineShader.lineLengthDesc = new VertexDeclaration(4, [
-                new VertexElement(0, VertexElementFormat.Single, 3),
-            ]);
-        }
+        LineShader.linePoisitionDesc = new VertexDeclaration(16, [
+            new VertexElement(0, VertexElementFormat.Vector4, 2),
+        ]);
+
+        LineShader.lineLengthDesc = new VertexDeclaration(4, [
+            new VertexElement(0, VertexElementFormat.Single, 3),
+        ]);
 
     }
 }
