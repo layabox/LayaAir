2fd660fddc5aa1017b92b52f750de56ac8524dc8
guzhu <guzhu@fairygui.com>
2024-12-20 15:40:47 +0800

    optimize: hittest of Area2D

--- src/layaAir/laya/display/Area2D.ts
+++ src/layaAir/laya/display/Area2D.ts
@@ -3,10 +3,18 @@ import { Camera2D } from "./Scene2DSpecial/Camera2D";
 import { Sprite } from "./Sprite";
 import { Scene } from "./Scene";
 import { Node } from "./Node";
+import { Point } from "../maths/Point";
+import { RenderState2D } from "../webgl/utils/RenderState2D";
+import { NodeFlags } from "../Const";
 
 export class Area2D extends Sprite {
     private _mainCamera: Camera2D;
     declare _scene: Scene;
+
+    constructor() {
+        super();
+        this._setBit(NodeFlags.AREA_2D, true);
+    }
 
     get mainCamera(): Camera2D {
         return this._mainCamera;
@@ -72,5 +80,34 @@ export class Area2D extends Sprite {
         }
 
         super._setUnBelongScene();
+    }
+
+    /**
+     * @en Convert screen coordinates to Area2D internal UI coordinates.
+     * @param x The x axis of screen coordinates.
+     * @param y The y axis of screen coordinates.
+     * @param out The output point. If not passed, a new point will be created.
+     * @returns The output point. 
+     * @zh 将屏幕坐标转换为Area2D内部UI坐标
+     * @param x 屏幕坐标的x轴.
+     * @param y 屏幕坐标的y轴.
+     * @param out 输出点，如果不传入，则会创建一个新的点。
+     * @returns 输出点。
+     */
+    transformPoint(x: number, y: number, out?: Point): Point {
+        out = out || new Point();
+        out.setTo(x, y);
+        if (!this._mainCamera)
+            return out;
+
+        //如果之前没有Camera，根据scene计算screen坐标
+        this.localToGlobal(out);
+        //根据camera计算实际的world坐标
+        this.mainCamera.localToGlobal(out);
+        //根据实际坐标计算scene的实际local坐标
+        out.x -= RenderState2D.width * 0.5;
+        out.y -= RenderState2D.height * 0.5;
+        this.globalToLocal(out);
+        return out;
     }
 }
