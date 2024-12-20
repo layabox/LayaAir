f00306fb45e33e3f3923eabb543c5d7db5642571
Adonis2016 <cpi2012@qq.com>
2024-12-20 15:40:05 +0800

    add: two class for Light2D (灯光系统新增两个文件）

---
+++ src/layaAir/laya/display/Scene2DSpecial/Light2D/Light2DConfig.ts
@@ -0,0 +1,10 @@
+import { Color } from "../../../maths/Color";
+
+/**
+ * 2D灯光全局配置参数
+ */
+export class Light2DConfig {
+    lightHeight: number = 1; //灯光高度（0~1，影响法线效果）
+    ambient: Color = new Color(0.25, 0.25, 0.25, 0); //环境光
+    ambientLayerMask: number = 1; //环境光影响的层
+}
