70115534c9e67e727755ad88c49fe0f5b6283be4
QTEvD <agp62601@gmail.com>
2024-12-27 14:48:08 +0800

    fix:  add Circle2DShape.ts

--- src/samples/index.ts
+++ src/samples/index.ts
@@ -10,9 +10,13 @@ import { Physics2D } from "laya/physics/Physics2D";
 import { physics2DwasmFactory } from "laya/physics/factory/physics2DwasmFactory"
 import { RenderCMD2DDemo } from "./2d/RenderCMD2DDemo";
 import { SceneLoad1 } from "./3d/LayaAir3D_Scene3D/SceneLoad1";
+import { SceneLoad } from "./self/SceneLoad";
+import { UBOLayoutTest } from "./self/UBOLayoutTest";
 
 Resource.DEBUG = true;
 LayaGL.renderOBJCreate = new WebGLRenderEngineFactory();
 Physics2D.I._factory = new physics2DwasmFactory();
 Laya3D.PhysicsCreateUtil = new btPhysicsCreateUtil();
-new Main(true, false);
+// new Main(true, false);
+
+new UBOLayoutTest();
