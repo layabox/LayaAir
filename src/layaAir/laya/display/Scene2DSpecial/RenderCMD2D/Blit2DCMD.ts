2cfb0bbaf772cb3b33f37f92269ef1607cfe065e
Adonis2016 <cpi2012@qq.com>
2025-01-02 11:24:33 +0800

    bugfix: Blit2DCMD UpsideDown，Blit2DQuadCMD clear background（修改Bug，Blit2DCMD对INVERTY的支持，Blit2DQuadCMD清除了背景色）

--- src/layaAir/laya/display/Scene2DSpecial/RenderCMD2D/Blit2DCMD.ts
+++ src/layaAir/laya/display/Scene2DSpecial/RenderCMD2D/Blit2DCMD.ts
@@ -40,6 +40,10 @@ export class Blit2DCMD extends Command2D {
             gl_Position = vec4(u_OffsetScale.x * 2.0 - 1.0 + (a_PositionTexcoord.x + 1.0) * u_OffsetScale.z, (1.0 - ((u_OffsetScale.y * 2.0 - 1.0 + (-a_PositionTexcoord.y + 1.0) * u_OffsetScale.w) + 1.0) / 2.0) * 2.0 - 1.0, 0.0, 1.0);
 
             v_Texcoord0 = a_PositionTexcoord.zw;
+
+            #ifdef INVERTY
+            gl_Position.y = -gl_Position.y;
+            #endif
         }
         `;
         let blitfs = `
