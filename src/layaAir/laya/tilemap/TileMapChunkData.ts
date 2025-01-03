f1be071304d89b191f49ea8a114b1f6a22062bbd
Lurenyiabc <934555872@qq.com>
2025-01-03 20:47:16 +0800

    optimize:Tilemap supports reorganizing rendering data when adjusting animation frame rates.(tilemap 支持动画帧数调整时，重新组织渲染数据)

--- src/layaAir/laya/tilemap/TileMapChunkData.ts
+++ src/layaAir/laya/tilemap/TileMapChunkData.ts
@@ -169,8 +169,6 @@ export class TileMapChunkData {
             if (localIndexs) {
                 let cellData = tileSet.getCellDataByGid(gid);
                 if (cellData){//todo 算数据丢失
-                    if (cellData.cellowner._hasAni())
-                        this._animatorAlterArray.set(cellData.cellowner.nativeId, cellData.cellowner);
                     cellData._addNoticeRenderTile(this);
                     for (let i = 0, len = localIndexs.length; i < len; i++) {
                         let index = localIndexs[i];
@@ -317,12 +315,18 @@ export class TileMapChunkData {
             }
             let lastCell;
             let tempCellIndexArray: ChunkCellInfo[] = [];
+
+            this._animatorAlterArray.clear();
+
             for (var i = 0; i < this._chuckCellList.length; i++) {
                 // let cellIndex = this._softList[i].xOrider;
                 let chuckCellInfoData = this._chuckCellList[i];
                 let cellData = chuckCellInfoData.cell;
                 if (!cellData)
                     continue;
+
+                if (cellData.cellowner._hasAni())
+                    this._animatorAlterArray.set(cellData.cellowner.nativeId, cellData.cellowner);
 
                 if (!lastCell) {
                     lastCell = cellData;
@@ -333,6 +337,7 @@ export class TileMapChunkData {
                     lastCell = cellData;
                     this._createRenderElement(tempCellIndexArray)
                 }
+
                 tempCellIndexArray.push(chuckCellInfoData);
             }
             this._createRenderElement(tempCellIndexArray);
@@ -769,9 +774,6 @@ export class TileMapChunkData {
             cellData._addNoticeRenderTile(this);
         }
 
-        if (cellData.cellowner._hasAni())
-            this._animatorAlterArray.set(cellData.cellowner.nativeId, cellData.cellowner);
-
         let chuckCellInfo = this._cellDataMap[index];
         if (chuckCellInfo == null) {//create one ChunkCellInfo
             let chunk = this._tileLayer._chunk;
@@ -880,6 +882,10 @@ export class TileMapChunkData {
             let flags = this._dirtyFlags[type];
             flags.set(gid, flags.get(gid) | flag);
         }
+    }
+
+    modifyRenderData(){
+        this._reCreateRenderData = true;
     }
 
     /**
