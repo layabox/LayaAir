5901759db8581e770f8951ac16e2abdb682654a5
Lurenyiabc <934555872@qq.com>
2025-03-26 16:07:27 +0800

    fix: 1. Fix issue with DrawMesh2DCMD.setColor not working.(DrawMesh2DCMD.setColor 无效的问题) 2. Add compatibility for spine's ColorFilter.(兼容spine的ColorFilter) 3. Fix Tile Terrain algorithm issue.(Tile的Terrain算法问题)

--- src/layaAir/laya/NodeRender2D/BaseRenderNode2D.ts
+++ src/layaAir/laya/NodeRender2D/BaseRenderNode2D.ts
@@ -110,6 +110,9 @@ export class BaseRenderNode2D extends Component {
         commandUniform.addShaderUniform(BaseRenderNode2D.NORMAL2DSTRENGTH, "u_normal2DStrength", ShaderDataType.Float);
         commandUniform.addShaderUniform(ShaderDefines2D.UNIFORM_CLIPMATDIR, "u_clipMatDir", ShaderDataType.Vector4);
         commandUniform.addShaderUniform(ShaderDefines2D.UNIFORM_CLIPMATPOS, "u_clipMatPos", ShaderDataType.Vector2);
+        //兼容 COlOLR FILTER
+        commandUniform.addShaderUniform(ShaderDefines2D.UNIFORM_COLORALPHA, "u_colorAlpha", ShaderDataType.Vector4);
+        commandUniform.addShaderUniform(ShaderDefines2D.UNIFORM_COLORMAT, "u_colorMat", ShaderDataType.Matrix4x4);
     }
 
     /**
