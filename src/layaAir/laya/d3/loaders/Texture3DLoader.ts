71a229a2a9a93be9f3547e3dd40b7cfe8d6b407b
guozhaokui <guozhaokuiwang@gmail.com>
2025-06-26 10:23:25 +0800

    tex2darray 里面的路径是相对于tex2darray文件的。

--- src/layaAir/laya/d3/loaders/Texture3DLoader.ts
+++ src/layaAir/laya/d3/loaders/Texture3DLoader.ts
@@ -1,6 +1,7 @@
 import { RenderCapable } from "../../RenderEngine/RenderEnum/RenderCapable";
 import { LayaGL } from "../../layagl/LayaGL";
 import { ILoadTask, IResourceLoader, Loader } from "../../net/Loader";
+import { URL } from "../../net/URL";
 import { Texture2DArray } from "../../resource/Texture2DArray";
 import { Utils } from "../../utils/Utils";
 
@@ -55,11 +56,10 @@ export class Texture2DArrayLoader implements IResourceLoader {
             let textures: Array<string> = data.textures;
 
             let urls = [];
-            for (let index = 0; index < textures.length; index++) {
-                urls.push(textures[index]);
-            }
-
-
+            let basePath: string = URL.getPath(task.url);
+            for (let i = textures.length - 1; i >= 0; i--) {
+                urls.unshift(URL.join(basePath, textures[i]));
+            }            
             let options = task.options;
             return Promise.all(urls.map((url) => {
                 if (url) {
