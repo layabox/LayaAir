f08af918d9692301df534e94ff8d81fb67bf4d55
guzhu <guzhu@fairygui.com>
2025-07-03 18:14:06 +0800

    fix: fix last commit

--- src/layaAir/laya/display/Sprite.ts
+++ src/layaAir/laya/display/Sprite.ts
@@ -2180,13 +2180,14 @@ export class Sprite extends Node {
             if (this._subStruct) {
                 this._subStruct.enabled = b;
             }
-            if (b) {
-                this.repaint();
-                ILaya.stage.passManager.addPass(this._oriRenderPass);
-            } else {
-                ILaya.stage.passManager.removePass(this._oriRenderPass);
-            }
+            if (b) this.repaint();
             this.parentRepaint();
+            if (this._oriRenderPass) {
+                if (b)
+                    ILaya.stage.passManager.addPass(this._oriRenderPass);
+                else
+                    ILaya.stage.passManager.removePass(this._oriRenderPass);
+            }
             return true;
         }
         else
