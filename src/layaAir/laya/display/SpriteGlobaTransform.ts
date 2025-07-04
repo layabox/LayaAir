a73e66d5ca7c151617f438a3e7034d11d7caecd9
Lurenyiabc <934555872@qq.com>
2025-07-04 15:45:00 +0800

    fix:Incorrect flag update

--- src/layaAir/laya/display/SpriteGlobaTransform.ts
+++ src/layaAir/laya/display/SpriteGlobaTransform.ts
@@ -62,7 +62,7 @@ export class SpriteGlobalTransform {
         if (sp._parent) {
             Matrix.mul(this._matrix, sp._parent.globalTrans.getMatrix(), this._matrix);
             this._setFlag(TransformKind.Matrix, false);
-            this._syncFlag(TransformKind.Matrix, true);
+            // this._syncFlag(TransformKind.Matrix, true);
         }
         return this._matrix;
     }
