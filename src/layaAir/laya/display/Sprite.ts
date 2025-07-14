9b8fffb9dd3749427fb3d927f4e77ddc2d775439
Lurenyiabc <934555872@qq.com>
2025-07-14 16:07:12 +0800

    fix: The problem of calculating the width and height of Text.(Text 的宽高计算问题)

--- src/layaAir/laya/display/Sprite.ts
+++ src/layaAir/laya/display/Sprite.ts
@@ -1368,8 +1368,21 @@ export class Sprite extends Node {
             if (this._getBit(NodeFlags.DEMAND_TRANS_EVENT))
                 notifyTransChanged(this);
         }
-        else if (kind & TransformKind.Size && (this._pivotX !== 0 || this._pivotY !== 0)) { // 有锚点宽高变化也需要刷新矩阵
-            this._globalTrans._spTransChanged(kind);
+        else if (kind & TransformKind.Size) {
+            let needUpdate = false;
+            if (this._anchorX !== 0 || this._anchorY !== 0) {
+                this._pivotX = this._anchorX * this._width;
+                this._pivotY = this._anchorY * this._height;
+                needUpdate = true;
+            } else if (this._pivotX !== 0 || this._pivotY !== 0) {
+                this._anchorX = this._pivotX / this._width;
+                this._anchorY = this._pivotY / this._height;
+                needUpdate = true;
+            }
+
+            if (needUpdate) {
+                this._globalTrans._spTransChanged(kind);
+            }
         }
     }
 
