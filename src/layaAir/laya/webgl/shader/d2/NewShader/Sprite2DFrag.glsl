e65e6fefdae2c74dbed7fd0a74c95cb3cc8bf2a9
Lurenyiabc <934555872@qq.com>
2025-09-18 09:42:39 +0800

    fix:Supplementary submission of lighting code.(灯光代码补充提交)

--- src/layaAir/laya/webgl/shader/d2/NewShader/Sprite2DFrag.glsl
+++ src/layaAir/laya/webgl/shader/d2/NewShader/Sprite2DFrag.glsl
@@ -166,4 +166,8 @@ void clip(){
     if(v_cliped.x>1.) discard;
     if(v_cliped.y<0.) discard;
     if(v_cliped.y>1.) discard;
+    // if(v_cliped.x<0.) gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    // if(v_cliped.x>1.) gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    // if(v_cliped.y<0.) gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+    // if(v_cliped.y>1.) gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
 }
