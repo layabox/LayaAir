f95d742755f7c328ede6354ebf961f6f0c2fae47
guzhu <guzhu@fairygui.com>
2025-09-24 16:32:48 +0800

    fix: fixed bug in ColorEffect2D.setColor

--- src/layaAir/laya/display/effect2d/ColorEffect2D.ts
+++ src/layaAir/laya/display/effect2d/ColorEffect2D.ts
@@ -12,7 +12,6 @@ import { Shader3D } from "../../RenderEngine/RenderShader/Shader3D";
 import { Material } from "../../resource/Material";
 import { RenderTexture2D } from "../../resource/RenderTexture2D";
 import { ClassUtils } from "../../utils/ClassUtils";
-import { ColorUtils } from "../../utils/ColorUtils";
 import { PostProcess2D, PostProcessRenderContext2D } from "../PostProcess2D";
 import { PostProcess2DEffect } from "../PostProcess2DEffect";
 import { Blit2DCMD } from "../Scene2DSpecial/RenderCMD2D/Blit2DCMD";
@@ -21,23 +20,22 @@ import { Blit2DCMD } from "../Scene2DSpecial/RenderCMD2D/Blit2DCMD";
  * @en An array representing a list of contrast values.
  * @zh 表示对比度值列表的数组。
  */
-const DELTA_INDEX: any[] = [0, 0.01, 0.02, 0.04, 0.05, 0.06, 0.07, 0.08, 0.1, 0.11, 0.12, 0.14, 0.15, 0.16, 0.17, 0.18, 0.20, 0.21, 0.22, 0.24, 0.25, 0.27, 0.28, 0.30, 0.32, 0.34, 0.36, 0.38, 0.40, 0.42, 0.44, 0.46, 0.48, 0.5, 0.53, 0.56, 0.59, 0.62, 0.65, 0.68, 0.71, 0.74, 0.77, 0.80, 0.83, 0.86, 0.89, 0.92, 0.95, 0.98, 1.0, 1.06, 1.12, 1.18, 1.24, 1.30, 1.36, 1.42, 1.48, 1.54, 1.60, 1.66, 1.72, 1.78, 1.84, 1.90, 1.96, 2.0, 2.12, 2.25, 2.37, 2.50, 2.62, 2.75, 2.87, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.3, 4.7, 4.9, 5.0, 5.5, 6.0, 6.5, 6.8, 7.0, 7.3, 7.5, 7.8, 8.0, 8.4, 8.7, 9.0, 9.4, 9.6, 9.8, 10.0];
+const DELTA_INDEX = [0, 0.01, 0.02, 0.04, 0.05, 0.06, 0.07, 0.08, 0.1, 0.11, 0.12, 0.14, 0.15, 0.16, 0.17, 0.18, 0.20, 0.21, 0.22, 0.24, 0.25, 0.27, 0.28, 0.30, 0.32, 0.34, 0.36, 0.38, 0.40, 0.42, 0.44, 0.46, 0.48, 0.5, 0.53, 0.56, 0.59, 0.62, 0.65, 0.68, 0.71, 0.74, 0.77, 0.80, 0.83, 0.86, 0.89, 0.92, 0.95, 0.98, 1.0, 1.06, 1.12, 1.18, 1.24, 1.30, 1.36, 1.42, 1.48, 1.54, 1.60, 1.66, 1.72, 1.78, 1.84, 1.90, 1.96, 2.0, 2.12, 2.25, 2.37, 2.50, 2.62, 2.75, 2.87, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.3, 4.7, 4.9, 5.0, 5.5, 6.0, 6.5, 6.8, 7.0, 7.3, 7.5, 7.8, 8.0, 8.4, 8.7, 9.0, 9.4, 9.6, 9.8, 10.0];
 /**
  * @en An array representing a gray matrix.
  * @zh 表示灰色矩阵的数组。
  */
-const GRAY_MATRIX: any[] = [0.3086, 0.6094, 0.082, 0, 0, 0.3086, 0.6094, 0.082, 0, 0, 0.3086, 0.6094, 0.082, 0, 0, 0, 0, 0, 1, 0];
+const GRAY_MATRIX = [0.3086, 0.6094, 0.082, 0, 0, 0.3086, 0.6094, 0.082, 0, 0, 0.3086, 0.6094, 0.082, 0, 0, 0, 0, 0, 1, 0];
 /**
  * @en An array representing an identity matrix, which signifies no effect or change.
  * @zh 表示单位矩阵的数组，它表示没有效果或变化。
  */
-const IDENTITY_MATRIX: any[] = [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1];
+const IDENTITY_MATRIX = [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1];
 /**
  * @en Standard matrix length
  * @zh 标准矩阵长度。
  */
 const LENGTH: number = 25;
-
 
 export class ColorEffect2D extends PostProcess2DEffect {
     private _colorMat: Matrix4x4 = new Matrix4x4();
@@ -158,9 +156,8 @@ export class ColorEffect2D extends PostProcess2DEffect {
      * @param  color 颜色值
      */
     setColor(color: string): ColorEffect2D {
-        var arr = ColorUtils.create(color).arrColor;
-        var mt: number[] = [0, 0, 0, 0, 256 * arr[0], 0, 0, 0, 0, 256 * arr[1], 0, 0, 0, 0, 256 * arr[2], 0, 0, 0, 1, 0];
-        return this.setByMatrix(mt);
+        tmpColor.parse(color);
+        return this.color(tmpColor.r, tmpColor.g, tmpColor.b, 1);
     }
 
     /**
@@ -344,10 +341,8 @@ export class ColorEffect2D extends PostProcess2DEffect {
      * @zh 反序列化后调用。
      */
     onAfterDeserialize() {
-        if (SerializeUtil.hasProp("_color")) {
-            let arr: any[] = ColorUtils.create((<any>this)._color || "#FFFFFF").arrColor;
-            this.color(arr[0], arr[1], arr[2], arr[3]);
-        }
+        if (SerializeUtil.hasProp("_color"))
+            this.setColor((<any>this)._color);
         if (SerializeUtil.hasProp("_brightness", "_contrast", "_saturation", "_hue"))
             this.adjustColor((<any>this)._brightness || 0, (<any>this)._contrast || 0, (<any>this)._saturation || 0, (<any>this)._hue || 0);
     }
@@ -363,6 +358,8 @@ export class ColorEffect2D extends PostProcess2DEffect {
         this._renderElement && (this._renderElement.destroy());
     }
 }
+
+const tmpColor = new Color();
 
 ClassUtils.regClass("ColorEffect2D", ColorEffect2D);
 
