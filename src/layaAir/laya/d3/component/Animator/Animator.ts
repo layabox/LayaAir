37ec35746c98ecac8e432d90db84e0f490b1389a
zringhost11 <38626955@qq.com>
2025-10-28 12:23:00 +0800

    fix:修复当用户调用不存在的动画会抛出明确提示的error

--- src/layaAir/laya/d3/component/Animator/Animator.ts
+++ src/layaAir/laya/d3/component/Animator/Animator.ts
@@ -1548,7 +1548,7 @@ export class Animator extends Component {
 
             var animatorState: AnimatorState = name ? controllerLayer.getAnimatorState(name) : defaultState;
             if (!animatorState || !animatorState._clip) {
-                new Error("Animator:must have clip value,please set clip property.");
+                throw new Error("Animator:must have clip value,please set clip property.");
                 return;
             }
 
