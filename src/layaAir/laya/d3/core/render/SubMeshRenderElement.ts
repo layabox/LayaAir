6762459836f66eed24ee2c6e9f97eed4eb4cee93
guzhu <guzhu@fairygui.com>
2025-11-04 19:53:26 +0800

    fix: failed to remove event listener to transform in SubMeshRenderElement.destroy

--- src/layaAir/laya/d3/core/render/SubMeshRenderElement.ts
+++ src/layaAir/laya/d3/core/render/SubMeshRenderElement.ts
@@ -12,83 +12,86 @@ import { RenderElement } from "./RenderElement";
  */
 export class SubMeshRenderElement extends RenderElement {
 
-	/** @internal */
-	private _dynamicWorldPositionNormalNeedUpdate: boolean;
+    /** @internal */
+    private _dynamicWorldPositionNormalNeedUpdate: boolean;
 
-	/** @internal */
-	staticBatchIndexStart: number;
-	/** @internal */
-	staticBatchIndexEnd: number;
-	/** @internal */
-	staticBatchElementList: FastSinglelist<SubMeshRenderElement>;
+    /** @internal */
+    staticBatchIndexStart: number;
+    /** @internal */
+    staticBatchIndexEnd: number;
+    /** @internal */
+    staticBatchElementList: FastSinglelist<SubMeshRenderElement>;
 
-	/** @internal */
-	instanceSubMesh: SubMesh;
-	/** @internal */
-	instanceBatchElementList: FastSinglelist<SubMeshRenderElement>;
+    /** @internal */
+    instanceSubMesh: SubMesh;
+    /** @internal */
+    instanceBatchElementList: FastSinglelist<SubMeshRenderElement>;
 
-	/** @internal */
-	vertexBatchElementList: FastSinglelist<SubMeshRenderElement>;
-	/** @internal */
-	vertexBatchVertexDeclaration: VertexDeclaration;
-
-
-	/**
-	 * 创建一个 <code>SubMeshRenderElement</code> 实例。
-	 */
-	constructor() {
-		super();
-		this._dynamicWorldPositionNormalNeedUpdate = true;
-		this._renderElementOBJ.canDynamicBatch = true;
-	}
-
-	/**
-	 * @internal
-	 */
-	private _onWorldMatrixChanged(): void {
-		this._dynamicWorldPositionNormalNeedUpdate = true;
-	}
+    /** @internal */
+    vertexBatchElementList: FastSinglelist<SubMeshRenderElement>;
+    /** @internal */
+    vertexBatchVertexDeclaration: VertexDeclaration;
 
 
-	/**
-	 * @inheritDoc
-	 * @override
-	 */
-	setTransform(transform: Transform3D): void {
-		if (this.transform !== transform) {
-			(this.transform) && (this.transform.off(Event.TRANSFORM_CHANGED, this, this._onWorldMatrixChanged));
-			(transform) && (transform.on(Event.TRANSFORM_CHANGED, this, this._onWorldMatrixChanged));
-			this._dynamicWorldPositionNormalNeedUpdate = true;
-			this.transform = transform;
-		}
-	}
+    /**
+     * 创建一个 <code>SubMeshRenderElement</code> 实例。
+     */
+    constructor() {
+        super();
+        this._dynamicWorldPositionNormalNeedUpdate = true;
+        this._renderElementOBJ.canDynamicBatch = true;
+    }
 
-	/**
-	 * @inheritDoc
-	 * @override
-	 */
-	setGeometry(geometry: GeometryElement): void {
-		if (this._geometry !== geometry) {
-			this._geometry = geometry;
-			this._renderElementOBJ.geometry = geometry._geometryElementOBj;
-		}
-	}
+    /**
+     * @internal
+     */
+    private _onWorldMatrixChanged(): void {
+        this._dynamicWorldPositionNormalNeedUpdate = true;
+    }
 
-	/**
-	 * @inheritDoc
-	 * @override
-	 */
-	destroy(): void {
-		if (!this._renderElementOBJ) return;
-		(this.transform) && this.transform.off(Event.TRANSFORM_CHANGED, this, this._onWorldMatrixChanged);
-		super.destroy();
-		//this.staticBatch = null;
-		this.instanceSubMesh = null;
-		this.staticBatchElementList && this.staticBatchElementList.destroy();
-		this.instanceBatchElementList && this.instanceBatchElementList.destroy();
-		this.vertexBatchElementList && this.vertexBatchElementList.destroy();
-		this.vertexBatchVertexDeclaration = null;
-	}
+
+    /**
+     * @inheritDoc
+     * @override
+     */
+    setTransform(transform: Transform3D): void {
+        if (this.transform !== transform) {
+            (this.transform) && (this.transform.off(Event.TRANSFORM_CHANGED, this, this._onWorldMatrixChanged));
+            (transform) && (transform.on(Event.TRANSFORM_CHANGED, this, this._onWorldMatrixChanged));
+            this._dynamicWorldPositionNormalNeedUpdate = true;
+            this.transform = transform;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     * @override
+     */
+    setGeometry(geometry: GeometryElement): void {
+        if (this._geometry !== geometry) {
+            this._geometry = geometry;
+            this._renderElementOBJ.geometry = geometry._geometryElementOBj;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     * @override
+     */
+    destroy(): void {
+        if (!this._renderElementOBJ) return;
+        if (this._transform) {
+            this._transform.off(Event.TRANSFORM_CHANGED, this, this._onWorldMatrixChanged);
+            this._transform = null;
+        }
+        super.destroy();
+        //this.staticBatch = null;
+        this.instanceSubMesh = null;
+        this.staticBatchElementList && this.staticBatchElementList.destroy();
+        this.instanceBatchElementList && this.instanceBatchElementList.destroy();
+        this.vertexBatchElementList && this.vertexBatchElementList.destroy();
+        this.vertexBatchVertexDeclaration = null;
+    }
 }
 
 
