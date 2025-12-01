3a67e6febe5346c13b7af15f0f8e8efc296f4c2c
Lurenyiabc <934555872@qq.com>
2025-12-01 11:36:30 +0800

    fix: Templet.needSlot = (version == 4.1) => (version >= 4.1).

--- src/layaAir/laya/spine/SpineTempletLoader.ts
+++ src/layaAir/laya/spine/SpineTempletLoader.ts
@@ -33,12 +33,16 @@ class SpineTempletLoader implements IResourceLoader {
 
             let templet = new SpineTemplet();
             let version = SpineTemplet.RuntimeVersion;
-            if (version == "4.1") {
+            let versionString = version.split('.');
+            let versionNumber = Math.floor(Number(versionString[0]));
+            let versionNumber2 = Math.floor(Number(versionString[1]));
+
+            if (versionNumber >= 4 && versionNumber2 >= 1) {
                 templet.needSlot = true;
             }
 
             // debugger
-            if (version.startsWith('4.'))
+            if (versionNumber >= 4)
                 return this.parseAtlas4(res[0], res[1], task, templet);
             else
                 return this.parseAtlas3(res[0], res[1], task, templet);
