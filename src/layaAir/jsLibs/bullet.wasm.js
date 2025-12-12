064093cbba62d71986ce22d8d97eaf48e4dc792c
guzhu <guzhu@fairygui.com>
2025-12-12 11:42:56 +0800

    fix: bullet.wasm failed to load if version hash mark is enabled

--- src/layaAir/jsLibs/bullet.wasm.js
+++ src/layaAir/jsLibs/bullet.wasm.js
@@ -70,7 +70,7 @@ window.Physics3D = function (initialMemory, interactive) {
       p = Laya.WasmAdapter.instantiateWasm("bullet.wasm", imports);
     }
     else {
-      p = fetch((Laya.WasmAdapter.locateFile || locateFile)("bullet.wasm", scriptDirectory)).then((response) =>
+      p = fetch((Laya.WasmAdapter.locateFile || Laya.WasmAdapter.locateFileDefault)("bullet.wasm", scriptDirectory)).then((response) =>
         response.arrayBuffer().then((buffer) => WebAssembly.instantiate(buffer, imports)));
     }
 
