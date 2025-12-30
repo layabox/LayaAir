0532b9b6dcaf35d8b366d732aa0f5749fbd8c27c
LayaCharley <399050@qq.com>
2025-12-30 14:50:32 +0800

    chore: Added usage notes for the example entry.(增加示例入口的使用注释)

--- src/samples/index.ts
+++ src/samples/index.ts
@@ -17,7 +17,8 @@ import "laya/platform/TextInputAdapter";
 Resource.DEBUG = true;
 Physics2D.I._factory = new physics2DwasmFactory();
 Laya3D.PhysicsCreateUtil = new btPhysicsCreateUtil();
-var useWebGPU = true;
+/** 是否使用 WebGPU，使用的话需要设置为true */
+var useWebGPU = false;
 async function start(){
     if (useWebGPU) {
         var webGPUFile = './importWebGPU';//使用变量，避免tsc检查，因为有时候没有webgpu源码
@@ -25,7 +26,10 @@ async function start(){
     } else {
         await import ("./importWebGL")
     }
+    //初始化入口，如果需要默认访问某个指定DEMO，填入第三个参数
     new Main(false, false);
 }
 
 start();
+//启动后，可以在bin目录下启动anywhere等web服务访问
+//
