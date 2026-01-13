0b7965e054bf9b4fee9d5edf434e82960977af90
guzhu <guzhu@fairygui.com>
2026-01-13 15:42:03 +0800

    fix: LensFlareSettingsLoader path issue

--- src/layaAir/laya/d3/postProcessEffect/LensFlareSettingsLoader.ts
+++ src/layaAir/laya/d3/postProcessEffect/LensFlareSettingsLoader.ts
@@ -27,7 +27,7 @@ export class LensFlareSettingsLoader implements IResourceLoader {
                 return ret;
 
             let basePath = URL.getPath(task.url);
-            let textures: Array<string> = elements.map((e: any) => URL.join(basePath, e.texture?._$uuid)).filter((url: string) => url != "");
+            let textures: Array<string> = elements.map((e: any) => URL.join(basePath, URL.getResURLByUUID(e.texture?._$uuid))).filter((url: string) => url != "");
 
             return Promise.all(textures.map(url => task.loader.load(url, task.options))).then(() => {
                 ret.elements = new ObjDecoder().decodeObj(elements);
