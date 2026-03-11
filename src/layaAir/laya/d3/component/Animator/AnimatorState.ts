34781ecd1aebab2697f3e7ca6b93236f0b2779d9
INEEDSSD <hellomyworld1997@outlook.com>
2026-03-11 18:17:44 +0800

    feat: Added data recording for material changes during animation. 新功能：增加动画对材质更换的数据录制。

--- src/layaAir/laya/d3/component/Animator/AnimatorState.ts
+++ src/layaAir/laya/d3/component/Animator/AnimatorState.ts
@@ -75,7 +75,7 @@ export class AnimatorState extends EventDispatcher implements IClone {
      * @zh 实时数据数组，用于避免数据混淆。必须将实时数据存储在animatorState中，而不是animationClip中。
      * 这对于像crossFade()这样的操作是必要的，因为可能使用不同的animatorState但相同的片段源。
      */
-    _realtimeDatas: Array<number | Vector3 | Quaternion> = [];
+    _realtimeDatas: Array<number | string | Vector3 | Quaternion> = [];
 
     /** @internal */
     _scripts: AnimatorStateScript[] | null = null;
@@ -142,7 +142,7 @@ export class AnimatorState extends EventDispatcher implements IClone {
             if (this._clip)
                 (this._referenceCount > 0) && (this._clip._removeReference(this._referenceCount));
             if (value) {
-                var realtimeDatas: Array<number | Vector3 | Quaternion | Vector2 | Vector4> = this._realtimeDatas;
+                var realtimeDatas: Array<number | string | Vector3 | Quaternion | Vector2 | Vector4> = this._realtimeDatas;
                 var clipNodes: KeyframeNodeList = value._nodes!;
                 var count: number = clipNodes.count;
                 this._currentFrameIndices = new Int16Array(count);
@@ -171,6 +171,9 @@ export class AnimatorState extends EventDispatcher implements IClone {
                             realtimeDatas[i] = new Vector4();
                             break;
                         case KeyFrameValueType.PathPoint:
+                            break;
+                        case KeyFrameValueType.MaterialRef:
+                            realtimeDatas[i] = "";
                             break;
                         default:
                             throw new Error("AnimationClipParser04:unknown type.");
