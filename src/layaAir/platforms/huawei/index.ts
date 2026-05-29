81960f65c1ceae803f5f20a3446654d8d1da0655
INEEDSSD <hellomyworld1997@outlook.com>
2026-05-29 18:44:08 +0800

    Fix: Fixed a socket issue in Huawei mini-games. 修复: 修复华为小游戏Socket问题。

--- src/layaAir/platforms/huawei/index.ts
+++ src/layaAir/platforms/huawei/index.ts
@@ -1,3 +1,4 @@
+import { _WebSocket } from "../../laya/net/WebSocket";
 import { PAL } from "../../laya/platform/PlatformAdapters";
 import { Browser } from "../../laya/utils/Browser";
 import { MgBrowserAdapter } from "../minigame/MgBrowserAdapter";
@@ -6,3 +7,8 @@ MgBrowserAdapter.beforeInit = function () {
     Browser.onHWMiniGame = true;
     PAL.g = (window as any).hbs;
 };
+
+MgBrowserAdapter.afterInit = function () {
+    //TODO，补充华为快游戏的WebSocket内容,原生支持WebSocket的方案，无qg/hbs.connectSocket的内容
+    PAL.browser.webSocketClass = _WebSocket;
+}
