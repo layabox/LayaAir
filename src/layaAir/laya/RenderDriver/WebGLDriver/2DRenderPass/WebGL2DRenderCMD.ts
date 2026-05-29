449f8c49bbec7e1c16e30f86f3376cc02af70f56
Lurenyiabc <934555872@qq.com>
2026-05-29 15:07:07 +0800

    fix:Context._destRT=>this._dest

--- src/layaAir/laya/RenderDriver/WebGLDriver/2DRenderPass/WebGL2DRenderCMD.ts
+++ src/layaAir/laya/RenderDriver/WebGLDriver/2DRenderPass/WebGL2DRenderCMD.ts
@@ -94,7 +94,7 @@ export class WebGLBlit2DQuadCMD extends Blit2DQuadCMD {
 
     apply(context: WebglRenderContext2D): void {
         let cacheInvert = context.invertY;
-        if (!this._dest || context._destRT._textures[0].gammaCorrection != 1) {
+        if (!this._dest || this._dest._textures[0].gammaCorrection != 1) {
             context.invertY = false;
             this.element.materialShaderData.addDefine(WebGLBlit2DQuadCMD.GammaCorrect);
         } else {
