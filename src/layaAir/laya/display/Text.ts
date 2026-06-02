6446ead9cc0a98bae4aae0bcf066a7569c37b80a
Lurenyiabc <934555872@qq.com>
2026-06-02 10:47:58 +0800

    fix:Stat need init.

--- src/layaAir/laya/display/Text.ts
+++ src/layaAir/laya/display/Text.ts
@@ -224,6 +224,7 @@ export class Text extends Sprite {
         this._lines = [];
         this._padding = [0, 0, 0, 0];
         this._fontSizeScale = 1;
+        this.graphics._useSpriteRect = false;
     }
 
     /**
