95ea50de7812cd87cc5faa00c9d35b8086fa717c
lirong2f <18435164121@163.com>
2026-06-17 14:10:34 +0800

    ffi:LayaXInternalRT/LayaXInternalTex

--- src/layaAir/laya/RenderDriver/LayaXDriver/RenderDevice/LayaXInternalRT.ts
+++ src/layaAir/laya/RenderDriver/LayaXDriver/RenderDevice/LayaXInternalRT.ts
@@ -3,62 +3,83 @@ import { InternalRenderTarget } from "../../DriverDesign/RenderDevice/InternalRe
 import { InternalTexture } from "../../DriverDesign/RenderDevice/InternalTexture";
 import { LayaXInternalTex } from "./LayaXInternalTex";
 
+/**
+ * 共享属性块槽位，与 C++ `LayaXInternalRT_JS::Props` 结构体字段顺序一一对应。
+ * 改任一侧必须同步改另一侧。bool 字段用 Int32 槽 0/1 表示。
+ */
+const enum RTSlot {
+    Samples = 0,
+    IsCube = 1,
+    GenerateMipmap = 2,
+    ColorFormat = 3,
+    DepthStencilFormat = 4,
+    IsSRGB = 5,
+    GpuMemory = 6,
+    Count = 7,
+}
+
 export class LayaXInternalRT implements InternalRenderTarget {
     _texturesRef: InternalTexture[];
     _depthTextureRef: InternalTexture;
     _nativeObj: any;
 
+    /** @internal 共享属性块：C++ 创建时写、bind 时播种、JS 读；槽位见 RTSlot */
+    private _propBuf = new ArrayBuffer(RTSlot.Count * 4);
+    private _propU32 = new Uint32Array(this._propBuf);
+    private _propI32 = new Int32Array(this._propBuf);
+
     constructor(nativeObj: any) {
         this._nativeObj = nativeObj;
+        this._nativeObj.bindPropertyBuffer(this._propBuf);
     }
 
     get _isCube(): boolean {
-        return this._nativeObj._isCube;
+        return this._propI32[RTSlot.IsCube] !== 0;
     }
     set _isCube(value: boolean) {
-        this._nativeObj._isCube = value;
+        this._propI32[RTSlot.IsCube] = value ? 1 : 0;
     }
 
     get _samples(): number {
-        return this._nativeObj._samples;
+        return this._propU32[RTSlot.Samples];
     }
     set _samples(value: number) {
-        this._nativeObj._samples = value;
+        this._propU32[RTSlot.Samples] = value;
     }
 
     get _generateMipmap(): boolean {
-        return this._nativeObj._generateMipmap;
+        return this._propI32[RTSlot.GenerateMipmap] !== 0;
     }
     set _generateMipmap(value: boolean) {
-        this._nativeObj._generateMipmap = value;
+        this._propI32[RTSlot.GenerateMipmap] = value ? 1 : 0;
     }
 
     get colorFormat(): RenderTargetFormat {
-        return this._nativeObj.colorFormat;
+        return this._propI32[RTSlot.ColorFormat];
     }
     set colorFormat(value: RenderTargetFormat) {
-        this._nativeObj.colorFormat = value;
+        this._propI32[RTSlot.ColorFormat] = value;
     }
 
     get depthStencilFormat(): RenderTargetFormat {
-        return this._nativeObj.depthStencilFormat;
+        return this._propI32[RTSlot.DepthStencilFormat];
     }
     set depthStencilFormat(value: RenderTargetFormat) {
-        this._nativeObj.depthStencilFormat = value;
+        this._propI32[RTSlot.DepthStencilFormat] = value;
     }
 
     get isSRGB(): boolean {
-        return this._nativeObj.isSRGB;
+        return this._propI32[RTSlot.IsSRGB] !== 0;
     }
     set isSRGB(value: boolean) {
-        this._nativeObj.isSRGB = value;
+        this._propI32[RTSlot.IsSRGB] = value ? 1 : 0;
     }
 
     get gpuMemory(): number {
-        return this._nativeObj.gpuMemory;
+        return this._propU32[RTSlot.GpuMemory];
     }
     set gpuMemory(value: number) {
-        this._nativeObj.gpuMemory = value;
+        this._propU32[RTSlot.GpuMemory] = value;
     }
 
     get _textures(): InternalTexture[] {
