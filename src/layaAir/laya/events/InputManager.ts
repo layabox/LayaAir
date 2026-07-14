c2e854023d542b455bd298aa0a81956d9e29358c
guzhu <guzhu@fairygui.com>
2026-07-14 16:02:09 +0800

    fix: remove dup code

--- src/layaAir/laya/events/InputManager.ts
+++ src/layaAir/laya/events/InputManager.ts
@@ -248,17 +248,6 @@ export class InputManager {
         }
 
         let doc = Browser.document;
-        let win = Browser.window;
-        win.addEventListener("blur", () => {
-            inst.clearPressedKeys();
-        });
-        win.addEventListener("pagehide", () => {
-            inst.clearPressedKeys();
-        });
-        doc.addEventListener("visibilitychange", () => {
-            if (doc.hidden)
-                inst.clearPressedKeys();
-        });
         doc.addEventListener("keydown", ev => {
             inst.handleKeys(ev);
         }, true);
